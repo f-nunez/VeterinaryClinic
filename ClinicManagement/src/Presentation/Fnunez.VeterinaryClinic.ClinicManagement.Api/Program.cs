d0be9fbf19362ecdd058dd004272a755b7d31174
Francisco Nuñez <fonfonferi@gmail.com>
2022-11-23 18:27:05 -0800

    AddApplicationServices into ClinicManagement.Api #3

--- ClinicManagement/src/Presentation/Fnunez.VeterinaryClinic.ClinicManagement.Api/Program.cs
+++ ClinicManagement/src/Presentation/Fnunez.VeterinaryClinic.ClinicManagement.Api/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddApplicationServices();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
