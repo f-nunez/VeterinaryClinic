def6a11e8a0b3893eff18b1351e06c8c0197bd7c
Francisco Nuñez <fonfonferi@gmail.com>
2022-11-23 22:17:10 -0800

    Patient entity into ClinicManagement.Domain #3

---
+++ ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Domain/Entities/Patient.cs
@@ -0,0 +1,38 @@
+using Fnunez.VeterinaryClinic.ClinicManagement.Domain.Enums;
+using Fnunez.VeterinaryClinic.ClinicManagement.Domain.ValueObjects;
+using Fnunez.VeterinaryClinic.SharedKernel.Domain.Common;
+
+namespace Fnunez.VeterinaryClinic.ClinicManagement.Domain.Entities;
+
+public class Patient : BaseEntity<int>
+{
+    public int ClientId { get; private set; }
+    public string Name { get; private set; }
+    public AnimalSex AnimalSex { get; private set; }
+    public AnimalType? AnimalType { get; private set; }
+    public int? PreferredDoctorId { get; private set; }
+
+    public Patient()
+    {
+        Name = string.Empty;
+    }
+
+    public Patient(
+        int clientId,
+        string name,
+        AnimalSex animalSex,
+        AnimalType animalType,
+        int? preferredDoctorId)
+    {
+        ClientId = clientId;
+        Name = name;
+        AnimalSex = animalSex;
+        AnimalType = animalType;
+        PreferredDoctorId = preferredDoctorId;
+    }
+
+    public override string ToString()
+    {
+        return Name.ToString();
+    }
+}
