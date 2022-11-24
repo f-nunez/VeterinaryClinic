9c73bdbe6f6ad3561a62395cfaf634a0d2a6298f
Francisco Nuñez <fonfonferi@gmail.com>
2022-11-23 22:16:37 -0800

    AppointmentType aggregate into ClinicManagement.Domain #3

---
+++ ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Domain/Aggregates/AppointmentType.cs
@@ -0,0 +1,43 @@
+using Fnunez.VeterinaryClinic.SharedKernel.Domain.Common;
+
+namespace Fnunez.VeterinaryClinic.ClinicManagement.Domain.Aggregates;
+
+public class AppointmentType : BaseEntity<int>, IAggregateRoot
+{
+    public string Name { get; private set; }
+    public string Code { get; private set; }
+    public int Duration { get; private set; }
+
+    public AppointmentType()
+    {
+        Name = string.Empty;
+        Code = string.Empty;
+    }
+
+    public AppointmentType(
+        string name,
+        string code,
+        int duration)
+    {
+        Name = name;
+        Code = code;
+        Duration = duration;
+    }
+
+    public AppointmentType(
+        int id,
+        string name,
+        string code,
+        int duration)
+    {
+        Id = id;
+        Name = name;
+        Code = code;
+        Duration = duration;
+    }
+
+    public override string ToString()
+    {
+        return Name;
+    }
+}
