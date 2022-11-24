ec7f4da23205e88592fab85405c88ced94952c0f
Francisco Nuñez <fonfonferi@gmail.com>
2022-11-23 18:28:32 -0800

    Spacing code style into ClinicManagement.Api #3

--- ClinicManagement/src/Presentation/Fnunez.VeterinaryClinic.ClinicManagement.Api/Program.cs
+++ ClinicManagement/src/Presentation/Fnunez.VeterinaryClinic.ClinicManagement.Api/Program.cs
@@ -6,8 +6,10 @@ builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
