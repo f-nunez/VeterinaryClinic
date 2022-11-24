33f33e12b71fa81ae391d28a7ec19dc648a6d541
Francisco Nuñez <fonfonferi@gmail.com>
2022-11-23 18:01:46 -0800

    ConfigureServices into ClinicManagement.Application #3

---
+++ ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/ConfigureServices.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using FluentValidation;
+using MediatR;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddApplicationServices(
+        this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+        services.AddMediatR(Assembly.GetExecutingAssembly());
+
+        return services;
+    }
+}
