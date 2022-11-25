ff3f89ba6380251807574193488bc1652512b652
Francisco Nuñez <fonfonferi@gmail.com>
2022-11-24 19:50:31 -0800

    GetDoctorByIdQuery into ClinicManagement.Application #3

---
+++ ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Features/Doctors/Queries/GetDoctorById/GetDoctorByIdQueryHandler.cs
@@ -0,0 +1,38 @@
+using AutoMapper;
+using Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel.Doctor;
+using Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel.Doctor.GetDoctorById;
+using Fnunez.VeterinaryClinic.ClinicManagement.Domain.DoctorAggregate;
+using Fnunez.VeterinaryClinic.SharedKernel.Application.Repositories;
+using MediatR;
+
+namespace Fnunez.VeterinaryClinic.ClinicManagement.Application.Features.Doctors.Queries.GetDoctorById;
+
+public class GetDoctorByIdQueryHandler : IRequestHandler<GetDoctorByIdQuery, GetDoctorByIdResponse>
+{
+    private readonly IMapper _mapper;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public GetDoctorByIdQueryHandler(IMapper mapper, IUnitOfWork unitOfWork)
+    {
+        _mapper = mapper;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<GetDoctorByIdResponse> Handle(
+        GetDoctorByIdQuery query,
+        CancellationToken cancellationToken)
+    {
+        GetDoctorByIdRequest request = query.GetDoctorByIdRequest;
+        var response = new GetDoctorByIdResponse(request.CorrelationId);
+
+        var doctor = await _unitOfWork.ReadRepository<Doctor>()
+            .GetByIdAsync(request.DoctorId);
+
+        if (doctor is null)
+            return response;
+
+        response.Doctor = _mapper.Map<DoctorDto>(doctor);
+
+        return response;
+    }
+}
