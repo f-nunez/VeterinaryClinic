e3707a0f620eff8f445e435375131e9b191f73d0
Francisco Nuñez <fonfonferi@gmail.com>
2022-11-24 17:42:17 -0800

    RoomDto into ClinicManagement.Application.SharedModel #3

---
+++ ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel/Room/RoomDto.cs
@@ -0,0 +1,7 @@
+namespace Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel.Room;
+
+public class RoomDto
+{
+    public int RoomId { get; set; }
+    public string? Name { get; set; }
+}
