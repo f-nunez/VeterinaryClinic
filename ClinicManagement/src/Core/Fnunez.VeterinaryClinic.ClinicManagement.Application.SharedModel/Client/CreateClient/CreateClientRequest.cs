b84e37bd132d44db6cd933d43283690a9f3f6563
Francisco Nuñez <fonfonferi@gmail.com>
2022-11-25 11:35:10 -0800

    CreateClientRequest into ClinicManagement.Application.SharedModel #3

---
+++ ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel/Client/CreateClient/CreateClientRequest.cs
@@ -0,0 +1,13 @@
+using Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel.Common;
+
+namespace Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel.Client.CreateClient;
+
+public class CreateClientRequest : BaseRequest
+{
+    public string FullName { get; set; } = string.Empty;
+    public string EmailAddress { get; set; } = string.Empty;
+    public string Salutation { get; set; } = string.Empty;
+    public string PreferredName { get; set; } = string.Empty;
+    public int? PreferredDoctorId { get; set; }
+    public IList<int> Patients { get; set; } = new List<int>();
+}
