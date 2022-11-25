149510f90cdaf92639976867d9a2dc5197865509
Francisco Nuñez <fonfonferi@gmail.com>
2022-11-24 19:35:18 -0800

    DoctorDto into ClinicManagement.SharedModel #3

---
+++ ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel/Doctor/DoctorDto.cs
@@ -0,0 +1,7 @@
+namespace Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel.Doctor;
+
+public class DoctorDto
+{
+    public int DoctorId { get; set; }
+    public string FullName { get; set; } = string.Empty;
+}
