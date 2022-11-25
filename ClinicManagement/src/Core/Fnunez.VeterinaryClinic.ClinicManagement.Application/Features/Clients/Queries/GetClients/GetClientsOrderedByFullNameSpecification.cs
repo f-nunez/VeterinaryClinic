5e1b9220abc53ba0ed407a279c310a2c20f09956
Francisco Nuñez <fonfonferi@gmail.com>
2022-11-25 13:34:30 -0800

    GetClientsOrderedByFullNameSpecification into ClinicManagement.Application #3

---
+++ ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Features/Clients/Queries/GetClients/GetClientsOrderedByFullNameSpecification.cs
@@ -0,0 +1,12 @@
+using Fnunez.VeterinaryClinic.ClinicManagement.Domain.ClientAggregate;
+using Fnunez.VeterinaryClinic.SharedKernel.Application.Specifications;
+
+namespace Fnunez.VeterinaryClinic.ClinicManagement.Application.Features.Clients.Queries.GetClients;
+
+public class GetClientsOrderedByFullNameSpecification : BaseSpecification<Client>
+{
+    public GetClientsOrderedByFullNameSpecification()
+    {
+        AddOrderBy(client => client.FullName);
+    }
+}
