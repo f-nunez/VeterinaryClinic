dd2ddc693b83dbddd37a8d373a88aa8d6b27a784
Francisco Nuñez <fonfonferi@gmail.com>
2022-11-26 22:23:03 -0800

    Add validation clauses on Client's entity constructor in ClinicMamangement.Domain #3

--- ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Domain/ClientAggregate/Client.cs
+++ ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Domain/ClientAggregate/Client.cs
@@ -27,6 +27,31 @@ public class Client : BaseEntity<int>, IAggregateRoot
         string emailAddress,
         int preferredDoctorId)
     {
+        if (string.IsNullOrEmpty(fullName))
+            throw new ArgumentException(
+                $"Required input {nameof(fullName)} was empty.",
+                nameof(fullName));
+
+        if (string.IsNullOrEmpty(preferredName))
+            throw new ArgumentException(
+                $"Required input {nameof(preferredName)} was empty.",
+                nameof(preferredName));
+
+        if (string.IsNullOrEmpty(salutation))
+            throw new ArgumentException(
+                $"Required input {nameof(salutation)} was empty.",
+                nameof(salutation));
+
+        if (string.IsNullOrEmpty(emailAddress))
+            throw new ArgumentException(
+                $"Required input {nameof(emailAddress)} was empty.",
+                nameof(emailAddress));
+
+        if (preferredDoctorId <= 0)
+            throw new ArgumentException(
+                $"Required input {nameof(preferredDoctorId)} cannot be zero or negative.",
+                nameof(preferredDoctorId));
+
         FullName = fullName;
         PreferredName = preferredName;
         Salutation = salutation;
