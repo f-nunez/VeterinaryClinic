5e9611d2c0fb29d9be4166308850e79147627d28
Francisco Nuñez <fonfonferi@gmail.com>
2022-11-28 12:22:09 -0800

    GetAppointmentTypesQuery into ClinicManagement.Application #3

---
+++ ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Features/AppointmentTypes/Queries/GetAppointmentTypes/GetAppointmentTypesQuery.cs
@@ -0,0 +1,6 @@
+using Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel.AppointmentType.GetAppointmentTypes;
+using MediatR;
+
+namespace Fnunez.VeterinaryClinic.ClinicManagement.Application.Features.AppointmentTypes.Queries.GetAppointmentTypes;
+
+public record GetAppointmentTypesQuery(GetAppointmentTypesRequest GetAppointmentTypesRequest) : IRequest<GetAppointmentTypesResponse>;
---
+++ ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Features/AppointmentTypes/Queries/GetAppointmentTypes/GetAppointmentTypesQueryHandler.cs
@@ -0,0 +1,39 @@
+using AutoMapper;
+using Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel.AppointmentType;
+using Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel.AppointmentType.GetAppointmentTypes;
+using Fnunez.VeterinaryClinic.ClinicManagement.Domain.AppointmentTypeAggregate;
+using Fnunez.VeterinaryClinic.SharedKernel.Application.Repositories;
+using MediatR;
+
+namespace Fnunez.VeterinaryClinic.ClinicManagement.Application.Features.AppointmentTypes.Queries.GetAppointmentTypes;
+
+public class GetAppointmentTypesQueryHandler : IRequestHandler<GetAppointmentTypesQuery, GetAppointmentTypesResponse>
+{
+    private readonly IMapper _mapper;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public GetAppointmentTypesQueryHandler(IMapper mapper, IUnitOfWork unitOfWork)
+    {
+        _mapper = mapper;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<GetAppointmentTypesResponse> Handle(
+        GetAppointmentTypesQuery query,
+        CancellationToken cancellationToken)
+    {
+        GetAppointmentTypesRequest request = query.GetAppointmentTypesRequest;
+        var response = new GetAppointmentTypesResponse(request.CorrelationId);
+
+        var appointmentTypes = await _unitOfWork
+            .ReadRepository<AppointmentType>()
+            .ListAsync(cancellationToken);
+
+        response.AppointmentTypes = _mapper
+            .Map<List<AppointmentTypeDto>>(appointmentTypes);
+
+        response.Count = response.AppointmentTypes.Count;
+
+        return response;
+    }
+}
