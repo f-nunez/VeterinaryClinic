8febb8ad84deb771777cc7674384cb853257b58f
Francisco Nuñez <fonfonferi@gmail.com>
2022-11-27 23:27:26 -0800

    CreatePatientCommand into ClinicManagement.Application #3

---
+++ ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Features/Patients/Commands/CreatePatient/CreatePatientCommandHandler.cs
@@ -0,0 +1,57 @@
+using AutoMapper;
+using Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel.Patient;
+using Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel.Patient.CreatePatient;
+using Fnunez.VeterinaryClinic.ClinicManagement.Domain.ClientAggregate;
+using Fnunez.VeterinaryClinic.ClinicManagement.Domain.ClientAggregate.Entities;
+using Fnunez.VeterinaryClinic.ClinicManagement.Domain.ClientAggregate.Enums;
+using Fnunez.VeterinaryClinic.ClinicManagement.Domain.ClientAggregate.ValueObjects;
+using Fnunez.VeterinaryClinic.SharedKernel.Application.Repositories;
+using MediatR;
+
+namespace Fnunez.VeterinaryClinic.ClinicManagement.Application.Features.Patients.Commands.CreatePatient;
+
+public class CreatePatientCommandHandler : IRequestHandler<CreatePatientCommand, CreatePatientResponse>
+{
+    private readonly IMapper _mapper;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CreatePatientCommandHandler(IMapper mapper, IUnitOfWork unitOfWork)
+    {
+        _mapper = mapper;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<CreatePatientResponse> Handle(
+        CreatePatientCommand command,
+        CancellationToken cancellationToken)
+    {
+        CreatePatientRequest request = command.CreatePatientRequest;
+        var response = new CreatePatientResponse(request.CorrelationId);
+        var specification = new ClientByIdIncludePatientsSpecification(request.ClientId);
+
+        var client = await _unitOfWork.Repository<Client>()
+            .FirstOrDefaultAsync(specification, cancellationToken);
+
+        if (client is null)
+            return response;
+
+        var newPatient = new Patient(
+            clientId: request.ClientId,
+            name: request.PatientName,
+            animalSex: AnimalSex.Female,
+            animalType: new AnimalType("Dog", "Chihuahua"),
+            preferredDoctorId: null
+        );
+
+        client.AddPatient(newPatient);
+
+        await _unitOfWork.Repository<Client>()
+            .UpdateAsync(client, cancellationToken);
+        
+        await _unitOfWork.CommitAsync(cancellationToken);
+
+        response.Patient = _mapper.Map<PatientDto>(newPatient);
+
+        return response;
+    }
+}
