a52fb1a68417c3027eaac0e6c7e792f6252ac011
Francisco Nuñez <fonfonferi@gmail.com>
2022-12-05 21:01:25 -0800

    AppointmentConfiguration in Scheduling.Infrastructure #5

---
+++ Scheduling/src/Infrastructure/Fnunez.VeterinaryClinic.Scheduling.Infrastructure/Persistence/Configurations/AppointmentConfiguration.cs
@@ -0,0 +1,26 @@
+using Fnunez.VeterinaryClinic.Scheduling.Domain.ScheduleAggregate.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.Infrastructure.Persistence.Configurations;
+
+public class AppointmentConfiguration : IEntityTypeConfiguration<Appointment>
+{
+    public void Configure(EntityTypeBuilder<Appointment> builder)
+    {
+        builder.OwnsOne(a => a.DateRange, a =>
+        {
+            a.Property(d => d.StartOn)
+                .HasColumnName("DateRange_StartOn")
+                .IsRequired();
+
+            a.Property(d => d.EndOn)
+                .HasColumnName("DateRange_EndOn")
+                .IsRequired();
+        });
+
+        builder.Property(a => a.Title)
+            .HasMaxLength(200)
+            .IsRequired();
+    }
+}
