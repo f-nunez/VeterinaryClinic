fa6967c7c388bc6a8aa8d2ec5006ff5eeed64cc9
Francisco Nuñez <fonfonferi@gmail.com>
2022-12-06 12:28:10 -0800

    GetRoomsRequest in Scheduling.Application.SharedModel #5

---
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel/Room/GetRooms/GetRoomsRequest.cs
@@ -0,0 +1,7 @@
+using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Common;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Room.GetRooms;
+
+public class GetRoomsRequest : BaseRequest
+{
+}
