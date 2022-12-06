ee549c752cbd2987dfcf29a6a9a2eeadea15f0f7
Francisco Nuñez <fonfonferi@gmail.com>
2022-12-06 12:27:31 -0800

    BaseRequest in Scheduling.Application.SharedModel #5

---
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel/Common/BaseRequest.cs
@@ -0,0 +1,8 @@
+namespace Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Common;
+
+/// <summary>
+/// Base class used by API requests
+/// </summary>
+public abstract class BaseRequest : BaseMessage
+{
+}
