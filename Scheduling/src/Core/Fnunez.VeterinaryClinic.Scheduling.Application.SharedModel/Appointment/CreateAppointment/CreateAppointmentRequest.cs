d9214bc120386c914f4b7f7a8638f6e00d94e4a1
Francisco Nuñez <fonfonferi@gmail.com>
2022-12-06 12:38:54 -0800

    CreateAppointmentRequest in Scheduling.Application.SharedModel #5

---
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel/Appointment/CreateAppointment/CreateAppointmentRequest.cs
@@ -0,0 +1,15 @@
+using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Common;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Appointment.CreateAppointment;
+
+public class CreateAppointmentRequest : BaseRequest
+{
+    public int AppointmentTypeId { get; set; }
+    public int ClientId { get; set; }
+    public DateTimeOffset DateOfAppointment { get; set; }
+    public int PatientId { get; set; }
+    public int RoomId { get; set; }
+    public int SelectedDoctor { get; set; }
+    public Guid ScheduleId { get; set; }
+    public string Title { get; set; } = string.Empty;
+}
