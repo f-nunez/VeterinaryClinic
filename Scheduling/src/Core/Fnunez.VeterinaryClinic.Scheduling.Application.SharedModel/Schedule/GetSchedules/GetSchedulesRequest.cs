4ec8e80992cff9bafe58beb9afe5d443a8083721
Francisco Nuñez <fonfonferi@gmail.com>
2022-12-06 12:41:16 -0800

    GetSchedulesRequest in Scheduling.Application.SharedModel #5

---
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel/Schedule/GetSchedules/GetSchedulesRequest.cs
@@ -0,0 +1,7 @@
+using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Common;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Schedule.GetSchedules;
+
+public class GetSchedulesRequest : BaseRequest
+{
+}
