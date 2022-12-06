0fe9abc14e7ffc6299a6677b17a1b79dfad42a70
Francisco Nuñez <fonfonferi@gmail.com>
2022-12-06 13:03:20 -0800

    GetAppointmentTypesQuery in Scheduling.Application.SharedModel #5

---
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application/Features/AppointmentTypes/Queries/GetAppointmentTypes/GetAppointmentTypesQuery.cs
@@ -0,0 +1,6 @@
+using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.AppointmentType.GetAppointmentTypes;
+using MediatR;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.Application.Features.AppointmentTypes.Queries.GetAppointmentTypes;
+
+public record GetAppointmentTypesQuery(GetAppointmentTypesRequest GetAppointmentTypesRequest) : IRequest<GetAppointmentTypesResponse>;
