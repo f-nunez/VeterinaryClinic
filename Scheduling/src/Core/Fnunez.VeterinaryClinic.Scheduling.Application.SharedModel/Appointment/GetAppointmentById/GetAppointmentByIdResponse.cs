0d18c140f81b43536c4470d3eac58cea4b471fab
Francisco Nuñez <fonfonferi@gmail.com>
2022-12-06 12:39:49 -0800

    GetAppointmentByIdResponse in Scheduling.Application.SharedModel #5

---
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel/Appointment/GetAppointmentById/GetAppointmentByIdResponse.cs
@@ -0,0 +1,13 @@
+using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Appointment.CreateAppointment;
+using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Common;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Appointment.GetAppointmentById;
+
+public class GetAppointmentByIdResponse : BaseResponse
+{
+    public AppointmentDto Appointment { get; set; } = new();
+
+    public GetAppointmentByIdResponse(Guid correlationId) : base(correlationId)
+    {
+    }
+}
