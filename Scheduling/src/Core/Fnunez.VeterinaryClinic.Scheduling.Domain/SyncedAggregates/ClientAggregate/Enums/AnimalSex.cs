0c6f36fd2a7ce7db6c38275f47a9f777a20aeab4
Francisco Nuñez <fonfonferi@gmail.com>
2022-12-05 16:54:23 -0800

    Syncronized Client aggregate in Scheduling.Domain #5

---
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Domain/SyncedAggregates/ClientAggregate/Enums/AnimalSex.cs
@@ -0,0 +1,7 @@
+namespace Fnunez.VeterinaryClinic.Scheduling.Domain.SyncedAggregates.ClientAggregate.Enums;
+
+public enum AnimalSex
+{
+    Female,
+    Male
+}
---
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Domain/SyncedAggregates/ClientAggregate/ValueObjects/AnimalType.cs
@@ -0,0 +1,25 @@
+using Fnunez.VeterinaryClinic.SharedKernel.Domain.Common;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.Domain.SyncedAggregates.ClientAggregate.ValueObjects;
+
+public class AnimalType : ValueObject
+{
+    public string Breed { get; private set; } = string.Empty;
+    public string Species { get; private set; } = string.Empty;
+
+    public AnimalType()
+    {
+    }
+
+    public AnimalType(string breed, string species)
+    {
+        Breed = breed;
+        Species = species;
+    }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Breed;
+        yield return Species;
+    }
+}
