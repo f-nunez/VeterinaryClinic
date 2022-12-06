b7b4506381b0f8209098b885bdb6f27d2678049f
Francisco Nuñez <fonfonferi@gmail.com>
2022-12-06 13:17:20 -0800

    GetPatientsQuery in Scheduling.Application #5

---
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application/Features/Patients/Queries/GetPatients/GetPatientsQuery.cs
@@ -0,0 +1,6 @@
+using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Patient.GetPatients;
+using MediatR;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.Application.Features.Patients.Queries.GetPatients;
+
+public record GetPatientsQuery(GetPatientsRequest GetPatientsRequest) : IRequest<GetPatientsResponse>;
