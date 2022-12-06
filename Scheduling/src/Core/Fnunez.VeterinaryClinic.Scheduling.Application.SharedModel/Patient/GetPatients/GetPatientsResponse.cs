38fdc89fef467fbc578f402531ddf79395a6736f
Francisco Nuñez <fonfonferi@gmail.com>
2022-12-06 12:37:17 -0800

    GetPatientsResponse in Scheduling.Application.SharedModel #5

---
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel/Patient/GetPatients/GetPatientsResponse.cs
@@ -0,0 +1,13 @@
+using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Common;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Patient.GetPatients;
+
+public class GetPatientsResponse : BaseResponse
+{
+    public List<PatientDto> Patients { get; set; } = new();
+    public int Count { get; set; }
+
+    public GetPatientsResponse(Guid correlationId) : base(correlationId)
+    {
+    }
+}
