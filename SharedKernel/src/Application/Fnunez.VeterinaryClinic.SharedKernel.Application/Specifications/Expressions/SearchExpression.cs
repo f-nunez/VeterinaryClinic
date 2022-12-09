7120be39cd703bb4eb69d936b36d57bae6aa99ec
Francisco Nuñez <fonfonferi@gmail.com>
2022-12-09 11:55:31 -0800

    Code mells at SearchExpression in SharedKernel.Application #6

--- SharedKernel/src/Application/Fnunez.VeterinaryClinic.SharedKernel.Application/Specifications/Expressions/SearchExpression.cs
+++ SharedKernel/src/Application/Fnunez.VeterinaryClinic.SharedKernel.Application/Specifications/Expressions/SearchExpression.cs
@@ -5,24 +5,6 @@ namespace Fnunez.VeterinaryClinic.SharedKernel.Application.Specifications.Expres
 public class SearchExpression<T>
 {
     /// <summary>
-    /// Creates instance of <see cref="SearchExpression{T}" />.
-    /// </summary>
-    /// <param name="selector">The property to apply the SQL LIKE against.</param>
-    /// <param name="searchTerm">The value to use for the SQL LIKE.</param>
-    /// <param name="searchGroup">The index used to group sets of Selectors and SearchTerms together.</param>
-    /// <exception cref="ArgumentNullException">If <paramref name="selector"/> is null.</exception>
-    /// <exception cref="ArgumentException">If <paramref name="searchTerm"/> is null or empty.</exception>
-    public SearchExpression(Expression<Func<T, string>> selector, string searchTerm, int searchGroup = 1)
-    {
-        _ = selector ?? throw new ArgumentNullException(nameof(selector));
-        if (string.IsNullOrEmpty(searchTerm)) throw new ArgumentException(nameof(searchTerm));
-
-        this.Selector = selector;
-        this.SearchTerm = searchTerm;
-        this.SearchGroup = searchGroup;
-    }
-
-    /// <summary>
     /// The property to apply the SQL LIKE against.
     /// </summary>
     public Expression<Func<T, string>> Selector { get; }
@@ -36,4 +18,25 @@ public class SearchExpression<T>
     /// The index used to group sets of Selectors and SearchTerms together.
     /// </summary>
     public int SearchGroup { get; }
+
+    /// <summary>
+    /// Creates instance of <see cref="SearchExpression{T}" />.
+    /// </summary>
+    /// <param name="selector">The property to apply the SQL LIKE against.</param>
+    /// <param name="searchTerm">The value to use for the SQL LIKE.</param>
+    /// <param name="searchGroup">The index used to group sets of Selectors and SearchTerms together.</param>
+    /// <exception cref="ArgumentNullException">If <paramref name="selector"/> is null.</exception>
+    /// <exception cref="ArgumentException">If <paramref name="searchTerm"/> is null or empty.</exception>
+    public SearchExpression(Expression<Func<T, string>> selector, string searchTerm, int searchGroup = 1)
+    {
+        if (selector is null)
+            throw new ArgumentNullException(nameof(selector));
+
+        if (string.IsNullOrEmpty(searchTerm))
+            throw new ArgumentException(nameof(searchTerm));
+
+        Selector = selector;
+        SearchTerm = searchTerm;
+        SearchGroup = searchGroup;
+    }
 }
