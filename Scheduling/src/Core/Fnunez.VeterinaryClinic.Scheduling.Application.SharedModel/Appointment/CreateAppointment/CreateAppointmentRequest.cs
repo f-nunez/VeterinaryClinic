5cd53b7823e9829ca693c315d8f12db5a3416331
Francisco Nuñez <fonfonferi@gmail.com>
2022-12-12 11:31:44 -0800

    Rename property Selectordoctor to DoctorId into CreateAppointmentRequest in Scheduling.Application.SharedModel #5

--- Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel/Appointment/CreateAppointment/CreateAppointmentRequest.cs
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel/Appointment/CreateAppointment/CreateAppointmentRequest.cs
@@ -6,10 +6,10 @@ public class CreateAppointmentRequest : BaseRequest
 {
     public int AppointmentTypeId { get; set; }
     public int ClientId { get; set; }
+    public int DoctorId { get; set; }
     public DateTimeOffset DateOfAppointment { get; set; }
     public int PatientId { get; set; }
     public int RoomId { get; set; }
-    public int SelectedDoctor { get; set; }
     public Guid ScheduleId { get; set; }
     public string Title { get; set; } = string.Empty;
 }
