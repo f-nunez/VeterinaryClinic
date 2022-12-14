8af933d7a122fa8cda8394fae975236cabdcccb5
Francisco Nuñez <fonfonferi@gmail.com>
2022-12-13 19:06:34 -0800

    CreateScheduleCommand in Scheduling.Application #5

---
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application/Features/Schedules/Commands/CreateSchedule/CreateScheduleCommand.cs
@@ -0,0 +1,6 @@
+using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Schedule.CreateSchedule;
+using MediatR;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.Application.Features.Schedules.Commands.CreateSchedule;
+
+public record CreateScheduleCommand(CreateScheduleRequest CreateScheduleRequest) : IRequest<CreateScheduleResponse>;
---
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application/Features/Schedules/Commands/CreateSchedule/CreateScheduleCommandHandler.cs
@@ -0,0 +1,57 @@
+using AutoMapper;
+using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Schedule;
+using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Schedule.CreateSchedule;
+using Fnunez.VeterinaryClinic.Scheduling.Domain.ScheduleAggregate;
+using Fnunez.VeterinaryClinic.Scheduling.Domain.ScheduleAggregate.ValueObjects;
+using Fnunez.VeterinaryClinic.SharedKernel.Application.Repositories;
+using MediatR;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.Application.Features.Schedules.Commands.CreateSchedule;
+
+public class CreateScheduleCommandHandler : IRequestHandler<CreateScheduleCommand, CreateScheduleResponse>
+{
+    private readonly IMapper _mapper;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CreateScheduleCommandHandler(
+        IMapper mapper,
+        IUnitOfWork unitOfWork)
+    {
+        _mapper = mapper;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<CreateScheduleResponse> Handle(
+        CreateScheduleCommand command,
+        CancellationToken cancellationToken)
+    {
+        CreateScheduleRequest request = command.CreateScheduleRequest;
+        var response = new CreateScheduleResponse(request.CorrelationId);
+        var newSchedule = MapNewSchedule(request);
+
+        await _unitOfWork.Repository<Schedule>()
+            .AddAsync(newSchedule, cancellationToken);
+
+        await _unitOfWork.CommitAsync(cancellationToken);
+
+        response.Schedule = _mapper.Map<ScheduleDto>(newSchedule);
+
+        return response;
+    }
+
+    private Schedule MapNewSchedule(CreateScheduleRequest request)
+    {
+        var dateRange = new DateTimeOffsetRange(
+           request.StartOn,
+           request.EndOn
+       );
+
+        var newSchedule = new Schedule(
+            Guid.NewGuid(),
+            request.ClinicId,
+            dateRange
+        );
+
+        return newSchedule;
+    }
+}
