8ffa4368ea42fd7993f3f68923143c13370e40ee
Francisco Nuñez <fonfonferi@gmail.com>
2022-12-13 19:07:24 -0800

    GetSchedulesQuery in Scheduling.Application #5

---
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application/Features/Schedules/Queries/GetSchedules/GetSchedulesQuery.cs
@@ -0,0 +1,6 @@
+using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Schedule.GetSchedules;
+using MediatR;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.Application.Features.Schedules.Queries.GetSchedules;
+
+public record GetSchedulesQuery(GetSchedulesRequest GetSchedulesRequest) : IRequest<GetSchedulesResponse>;
---
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application/Features/Schedules/Queries/GetSchedules/GetSchedulesQueryHandler.cs
@@ -0,0 +1,40 @@
+using AutoMapper;
+using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Schedule;
+using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Schedule.GetSchedules;
+using Fnunez.VeterinaryClinic.Scheduling.Domain.ScheduleAggregate;
+using Fnunez.VeterinaryClinic.SharedKernel.Application.Repositories;
+using MediatR;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.Application.Features.Schedules.Queries.GetSchedules;
+
+public class GetSchedulesQueryHandler : IRequestHandler<GetSchedulesQuery, GetSchedulesResponse>
+{
+    private readonly IMapper _mapper;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public GetSchedulesQueryHandler(
+        IMapper mapper,
+        IUnitOfWork unitOfWork)
+    {
+        _mapper = mapper;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<GetSchedulesResponse> Handle(
+        GetSchedulesQuery query,
+        CancellationToken cancellationToken)
+    {
+        GetSchedulesRequest request = query.GetSchedulesRequest;
+        var response = new GetSchedulesResponse(request.CorrelationId);
+
+        var schedules = await _unitOfWork.ReadRepository<Schedule>()
+            .ListAsync(cancellationToken);
+
+        if (schedules is null)
+            return response;
+
+        response.Schedules = _mapper.Map<List<ScheduleDto>>(schedules);
+
+        return response;
+    }
+}
