1b6f5416c4a8d833e91adf36af746eedea430bfe
Francisco Nuñez <fonfonferi@gmail.com>
2022-12-19 08:55:33 -0800

    HttpService in Scheduling.BlazorClient.Client #5

--- Scheduling/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client/Program.cs
+++ Scheduling/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client/Program.cs
@@ -2,12 +2,16 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client;
 using Radzen;
+using Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// register HttpClient and HttpService
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["ApiUrl"]!) });
+builder.Services.AddScoped<HttpService>();
+
 builder.Services.AddScoped<DialogService>();
 builder.Services.AddScoped<NotificationService>();
 
