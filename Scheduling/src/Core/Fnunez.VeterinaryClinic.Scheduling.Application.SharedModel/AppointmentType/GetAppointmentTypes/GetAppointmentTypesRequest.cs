0979f0555c5d8d121c7a8298350a732fee3a91f4
Francisco Nuñez <fonfonferi@gmail.com>
2022-12-20 15:45:58 -0800

    Add DataGridRequest into GetAppointmentTypesRequest in Scheduling.Application.SharedModel #5

--- Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel/AppointmentType/GetAppointmentTypes/GetAppointmentTypesRequest.cs
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel/AppointmentType/GetAppointmentTypes/GetAppointmentTypesRequest.cs
@@ -4,4 +4,10 @@ namespace Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Appointment
 
 public class GetAppointmentTypesRequest : BaseRequest
 {
+    public DataGridRequest DataGridRequest { get; set; } = new();
+    public string CodeFilterValue { get; set; } = null!;
+    public string DurationFilterValue { get; set; } = null!;
+    public string IdFilterValue { get; set; } = null!;
+    public string NameFilterValue { get; set; } = null!;
+    public string SearchFilterValue { get; set; } = null!;
 }
