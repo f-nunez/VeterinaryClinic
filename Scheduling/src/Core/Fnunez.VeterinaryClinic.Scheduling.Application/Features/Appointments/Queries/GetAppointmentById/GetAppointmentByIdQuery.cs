6e1dd9ab0bfc8e371cd869a42c98e5b57be31293
Francisco Nuñez <fonfonferi@gmail.com>
2022-12-20 15:54:18 -0800

    Code smells at GetAppointmentByIdQuery in Scheduling.Application #5

--- Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application/Features/Appointments/Queries/GetAppointmentById/GetAppointmentByIdQuery.cs
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application/Features/Appointments/Queries/GetAppointmentById/GetAppointmentByIdQuery.cs
@@ -3,4 +3,5 @@ using MediatR;
 
 namespace Fnunez.VeterinaryClinic.Scheduling.Application.Features.Appointments.Queries.GetAppointmentById;
 
-public record GetAppointmentByIdQuery(GetAppointmentByIdRequest GetAppointmentByIdRequest) : IRequest<GetAppointmentByIdResponse>;
+public record GetAppointmentByIdQuery(GetAppointmentByIdRequest GetAppointmentByIdRequest)
+    : IRequest<GetAppointmentByIdResponse>;
