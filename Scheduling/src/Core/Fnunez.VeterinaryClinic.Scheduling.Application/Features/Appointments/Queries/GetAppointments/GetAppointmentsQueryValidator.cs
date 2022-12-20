b33abbb2a49979118226418a227d0f3fe38ff13f
Francisco Nuñez <fonfonferi@gmail.com>
2022-12-20 15:55:13 -0800

    Code smells at GetAppointmentsQueryValidator in Scheduling.Application #5

--- Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application/Features/Appointments/Queries/GetAppointments/GetAppointmentsQueryValidator.cs
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application/Features/Appointments/Queries/GetAppointments/GetAppointmentsQueryValidator.cs
@@ -2,7 +2,8 @@ using FluentValidation;
 
 namespace Fnunez.VeterinaryClinic.Scheduling.Application.Features.Appointments.Queries.GetAppointments;
 
-public class GetAppointmentsQueryValidator : AbstractValidator<GetAppointmentsQuery>
+public class GetAppointmentsQueryValidator
+    : AbstractValidator<GetAppointmentsQuery>
 {
     public GetAppointmentsQueryValidator()
     {
