ca757a57ba68a78457fbe8b1311e53111bcdf6eb
Francisco Nuñez <fonfonferi@gmail.com>
2022-12-21 11:45:44 -0800

    ClientSalutationSpecification in Scheduling.Application #5

---
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application/Features/Clients/Queries/GetClientsFilterSalutation/ClientSalutationsSpecification.cs
@@ -0,0 +1,20 @@
+using Fnunez.VeterinaryClinic.Scheduling.Domain.SyncedAggregates.ClientAggregate;
+using Fnunez.VeterinaryClinic.SharedKernel.Application.Specifications;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.Application.Features.Clients.Queries.GetClientsFilterSalutation;
+
+public class ClientSalutationsSpecification : BaseSpecification<Client, string>
+{
+    public ClientSalutationsSpecification(string salutationFilterValue)
+    {
+        Query
+            .AsNoTracking()
+            .Where(c => c.Salutation.Trim().ToLower().Contains(
+                salutationFilterValue.Trim().ToLower()))
+            .OrderBy(c => c.Salutation)
+            .Take(10);
+
+        Query
+            .Select(c => c.Salutation);
+    }
+}
