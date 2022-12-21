77627093e9a97835a9de2b428a440f84640ab97c
Francisco Nuñez <fonfonferi@gmail.com>
2022-12-21 05:44:14 -0800

    GetAppointmentTypesFilterDurationQueryValidator in Scheduling.Application #5

---
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application/Features/AppointmentTypes/Queries/GetAppointmentTypesFilterDuration/GetAppointmentTypesFilterDurationQueryValidator.cs
@@ -0,0 +1,14 @@
+using FluentValidation;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.Application.Features.AppointmentTypes.Queries.GetAppointmentTypesFilterDuration;
+
+public class GetAppointmentTypesFilterDurationQueryValidator
+    : AbstractValidator<GetAppointmentTypesFilterDurationQuery>
+{
+    public GetAppointmentTypesFilterDurationQueryValidator()
+    {
+        RuleFor(v => v.GetAppointmentTypesFilterDurationRequest.DurationFilterValue)
+            .NotNull().WithMessage("DurationFilterValue is required.")
+            .NotEmpty().WithMessage("DurationFilterValue is required.");
+    }
+}
