2fe919d5e86ac48775b1047cea6b9e3fa6c0b325
Francisco Nuñez <fonfonferi@gmail.com>
2022-12-21 11:44:14 -0800

    GetClientsFilterIdQueryHandler in Scheduling.Application #5

---
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application/Features/Clients/Queries/GetClientsFilterId/GetClientsFilterIdQueryHandler.cs
@@ -0,0 +1,37 @@
+using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Client.GetClientsFilterId;
+using Fnunez.VeterinaryClinic.Scheduling.Domain.SyncedAggregates.ClientAggregate;
+using Fnunez.VeterinaryClinic.SharedKernel.Application.Repositories;
+using MediatR;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.Application.Features.Clients.Queries.GetClientsFilterId;
+
+public class GetClientsFilterIdQueryHandler
+    : IRequestHandler<GetClientsFilterIdQuery, GetClientsFilterIdResponse>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public GetClientsFilterIdQueryHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<GetClientsFilterIdResponse> Handle(
+        GetClientsFilterIdQuery query,
+        CancellationToken cancellationToken)
+    {
+        GetClientsFilterIdRequest request = query.GetClientsFilterIdRequest;
+        var response = new GetClientsFilterIdResponse(request.CorrelationId);
+        var specification = new ClientIdsSpecification(request.IdFilterValue);
+
+        var clientIds = await _unitOfWork
+            .ReadRepository<Client>()
+            .ListAsync(specification, cancellationToken);
+
+        if (clientIds is null)
+            return response;
+
+        response.ClientIds = clientIds;
+
+        return response;
+    }
+}
