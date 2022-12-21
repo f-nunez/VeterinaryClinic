c4f5d74b610b54862019744bf1cc97f1dfe11ba3
Francisco Nuñez <fonfonferi@gmail.com>
2022-12-21 09:26:56 -0800

    Add data grid response into GetClientsResponse in Scheduling.Application.SharedModel #5

--- Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel/Client/GetClients/GetClientsResponse.cs
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel/Client/GetClients/GetClientsResponse.cs
@@ -4,8 +4,7 @@ namespace Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Client.GetC
 
 public class GetClientsResponse : BaseResponse
 {
-    public List<ClientDto> Clients { get; set; } = new();
-    public int Count { get; set; }
+    public DataGridResponse<ClientDto>? DataGridResponse { get; set; }
 
     public GetClientsResponse(Guid correlationId) : base(correlationId)
     {
