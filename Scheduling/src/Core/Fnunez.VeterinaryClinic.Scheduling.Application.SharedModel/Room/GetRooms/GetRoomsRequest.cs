2fab86c0d265633ccd89f2854a0980320b90074b
Francisco Nuñez <fonfonferi@gmail.com>
2022-12-21 11:49:53 -0800

    Add DataGridRequest into GetRoomsRequest in Scheduling.Application.SharedModel #5

--- Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel/Room/GetRooms/GetRoomsRequest.cs
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel/Room/GetRooms/GetRoomsRequest.cs
@@ -4,4 +4,8 @@ namespace Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Room.GetRoo
 
 public class GetRoomsRequest : BaseRequest
 {
+    public DataGridRequest DataGridRequest { get; set; } = new();
+    public string IdFilterValue { get; set; } = null!;
+    public string NameFilterValue { get; set; } = null!;
+    public string SearchFilterValue { get; set; } = null!;
 }
