c6047c9e87f435982fd88b4349dbedadb7dd7c05
Francisco Nuñez <fonfonferi@gmail.com>
2022-12-21 11:42:24 -0800

    GetClientsFilterEmailAddressQueryHandler in Scheduling.Application #5

---
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application/Features/Clients/Queries/GetClientsFilterEmailAddress/GetClientsFilterEmailAddressQueryHandler.cs
@@ -0,0 +1,42 @@
+using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Client.GetClientsFilterEmailAddress;
+using Fnunez.VeterinaryClinic.Scheduling.Domain.SyncedAggregates.ClientAggregate;
+using Fnunez.VeterinaryClinic.SharedKernel.Application.Repositories;
+using MediatR;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.Application.Features.Clients.Queries.GetClientsFilterEmailAddress;
+
+public class GetClientsFilterEmailAddressQueryHandler
+    : IRequestHandler<GetClientsFilterEmailAddressQuery, GetClientsFilterEmailAddressResponse>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public GetClientsFilterEmailAddressQueryHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<GetClientsFilterEmailAddressResponse> Handle(
+        GetClientsFilterEmailAddressQuery query,
+        CancellationToken cancellationToken)
+    {
+        GetClientsFilterEmailAddressRequest request = query
+            .GetClientsFilterEmailAddressRequest;
+
+        var response = new GetClientsFilterEmailAddressResponse(
+            request.CorrelationId);
+
+        var specification = new ClientEmailAddressesSpecification(
+            request.EmailAddressFilterValue);
+
+        var clientEmailAddresses = await _unitOfWork
+            .ReadRepository<Client>()
+            .ListAsync(specification, cancellationToken);
+
+        if (clientEmailAddresses is null)
+            return response;
+
+        response.ClientEmailAddresses = clientEmailAddresses;
+
+        return response;
+    }
+}
