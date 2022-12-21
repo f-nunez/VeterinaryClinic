23211c18a4ac498ee4b9540504d2e3b0b62fbf51
Francisco Nuñez <fonfonferi@gmail.com>
2022-12-21 11:44:24 -0800

    GetClientsFilterIdQueryValidator in Scheduling.Application #5

---
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application/Features/Clients/Queries/GetClientsFilterId/GetClientsFilterIdQueryValidator.cs
@@ -0,0 +1,14 @@
+using FluentValidation;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.Application.Features.Clients.Queries.GetClientsFilterId;
+
+public class GetClientsFilterIdQueryValidator
+    : AbstractValidator<GetClientsFilterIdQuery>
+{
+    public GetClientsFilterIdQueryValidator()
+    {
+        RuleFor(v => v.GetClientsFilterIdRequest.IdFilterValue)
+            .NotNull().WithMessage("IdFilterValue is required.")
+            .NotEmpty().WithMessage("IdFilterValue is required.");
+    }
+}
