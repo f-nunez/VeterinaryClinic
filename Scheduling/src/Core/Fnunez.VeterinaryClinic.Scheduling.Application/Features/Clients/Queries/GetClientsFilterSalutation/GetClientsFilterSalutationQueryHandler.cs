3cea720f07718f7dd961b5867e100c9dc2b2aa00
Francisco Nuñez <fonfonferi@gmail.com>
2022-12-21 11:46:09 -0800

    GetClientsFilterSalutationQueryHandler in Scheduling.Application #5

---
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application/Features/Clients/Queries/GetClientsFilterSalutation/GetClientsFilterSalutationQueryHandler.cs
@@ -0,0 +1,42 @@
+using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Client.GetClientsFilterSalutation;
+using Fnunez.VeterinaryClinic.Scheduling.Domain.SyncedAggregates.ClientAggregate;
+using Fnunez.VeterinaryClinic.SharedKernel.Application.Repositories;
+using MediatR;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.Application.Features.Clients.Queries.GetClientsFilterSalutation;
+
+public class GetClientsFilterSalutationQueryHandler
+    : IRequestHandler<GetClientsFilterSalutationQuery, GetClientsFilterSalutationResponse>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public GetClientsFilterSalutationQueryHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<GetClientsFilterSalutationResponse> Handle(
+        GetClientsFilterSalutationQuery query,
+        CancellationToken cancellationToken)
+    {
+        GetClientsFilterSalutationRequest request = query
+            .GetClientsFilterSalutationRequest;
+
+        var response = new GetClientsFilterSalutationResponse(
+            request.CorrelationId);
+
+        var specification = new ClientSalutationsSpecification(
+            request.SalutationFilterValue);
+
+        var clientSalutations = await _unitOfWork
+            .ReadRepository<Client>()
+            .ListAsync(specification, cancellationToken);
+
+        if (clientSalutations is null)
+            return response;
+
+        response.ClientSalutations = clientSalutations;
+
+        return response;
+    }
+}
