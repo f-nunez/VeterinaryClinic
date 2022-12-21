1133b323d7043fbd1316df6a486bcb5d789aa8b5
Francisco Nuñez <fonfonferi@gmail.com>
2022-12-21 09:26:31 -0800

    Add data grid feature into GetClientsRequest in Scheduling.Application.SharedModel #5

--- Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel/Client/GetClients/GetClientsRequest.cs
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel/Client/GetClients/GetClientsRequest.cs
@@ -4,4 +4,11 @@ namespace Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Client.GetC
 
 public class GetClientsRequest : BaseRequest
 {
+    public DataGridRequest DataGridRequest { get; set; } = new();
+    public string EmailAddressFilterValue { get; set; } = string.Empty;
+    public string FullNameFilterValue { get; set; } = string.Empty;
+    public string IdFilterValue { get; set; } = string.Empty;
+    public string PreferredNameFilterValue { get; set; } = string.Empty;
+    public string SalutationFilterValue { get; set; } = string.Empty;
+    public string SearchFilterValue { get; set; } = string.Empty;
 }
