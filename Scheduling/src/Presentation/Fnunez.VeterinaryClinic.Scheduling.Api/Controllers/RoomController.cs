684253cd567171b3d35f3b7dd61510763641d291
Francisco Nuñez <fonfonferi@gmail.com>
2022-12-22 12:32:17 -0800

    Add DataGrid endpoints into RoomController in Scheduling.Api #5

--- Scheduling/src/Presentation/Fnunez.VeterinaryClinic.Scheduling.Api/Controllers/RoomController.cs
+++ Scheduling/src/Presentation/Fnunez.VeterinaryClinic.Scheduling.Api/Controllers/RoomController.cs
@@ -1,19 +1,49 @@
 using Fnunez.VeterinaryClinic.Scheduling.Application.Features.Rooms.Queries.GetRooms;
+using Fnunez.VeterinaryClinic.Scheduling.Application.Features.Rooms.Queries.GetRoomsFilterId;
+using Fnunez.VeterinaryClinic.Scheduling.Application.Features.Rooms.Queries.GetRoomsFilterName;
 using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Room.GetRooms;
+using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Room.GetRoomsFilterId;
+using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Room.GetRoomsFilterName;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Fnunez.VeterinaryClinic.Scheduling.Api.Controllers;
 
 public class RoomController : BaseApiController
 {
-    [HttpGet("List")]
-    public async Task<ActionResult> List(
-        [FromQuery] GetRoomsRequest request,
+    [HttpPost("DataGrid")]
+    public async Task<ActionResult> DataGrid(
+        GetRoomsRequest request,
         CancellationToken cancellationToken)
     {
         var query = new GetRoomsQuery(request);
 
         GetRoomsResponse response = await Mediator
+            .Send(query, cancellationToken);
+
+        return Ok(response);
+    }
+
+    [HttpPost("DataGridFilterId")]
+    public async Task<ActionResult> DataGridFilterId(
+        GetRoomsFilterIdRequest request,
+        CancellationToken cancellationToken)
+    {
+        var query = new GetRoomsFilterIdQuery(request);
+
+        GetRoomsFilterIdResponse response = await Mediator
+            .Send(query, cancellationToken);
+
+        return Ok(response);
+    }
+
+    [HttpPost("DataGridFilterName")]
+    public async Task<ActionResult> DataGridFilterName(
+        GetRoomsFilterNameRequest request,
+        CancellationToken cancellationToken)
+    {
+        var query = new GetRoomsFilterNameQuery(request);
+
+        GetRoomsFilterNameResponse response = await Mediator
             .Send(query, cancellationToken);
 
         return Ok(response);
