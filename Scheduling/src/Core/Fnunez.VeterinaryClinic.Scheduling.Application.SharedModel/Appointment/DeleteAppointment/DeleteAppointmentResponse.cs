604e10d70464baac4089875536430c116af4583b
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-02 15:42:57 -0800

    Update DeleteAppointmentResponse in Scheduling.Application.SharedModel #5

--- Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel/Appointment/DeleteAppointment/DeleteAppointmentResponse.cs
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel/Appointment/DeleteAppointment/DeleteAppointmentResponse.cs
@@ -1,12 +1,9 @@
 using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Common;
-using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Schedule;
 
 namespace Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Appointment.DeleteAppointment;
 
 public class DeleteAppointmentResponse : BaseResponse
 {
-    public ScheduleDto Schedule { get; set; } = new();
-
     public DeleteAppointmentResponse(Guid correlationId) : base(correlationId)
     {
     }
