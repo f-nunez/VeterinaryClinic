f20eccab1186c35c5948995326f1b434c67ff924
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-02 15:47:46 -0800

    DoctorFilterValueDto in Scheduling.Application.SharedModel #5

---
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel/Appointment/GetAppointmentsFilterDoctor/DoctorFilterValueDto.cs
@@ -0,0 +1,7 @@
+namespace Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Appointment.GetAppointmentsFilterDoctor;
+
+public class DoctorFilterValueDto
+{
+    public int Id { get; set; }
+    public string FullName { get; set; } = string.Empty;
+}
