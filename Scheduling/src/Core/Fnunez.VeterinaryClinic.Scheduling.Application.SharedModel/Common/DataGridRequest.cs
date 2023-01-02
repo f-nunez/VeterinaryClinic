5f108e2e45bb6881d5b4d41a9f5788c2f6ffd562
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-02 15:41:37 -0800

    Add sort by filter string when Radzen components havent sort descriptor in Scheduling.Application.SharedModel #5

--- Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel/Common/DataGridRequest.cs
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel/Common/DataGridRequest.cs
@@ -2,16 +2,17 @@ namespace Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Common;
 
 public class DataGridRequest
 {
-    public List<DataGridRequestSort>? Sorts { get; set; }
+    public string Search { get; set; } = string.Empty;
     public int? Skip { get; set; }
+    public List<DataGridRequestSort>? Sorts { get; set; }
     public int? Take { get; set; }
 
     public override string ToString()
     {
         string sortProperties = string.Empty;
         if (Sorts != null)
-            sortProperties = string.Join(',', Sorts.Select(s => s.PropertyName));
+            sortProperties = string.Join(',', Sorts.Select(s => $"{s.PropertyName} isAsc:{s.IsAscending.ToString()}"));
 
-        return $"Skip: {Skip}, Take: {Take}, Sorts: {sortProperties}";
+        return $"Sorts: {sortProperties}, Search: {Search}, Skip: {Skip}, Take: {Take}";
     }
 }
