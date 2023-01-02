13c631be631022fe3b382b6a8244c3841d2c157a
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-02 15:43:49 -0800

    Update GetAppointmentsRequest in Scheduling.Application.SharedModel #5

--- Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel/Appointment/GetAppointments/GetAppointmentsRequest.cs
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel/Appointment/GetAppointments/GetAppointmentsRequest.cs
@@ -4,5 +4,10 @@ namespace Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Appointment
 
 public class GetAppointmentsRequest : BaseRequest
 {
-    public Guid ScheduleId { get; set; }
+    public DataGridRequest DataGridRequest { get; set; } = new();
+    public string ClientIdFilterValue { get; set; } = string.Empty;
+    public string ClinicIdFilterValue { get; set; } = string.Empty;
+    public string PatientIdFilterValue { get; set; } = string.Empty;
+    public DateTimeOffset StartOn { get; set; }
+    public DateTimeOffset EndOn { get; set; }
 }
