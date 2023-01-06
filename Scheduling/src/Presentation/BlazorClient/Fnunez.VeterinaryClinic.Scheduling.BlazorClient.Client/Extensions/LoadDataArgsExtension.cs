4b4330dcdca3b0ae92f1c07727c0923030fb4ab3
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-05 20:11:25 -0800

    Fix to get search filter from radzen components that have Search binding input in Scheduling.BlazorClient.Client #5

--- Scheduling/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client/Extensions/LoadDataArgsExtension.cs
+++ Scheduling/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client/Extensions/LoadDataArgsExtension.cs
@@ -8,6 +8,7 @@ public static class LoadDataArgsExtension
     public static DataGridRequest GetDataGridRequest(this LoadDataArgs loadDataArgs)
     {
         List<DataGridRequestSort> sorts = new();
+
         if (loadDataArgs.Sorts != null)
         {
             sorts.AddRange(
@@ -20,8 +21,20 @@ public static class LoadDataArgsExtension
             );
         }
 
+        if (loadDataArgs.Sorts is null && !string.IsNullOrEmpty(loadDataArgs.OrderBy))
+        {
+            string[] orderBy = loadDataArgs.OrderBy.Split(' ');
+            sorts.Add(
+                new DataGridRequestSort(
+                    orderBy[0],
+                    orderBy[1] == "asc"
+                )
+            );
+        }
+
         var dataGridRequest = new DataGridRequest
         {
+            Search = loadDataArgs.Filter,
             Skip = loadDataArgs.Skip,
             Sorts = sorts,
             Take = loadDataArgs.Top
