cb9d7fdb46140d91bded2cb86719045406d619dd
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-05 20:13:29 -0800

    Register LayoutSpinnerService into Program startup class in Scheduling.BlazorClient.Client #5

--- Scheduling/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client/Program.cs
+++ Scheduling/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client;
 using Radzen;
 using Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client.Services;
+using Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client.Shared.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -22,5 +23,8 @@ builder.Services.AddScoped<RoomService>();
 // register Radzen services
 builder.Services.AddScoped<DialogService>();
 builder.Services.AddScoped<NotificationService>();
+
+// register Spinner
+builder.Services.AddScoped<LayoutSpinnerService>();
 
 await builder.Build().RunAsync();
