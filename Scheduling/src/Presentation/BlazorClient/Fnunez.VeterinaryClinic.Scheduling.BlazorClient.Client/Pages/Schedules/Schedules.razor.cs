dd894c86edb31205a54e981d5021ecd2eda351f6
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-06 10:31:12 -0800

    Remove Schedules module in Scheduling.BlazorClient.Client #5

--- Scheduling/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client/Pages/Schedules/Schedules.razor.cs
+++
@@ -1,5 +0,0 @@
-namespace Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client.Pages.Schedules;
-
-public partial class SchedulesComponent
-{
-}
--- Scheduling/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client/Pages/Schedules/Schedules.razor.designer.cs
+++
@@ -1,7 +0,0 @@
-using Microsoft.AspNetCore.Components;
-
-namespace Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client.Pages.Schedules;
-
-public partial class SchedulesComponent : ComponentBase
-{
-}
