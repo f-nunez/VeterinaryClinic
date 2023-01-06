7619df370ecaad29ae6d50e6d1a8bc05267f305a
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-06 10:33:17 -0800

    Remove Schedule Aggregate in Scheduling.Application #5

--- Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Domain/ScheduleAggregate/Exceptions/DuplicateAppointmentException.cs
+++
@@ -1,9 +0,0 @@
-namespace Fnunez.VeterinaryClinic.Scheduling.Domain.ScheduleAggregate.Exceptions;
-
-public class DuplicateAppointmentException : ArgumentException
-{
-    public DuplicateAppointmentException(string message, string paramName)
-        : base(message, paramName)
-    {
-    }
-}
