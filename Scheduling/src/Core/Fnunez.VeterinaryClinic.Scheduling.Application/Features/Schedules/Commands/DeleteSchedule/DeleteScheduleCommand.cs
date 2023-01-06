539c031eacefe31843fe1bf11dd5721f890d2211
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-06 10:32:53 -0800

    Remove Schedule features in Scheduling.Application #5

--- Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application/Features/Schedules/Commands/DeleteSchedule/DeleteScheduleCommand.cs
+++
@@ -1,7 +0,0 @@
-using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Schedule.DeleteSchedule;
-using MediatR;
-
-namespace Fnunez.VeterinaryClinic.Scheduling.Application.Features.Schedules.Commands.DeleteSchedule;
-
-public record DeleteScheduleCommand(DeleteScheduleRequest DeleteScheduleRequest)
-    : IRequest<DeleteScheduleResponse>;
--- Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application/Features/Schedules/Queries/GetScheduleById/GetScheduleByIdQuery.cs
+++
@@ -1,7 +0,0 @@
-using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Schedule.GetScheduleById;
-using MediatR;
-
-namespace Fnunez.VeterinaryClinic.Scheduling.Application.Features.Schedules.Queries.GetScheduleById;
-
-public record GetScheduleByIdQuery(GetScheduleByIdRequest GetScheduleByIdRequest)
-    : IRequest<GetScheduleByIdResponse>;
