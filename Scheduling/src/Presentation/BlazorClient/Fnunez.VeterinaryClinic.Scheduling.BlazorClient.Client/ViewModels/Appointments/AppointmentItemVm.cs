b57a253a731cda1778eb7f6f561eb5ebfd19f138
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-05 20:10:14 -0800

    AppointmentItemVm to map appointment info for add and edit page in Scheduling.BlazorClient.Client #5

---
+++ Scheduling/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client/ViewModels/Appointments/AppointmentItemVm.cs
@@ -0,0 +1,20 @@
+namespace Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client.ViewModels.Appointments;
+
+public class AppointmentItemVm
+{
+    public Guid AppointmentId { get; set; }
+    public int AppointmentTypeId { get; set; }
+    public int ClientId { get; set; }
+    public string ClientFullName { get; set; } = string.Empty;
+    public int ClinicId { get; set; }
+    public string ClinicName { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public int DoctorId { get; set; }
+    public bool IsConfirmed { get; set; }
+    public int PatientId { get; set; }
+    public string PatientName { get; set; } = string.Empty;
+    public int RoomId { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public DateTime StartOn { get; set; }
+    public DateTime EndOn { get; set; }
+}
