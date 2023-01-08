f6b1caae90e603e6b2f6365a3447f4de6c1cbe12
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-07 17:57:14 -0800

    Register TimeZone and UserSettingsServices into Program startup class in Scheduling.BlazorClient.Client #5

--- Scheduling/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client/Program.cs
+++ Scheduling/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client/Program.cs
@@ -4,6 +4,10 @@ using Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client;
 using Radzen;
 using Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client.Services;
 using Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client.Shared.Components;
+using Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client.Shared.Components.TimeZone;
+using Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client.Settings;
+using Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client.Shared.Components.UserSettings;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -21,11 +25,29 @@ builder.Services.AddScoped<ClinicService>();
 builder.Services.AddScoped<DoctorService>();
 builder.Services.AddScoped<RoomService>();
 
+// register UserSettingsService
+builder.Services.AddScoped<UserSettingsService>();
+
 // register Radzen services
 builder.Services.AddScoped<DialogService>();
 builder.Services.AddScoped<NotificationService>();
 
 // register Spinner
 builder.Services.AddScoped<LayoutSpinnerService>();
+
+// register TimeZone
+builder.Services.AddSingleton<ITimeZoneComponentData>(builder.Configuration.GetSection(typeof(TimeZoneComponentData).Name).Get<TimeZoneComponentData>()!);
+
+// register TimeZone service
+builder.Services.AddScoped<ITimeZoneComponentService, TimeZoneComponentService>();
+
+// register Cookie settings
+builder.Services.AddSingleton<ICookieSettings>(builder.Configuration.GetSection(typeof(CookieSettings).Name).Get<CookieSettings>()!);
+
+// register User settings service
+builder.Services.AddScoped<IUserSettingsComponentService, UserSettingsComponentService>();
+
+// register BlazoredLocalStorage
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
