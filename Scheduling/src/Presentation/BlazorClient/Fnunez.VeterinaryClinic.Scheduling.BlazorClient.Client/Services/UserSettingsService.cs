41989031c6d29da9ccafda2de0cda07fb8bc7b7f
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-07 17:49:20 -0800

    UserSettingsService to manage UserSettingsComponent behaviors in Scheduling.BlazorClient.Client #5

---
+++ Scheduling/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client/Services/UserSettingsService.cs
@@ -0,0 +1,40 @@
+using Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client.Shared.Components.TimeZone;
+using Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client.Shared.Components.UserSettings;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client.Services;
+
+public class UserSettingsService
+{
+    private readonly IUserSettingsComponentService _userSettingsComponentService;
+    private readonly ITimeZoneComponentService _timeZoneComponentService;
+
+    public UserSettingsService(
+        IUserSettingsComponentService userSettingsComponentService,
+        ITimeZoneComponentService timeZoneComponentService)
+    {
+        _userSettingsComponentService = userSettingsComponentService;
+        _timeZoneComponentService = timeZoneComponentService;
+    }
+
+    public async Task<int> GetUtcOffsetInMinutesAsync()
+    {
+        var userSettings = await _userSettingsComponentService
+            .GetSettingsAsync();
+
+        var timeZone = _timeZoneComponentService
+            .GetTimeZone(userSettings.TimeZoneId);
+
+        return timeZone.UtcOffsetInMinutes;
+    }
+
+    public async Task<string> GetTimeZoneNameAsync()
+    {
+        var userSettings = await _userSettingsComponentService
+            .GetSettingsAsync();
+
+        var timeZone = _timeZoneComponentService
+            .GetTimeZone(userSettings.TimeZoneId);
+
+        return timeZone.DisplayName;
+    }
+}
