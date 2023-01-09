74b434e0d5dd134830353885883b820a0d38971f
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-08 21:41:14 -0800

    Register service classes by interfaces into dependecy container in Scheduling.BlazorClient.Client #5

--- Scheduling/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client/Program.cs
+++ Scheduling/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client/Program.cs
@@ -3,11 +3,11 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client;
 using Radzen;
 using Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client.Services;
-using Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client.Shared.Components;
 using Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client.Shared.Components.TimeZone;
 using Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client.Settings;
 using Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client.Shared.Components.UserSettings;
 using Blazored.LocalStorage;
+using Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client.Shared.Components.Spinner;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -15,39 +15,36 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 // register HttpClient and HttpService
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["ApiUrl"]!) });
-builder.Services.AddScoped<HttpService>();
+builder.Services.AddScoped<IHttpService, HttpService>();
 
-// register http services
-builder.Services.AddScoped<AppointmentService>();
-builder.Services.AddScoped<AppointmentTypeService>();
-builder.Services.AddScoped<ClientService>();
-builder.Services.AddScoped<ClinicService>();
-builder.Services.AddScoped<DoctorService>();
-builder.Services.AddScoped<RoomService>();
-
-// register UserSettingsService
-builder.Services.AddScoped<UserSettingsService>();
-
-// register Radzen services
-builder.Services.AddScoped<DialogService>();
-builder.Services.AddScoped<NotificationService>();
-
-// register Spinner
-builder.Services.AddScoped<LayoutSpinnerService>();
-
-// register TimeZone
-builder.Services.AddSingleton<ITimeZoneComponentData>(builder.Configuration.GetSection(typeof(TimeZoneComponentData).Name).Get<TimeZoneComponentData>()!);
-
-// register TimeZone service
-builder.Services.AddScoped<ITimeZoneComponentService, TimeZoneComponentService>();
+// register Feature services
+builder.Services.AddScoped<IAppointmentService, AppointmentService>();
+builder.Services.AddScoped<IAppointmentTypeService, AppointmentTypeService>();
+builder.Services.AddScoped<IClientService, ClientService>();
+builder.Services.AddScoped<IClinicService, ClinicService>();
+builder.Services.AddScoped<IDoctorService, DoctorService>();
+builder.Services.AddScoped<IRoomService, RoomService>();
+builder.Services.AddScoped<ISpinnerService, SpinnerService>();
+builder.Services.AddScoped<IUserSettingsService, UserSettingsService>();
 
 // register Cookie settings
 builder.Services.AddSingleton<ICookieSettings>(builder.Configuration.GetSection(typeof(CookieSettings).Name).Get<CookieSettings>()!);
 
-// register User settings service
+// register Spinner component
+builder.Services.AddScoped<ISpinnerComponentService, SpinnerComponentService>();
+
+// register TimeZone component
+builder.Services.AddSingleton<ITimeZoneComponentData>(builder.Configuration.GetSection(typeof(TimeZoneComponentData).Name).Get<TimeZoneComponentData>()!);
+builder.Services.AddScoped<ITimeZoneComponentService, TimeZoneComponentService>();
+
+// register UserSettings component
 builder.Services.AddScoped<IUserSettingsComponentService, UserSettingsComponentService>();
 
 // register BlazoredLocalStorage
 builder.Services.AddBlazoredLocalStorage();
+
+// register Radzen services
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
 
 await builder.Build().RunAsync();
