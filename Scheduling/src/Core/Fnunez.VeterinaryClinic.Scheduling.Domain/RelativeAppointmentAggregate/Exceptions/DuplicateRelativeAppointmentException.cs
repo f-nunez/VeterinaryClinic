c7461988b0c3ebbae3c11ae69c37620d28d876c5
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-08 20:14:27 -0800

    Remove RelativeAppointment aggregate from Scheduling.Domain #5

--- Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Domain/RelativeAppointmentAggregate/Exceptions/DuplicateRelativeAppointmentException.cs
+++
@@ -1,9 +0,0 @@
-namespace Fnunez.VeterinaryClinic.Scheduling.Domain.RelativeAppointmentAggregate.Exceptions;
-
-public class DuplicateRelativeAppointmentException : ArgumentException
-{
-    public DuplicateRelativeAppointmentException(string message, string paramName)
-        : base(message, paramName)
-    {
-    }
-}
