5f1dd55b1fbbd25fc5f6423df9abadc66c7c6d8a
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-12 00:05:59 -0800

    AppointmentDetailDto in Scheduling.Application.SharedModel #5

--- Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel/Appointment/GetAppointmentDetail/AppointmentDetailDto.cs
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel/Appointment/GetAppointmentDetail/AppointmentDetailDto.cs
@@ -3,20 +3,15 @@ namespace Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Appointment
 public class AppointmentDetailDto
 {
     public Guid AppointmentId { get; set; }
-    public int AppointmentTypeId { get; set; }
-    public int ClientId { get; set; }
+    public string AppointmentTypeName { get; set; } = string.Empty;
     public string ClientFullName { get; set; } = string.Empty;
-    public int ClinicId { get; set; }
     public string ClinicName { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    public int DoctorId { get; set; }
     public string DoctorFullName { get; set; } = string.Empty;
+    public DateTimeOffset EndOn { get; set; }
     public bool IsConfirmed { get; set; }
-    public int PatientId { get; set; }
     public string PatientName { get; set; } = string.Empty;
-    public int RoomId { get; set; }
     public string RoomName { get; set; } = string.Empty;
     public string Title { get; set; } = string.Empty;
     public DateTimeOffset StartOn { get; set; }
-    public DateTimeOffset EndOn { get; set; }
 }
