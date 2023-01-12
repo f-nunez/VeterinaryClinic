ea8aaa47a9145cc993b1cccff2b0b44575b7ceab
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-11 23:35:18 -0800

    GetAppointmentEditResponse in Scheduling.Application.SharedModel #5

---
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel/Appointment/GetAppointmentEdit/GetAppointmentEditResponse.cs
@@ -0,0 +1,19 @@
+using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Appointment.GetAppointmentsFilterAppointmentType;
+using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Appointment.GetAppointmentsFilterDoctor;
+using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Appointment.GetAppointmentsFilterRoom;
+using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Common;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Appointment.GetAppointmentEdit;
+
+public class GetAppointmentEditResponse : BaseResponse
+{
+    public AppointmentDto? Appointment { get; set; }
+    public List<AppointmentTypeFilterValueDto> AppointmentTypeFilterValues { get; set; } = new();
+    public List<DoctorFilterValueDto> DoctorFilterValues { get; set; } = new();
+    public List<RoomFilterValueDto> RoomFilterValues { get; set; } = new();
+
+    public GetAppointmentEditResponse(Guid correlationId)
+        : base(correlationId)
+    {
+    }
+}
