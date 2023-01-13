4230668ab59e75fd88bf3bed698d417a2f1538fc
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-13 11:46:36 -0800

    Add Clinic entity into database context in ClinicManagement.Infrastructure #17

--- ClinicManagement/src/Infrastructure/Fnunez.VeterinaryClinic.ClinicManagement.Infrastructure/Persistence/Contexts/ApplicationDbContext.cs
+++ ClinicManagement/src/Infrastructure/Fnunez.VeterinaryClinic.ClinicManagement.Infrastructure/Persistence/Contexts/ApplicationDbContext.cs
@@ -2,6 +2,7 @@ using System.Reflection;
 using Fnunez.VeterinaryClinic.ClinicManagement.Domain.AppointmentTypeAggregate;
 using Fnunez.VeterinaryClinic.ClinicManagement.Domain.ClientAggregate;
 using Fnunez.VeterinaryClinic.ClinicManagement.Domain.ClientAggregate.Entities;
+using Fnunez.VeterinaryClinic.ClinicManagement.Domain.ClinicAggregate;
 using Fnunez.VeterinaryClinic.ClinicManagement.Domain.DoctorAggregate;
 using Fnunez.VeterinaryClinic.ClinicManagement.Domain.RoomAggregate;
 using Fnunez.VeterinaryClinic.SharedKernel.Domain.Common;
@@ -24,6 +25,7 @@ public class ApplicationDbContext : DbContext
 
     public DbSet<AppointmentType> AppointmentTypes => Set<AppointmentType>();
     public DbSet<Client> Clients => Set<Client>();
+    public DbSet<Clinic> Clinics => Set<Clinic>();
     public DbSet<Doctor> Doctors => Set<Doctor>();
     public DbSet<Patient> Patients => Set<Patient>();
     public DbSet<Room> Rooms => Set<Room>();
