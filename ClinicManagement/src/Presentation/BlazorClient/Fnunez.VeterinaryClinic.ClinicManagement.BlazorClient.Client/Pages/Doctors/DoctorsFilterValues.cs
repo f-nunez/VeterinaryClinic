c348805fc26e06c67bb7efd399425da5702cbeef
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-13 16:56:32 -0800

    Doctors module in ClinicManagement.BlazorClient.Client #17

---
+++ ClinicManagement/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client/Pages/Doctors/DoctorsFilterValues.cs
@@ -0,0 +1,7 @@
+namespace Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client.Pages.Doctors;
+
+public class DoctorsFilterValues
+{
+    public string FullNameFilterValue { get; set; } = null!;
+    public string IdFilterValue { get; set; } = null!;
+}
