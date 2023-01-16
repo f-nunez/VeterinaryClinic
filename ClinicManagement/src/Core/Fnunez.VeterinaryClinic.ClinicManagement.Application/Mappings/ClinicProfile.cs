84130729b193f28698a6409477d81aa6c27f5e96
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-16 15:30:55 -0800

    Add mappers into ClinicProfile in ClinicManagement.Application #17

--- ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Mappings/ClinicProfile.cs
+++ ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Mappings/ClinicProfile.cs
@@ -1,5 +1,8 @@
 using AutoMapper;
 using Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel.Clinic;
+using Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel.Clinic.CreateClinic;
+using Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel.Clinic.DeleteClinic;
+using Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel.Clinic.UpdateClinic;
 using Fnunez.VeterinaryClinic.ClinicManagement.Domain.ClinicAggregate;
 
 namespace Fnunez.VeterinaryClinic.ClinicManagement.Application.Mappings;
@@ -9,5 +12,13 @@ public class ClinicProfile : Profile
     public ClinicProfile()
     {
         CreateMap<Clinic, ClinicDto>();
+
+        CreateMap<ClinicDto, Clinic>();
+
+        CreateMap<CreateClinicRequest, Clinic>();
+
+        CreateMap<DeleteClinicRequest, Clinic>();
+
+        CreateMap<UpdateClinicRequest, Clinic>();
     }
 }
