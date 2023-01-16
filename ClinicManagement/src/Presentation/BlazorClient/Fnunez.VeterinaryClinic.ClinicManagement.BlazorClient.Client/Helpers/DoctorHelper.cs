6f527032932177c9c154aebc97a08c06d47fb2cb
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-16 12:00:46 -0800

    DoctorHelper in ClinicManagement.BlazorClient.Client #17

---
+++ ClinicManagement/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client/Helpers/DoctorHelper.cs
@@ -0,0 +1,16 @@
+using Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel.Doctor;
+using Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client.ViewModels.Doctors;
+
+namespace Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client.Helpers;
+
+public static class DoctorHelper
+{
+    public static DoctorVm MapDoctorViewModel(DoctorDto doctorDto)
+    {
+        return new DoctorVm
+        {
+            FullName = doctorDto.FullName,
+            Id = doctorDto.Id
+        };
+    }
+}
