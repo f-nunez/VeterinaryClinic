18f19034e20c650b1059606124c791fe03995805
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-15 16:20:21 -0800

    ClientDetail component page in ClinicManagement.BlazorClient.Client #17

---
+++ ClinicManagement/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client/Pages/Clients/ClientDetail.razor.designer.cs
@@ -0,0 +1,23 @@
+using Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client.ViewModels.Clients;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using Radzen;
+
+namespace Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client.Pages.Clients;
+
+public partial class ClientDetailComponent : ComponentBase
+{
+    [Inject]
+    private DialogService _dialogService { get; set; }
+
+    [Inject]
+    protected IStringLocalizer<ClientDetailComponent> StringLocalizer { get; set; }
+
+    [Parameter]
+    public ClientDetailVm Model { get; set; }
+
+    protected void OnClickAccept()
+    {
+        _dialogService.Close();
+    }
+}
