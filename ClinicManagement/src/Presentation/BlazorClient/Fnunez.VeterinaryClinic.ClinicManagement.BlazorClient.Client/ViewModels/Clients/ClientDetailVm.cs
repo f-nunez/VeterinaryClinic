b8ee9a7d076603b29c17cc6ced43db3449577871
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-15 16:02:16 -0800

    ClientDetailVm in ClinicManagement.BlazorClient.Client #17

---
+++ ClinicManagement/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client/ViewModels/Clients/ClientDetailVm.cs
@@ -0,0 +1,11 @@
+namespace Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client.ViewModels.Clients;
+
+public class ClientDetailVm
+{
+    public int ClientId { get; set; }
+    public string EmailAddress { get; set; } = string.Empty;
+    public string FullName { get; set; } = string.Empty;
+    public string PreferredDoctorFullName { get; set; } = string.Empty;
+    public string PreferredName { get; set; } = string.Empty;
+    public string Salutation { get; set; } = string.Empty;
+}
