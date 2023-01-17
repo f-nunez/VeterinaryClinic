bab00a2e76a745ba55c5ef76d3616471c5195b31
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-16 17:30:34 -0800

    RoomHelper in ClinicManagement.BlazorClient.Client #17

---
+++ ClinicManagement/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client/Helpers/RoomHelper.cs
@@ -0,0 +1,16 @@
+using Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel.Room;
+using Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client.ViewModels.Rooms;
+
+namespace Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client.Helpers;
+
+public static class RoomHelper
+{
+    public static RoomVm MapRoomViewModel(RoomDto roomDto)
+    {
+        return new RoomVm
+        {
+            Id = roomDto.Id,
+            Name = roomDto.Name
+        };
+    }
+}
