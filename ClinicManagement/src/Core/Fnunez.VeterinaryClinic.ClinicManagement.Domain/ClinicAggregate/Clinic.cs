eebf6e0530a59c2bd49ea987a3186a595ae81506
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-17 16:46:38 -0800

    Validate Clinic's model constructor values in ClinicManagement.Domain #17

--- ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Domain/ClinicAggregate/Clinic.cs
+++ ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Domain/ClinicAggregate/Clinic.cs
@@ -17,6 +17,49 @@ public class Clinic : BaseEntity<int>, IAggregateRoot
 
     public Clinic(string address, string emailAddress, string name)
     {
+        if (string.IsNullOrEmpty(address))
+            throw new ArgumentException(
+                $"Required input {nameof(address)} was empty.",
+                nameof(address));
+
+        if (string.IsNullOrEmpty(emailAddress))
+            throw new ArgumentException(
+                $"Required input {nameof(emailAddress)} was empty.",
+                nameof(emailAddress));
+
+        if (string.IsNullOrEmpty(name))
+            throw new ArgumentException(
+                $"Required input {nameof(name)} was empty.",
+                nameof(name));
+
+        Address = address;
+        EmailAddress = emailAddress;
+        Name = name;
+    }
+
+    public Clinic(int id, string address, string emailAddress, string name)
+    {
+        if (id <= 0)
+            throw new ArgumentException(
+                $"Required input {nameof(id)} cannot be zero or negative.",
+                nameof(id));
+
+        if (string.IsNullOrEmpty(address))
+            throw new ArgumentException(
+                $"Required input {nameof(address)} was empty.",
+                nameof(address));
+
+        if (string.IsNullOrEmpty(emailAddress))
+            throw new ArgumentException(
+                $"Required input {nameof(emailAddress)} was empty.",
+                nameof(emailAddress));
+
+        if (string.IsNullOrEmpty(name))
+            throw new ArgumentException(
+                $"Required input {nameof(name)} was empty.",
+                nameof(name));
+
+        Id = id;
         Address = address;
         EmailAddress = emailAddress;
         Name = name;
