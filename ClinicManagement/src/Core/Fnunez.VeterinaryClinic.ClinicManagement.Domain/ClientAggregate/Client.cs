73773437049ee9cbda20202c5018e001308b911e
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-17 16:39:45 -0800

    Change PreferredDoctorId has nullable into Client model in ClinicManagement.Domain #17

--- ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Domain/ClientAggregate/Client.cs
+++ ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Domain/ClientAggregate/Client.cs
@@ -11,7 +11,7 @@ public class Client : BaseEntity<int>, IAggregateRoot
     public string PreferredName { get; private set; }
     public string Salutation { get; private set; }
     public string EmailAddress { get; private set; }
-    public int PreferredDoctorId { get; private set; }
+    public int? PreferredDoctorId { get; private set; }
     public IReadOnlyList<Patient> Patients => _patients.AsReadOnly();
 
     #region Navigations
@@ -31,7 +31,7 @@ public class Client : BaseEntity<int>, IAggregateRoot
         string preferredName,
         string salutation,
         string emailAddress,
-        int preferredDoctorId)
+        int? preferredDoctorId)
     {
         if (string.IsNullOrEmpty(fullName))
             throw new ArgumentException(
@@ -53,7 +53,52 @@ public class Client : BaseEntity<int>, IAggregateRoot
                 $"Required input {nameof(emailAddress)} was empty.",
                 nameof(emailAddress));
 
-        if (preferredDoctorId <= 0)
+        if (preferredDoctorId != null && preferredDoctorId <= 0)
+            throw new ArgumentException(
+                $"Required input {nameof(preferredDoctorId)} cannot be zero or negative.",
+                nameof(preferredDoctorId));
+
+        FullName = fullName;
+        PreferredName = preferredName;
+        Salutation = salutation;
+        EmailAddress = emailAddress;
+        PreferredDoctorId = preferredDoctorId;
+    }
+
+    public Client(
+        int id,
+        string fullName,
+        string preferredName,
+        string salutation,
+        string emailAddress,
+        int? preferredDoctorId)
+    {
+        if (id <= 0)
+            throw new ArgumentException(
+                $"Required input {nameof(id)} cannot be zero or negative.",
+                nameof(id));
+
+        if (string.IsNullOrEmpty(fullName))
+            throw new ArgumentException(
+                $"Required input {nameof(fullName)} was empty.",
+                nameof(fullName));
+
+        if (string.IsNullOrEmpty(preferredName))
+            throw new ArgumentException(
+                $"Required input {nameof(preferredName)} was empty.",
+                nameof(preferredName));
+
+        if (string.IsNullOrEmpty(salutation))
+            throw new ArgumentException(
+                $"Required input {nameof(salutation)} was empty.",
+                nameof(salutation));
+
+        if (string.IsNullOrEmpty(emailAddress))
+            throw new ArgumentException(
+                $"Required input {nameof(emailAddress)} was empty.",
+                nameof(emailAddress));
+
+        if (preferredDoctorId != null && preferredDoctorId <= 0)
             throw new ArgumentException(
                 $"Required input {nameof(preferredDoctorId)} cannot be zero or negative.",
                 nameof(preferredDoctorId));
