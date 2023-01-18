75583d57761224cb1ebe55464b5fb1fca51aab51
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-17 16:46:28 -0800

    Validate AppointmentType's model constructor values in ClinicManagement.Domain #17

--- ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Domain/AppointmentTypeAggregate/AppointmentType.cs
+++ ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Domain/AppointmentTypeAggregate/AppointmentType.cs
@@ -19,6 +19,21 @@ public class AppointmentType : BaseEntity<int>, IAggregateRoot
         string code,
         int duration)
     {
+        if (string.IsNullOrEmpty(name))
+            throw new ArgumentException(
+                $"Required input {nameof(name)} was empty.",
+                nameof(name));
+
+        if (string.IsNullOrEmpty(code))
+            throw new ArgumentException(
+                $"Required input {nameof(code)} was empty.",
+                nameof(code));
+
+        if (duration <= 0)
+            throw new ArgumentException(
+                $"Required input {nameof(duration)} cannot be zero or negative.",
+                nameof(duration));
+
         Name = name;
         Code = code;
         Duration = duration;
@@ -30,6 +45,26 @@ public class AppointmentType : BaseEntity<int>, IAggregateRoot
         string code,
         int duration)
     {
+        if (id <= 0)
+            throw new ArgumentException(
+                $"Required input {nameof(id)} cannot be zero or negative.",
+                nameof(id));
+
+        if (string.IsNullOrEmpty(name))
+            throw new ArgumentException(
+                $"Required input {nameof(name)} was empty.",
+                nameof(name));
+
+        if (string.IsNullOrEmpty(code))
+            throw new ArgumentException(
+                $"Required input {nameof(code)} was empty.",
+                nameof(code));
+
+        if (duration <= 0)
+            throw new ArgumentException(
+                $"Required input {nameof(duration)} cannot be zero or negative.",
+                nameof(duration));
+
         Id = id;
         Name = name;
         Code = code;
