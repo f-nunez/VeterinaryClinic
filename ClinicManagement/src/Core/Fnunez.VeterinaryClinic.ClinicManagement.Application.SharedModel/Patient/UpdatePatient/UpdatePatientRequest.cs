31d49d16482cb6ab4319f0ef84bac117b44c9d3d
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-20 19:01:25 -0800

    UpdatePatientRequest in ClinicManagement.Application.SharedModel #17

--- ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel/Patient/UpdatePatient/UpdatePatientRequest.cs
+++ ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel/Patient/UpdatePatient/UpdatePatientRequest.cs
@@ -4,8 +4,14 @@ namespace Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel.Patie
 
 public class UpdatePatientRequest : BaseRequest
 {
+    public string Breed { get; set; } = string.Empty;
     public int ClientId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public bool IsNewPhoto { get; set; }
     public int PatientId { get; set; }
-    public string PatientName { get; set; } = string.Empty;
+    public byte[] PhotoData { get; set; } = null!;
+    public string PhotoName { get; set; } = string.Empty;
     public int? PreferredDoctorId { get; set; }
+    public int Sex { get; set; }
+    public string Species { get; set; } = string.Empty;
 }
