564c71689687bd6974da9899ec763c5ba2fc0f74
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-20 19:13:06 -0800

    Read Patient's photo on GetPatientsQuery in ClinicManagement.Application #17

--- ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Features/Patients/Queries/GetPatients/GetPatientsQueryHandler.cs
+++ ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Features/Patients/Queries/GetPatients/GetPatientsQueryHandler.cs
@@ -1,9 +1,10 @@
 using AutoMapper;
 using Fnunez.VeterinaryClinic.ClinicManagement.Application.Common.Exceptions;
-using Fnunez.VeterinaryClinic.ClinicManagement.Application.Features.Patients.Commands.CreatePatient;
-using Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel.Patient;
+using Fnunez.VeterinaryClinic.ClinicManagement.Application.Interfaces.Services;
+using Fnunez.VeterinaryClinic.ClinicManagement.Application.Interfaces.Settings;
 using Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel.Patient.GetPatients;
 using Fnunez.VeterinaryClinic.ClinicManagement.Domain.ClientAggregate;
+using Fnunez.VeterinaryClinic.ClinicManagement.Domain.ClientAggregate.Entities;
 using Fnunez.VeterinaryClinic.SharedKernel.Application.Repositories;
 using MediatR;
 
@@ -11,11 +12,19 @@ namespace Fnunez.VeterinaryClinic.ClinicManagement.Application.Features.Patients
 
 public class GetPatientsQueryHandler : IRequestHandler<GetPatientsQuery, GetPatientsResponse>
 {
+    private readonly IClientStorageSetting _clientStorageSetting;
+    private readonly IFileSystemReaderService _fileSystemReaderService;
     private readonly IMapper _mapper;
     private readonly IUnitOfWork _unitOfWork;
 
-    public GetPatientsQueryHandler(IMapper mapper, IUnitOfWork unitOfWork)
+    public GetPatientsQueryHandler(
+        IClientStorageSetting clientStorageSetting,
+        IFileSystemReaderService fileSystemReaderService,
+        IMapper mapper,
+        IUnitOfWork unitOfWork)
     {
+        _clientStorageSetting = clientStorageSetting;
+        _fileSystemReaderService = fileSystemReaderService;
         _mapper = mapper;
         _unitOfWork = unitOfWork;
     }
@@ -26,9 +35,11 @@ public class GetPatientsQueryHandler : IRequestHandler<GetPatientsQuery, GetPati
     {
         GetPatientsRequest request = query.GetPatientsRequest;
         var response = new GetPatientsResponse(request.CorrelationId);
-        var specification = new ClientByIdIncludePatientsSpecification(request.ClientId);
 
-        var client = await _unitOfWork.ReadRepository<Client>()
+        var specification = new ClientByIdSpecification(request.ClientId);
+
+        var client = await _unitOfWork
+            .ReadRepository<Client>()
             .FirstOrDefaultAsync(specification, cancellationToken);
 
         if (client is null)
@@ -37,9 +48,40 @@ public class GetPatientsQueryHandler : IRequestHandler<GetPatientsQuery, GetPati
         if (client.Patients is null)
             return response;
 
-        response.Patients = _mapper.Map<List<PatientDto>>(client.Patients);
         response.Count = response.Patients.Count;
 
+        response.Patients = await MapPatientsDtos(client.Patients);
+
         return response;
+    }
+
+    private async Task<List<PatientsDto>> MapPatientsDtos(
+        IReadOnlyList<Patient> patients)
+    {
+        var patientsDtos = new List<PatientsDto>();
+
+        foreach (Patient patient in patients)
+        {
+            string relativePhotoPath = Path.Combine(
+                patient.ClientId.ToString(), patient.Photo.StoredName);
+
+            string photoPath = Path.Combine(
+                _clientStorageSetting.BasePath, relativePhotoPath);
+
+            var patientsDto = new PatientsDto
+            {
+                ClientId = patient.ClientId,
+                Name = patient.Name,
+                PatientId = patient.Id,
+                PhotoData = await _fileSystemReaderService.ReadAsync(photoPath),
+                PhotoName = patient.Photo.Name,
+                Sex = (int)patient.AnimalSex,
+                Species = patient.AnimalType.Species
+            };
+
+            patientsDtos.Add(patientsDto);
+        }
+
+        return patientsDtos;
     }
 }
