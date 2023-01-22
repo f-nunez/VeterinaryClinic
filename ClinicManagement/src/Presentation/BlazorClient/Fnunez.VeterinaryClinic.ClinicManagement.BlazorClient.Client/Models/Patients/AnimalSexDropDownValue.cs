885fbfedaff777cadd558dc20457ba387dd28a2f
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-21 23:17:01 -0800

    AnimalSex DropDown value in ClinicManagement.BlazorClient.Client #17

---
+++ ClinicManagement/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client/Models/Patients/AnimalSexDropDownValue.cs
@@ -0,0 +1,7 @@
+namespace Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client.Models.Patients;
+
+public class AnimalSexDropDownValue
+{
+    public string Text { get; set; } = string.Empty;
+    public int Value { get; set; }
+}
