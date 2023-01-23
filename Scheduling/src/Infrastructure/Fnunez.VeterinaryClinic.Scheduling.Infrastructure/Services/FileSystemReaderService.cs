de1d7c6a0a89d75a15d4680606026514003386e6
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-23 14:57:39 -0800

    FileSystemReaderService into appsettings in Scheduling.Infrastructure #19

---
+++ Scheduling/src/Infrastructure/Fnunez.VeterinaryClinic.Scheduling.Infrastructure/Services/FileSystemReaderService.cs
@@ -0,0 +1,24 @@
+using Fnunez.VeterinaryClinic.Scheduling.Application.Interfaces.Services;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.Infrastructure.Services;
+
+public class FileSystemReaderService : IFileSystemReaderService
+{
+    public async Task<byte[]> ReadAsync(string filePath)
+    {
+        if (string.IsNullOrEmpty(filePath))
+            throw new ArgumentException($"{nameof(filePath)} is empty.");
+
+        if (!File.Exists(filePath))
+            throw new ArgumentException($"{filePath} not found.");
+
+        var memoryStream = new MemoryStream();
+
+        using (var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            await fileStream.CopyToAsync(memoryStream);
+
+        memoryStream.Position = default;
+
+        return memoryStream.ToArray();
+    }
+}
