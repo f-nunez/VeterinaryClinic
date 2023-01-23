2e08d51660e75401b9cc79e780e25e59deb7825d
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-23 14:08:33 -0800

    Remove business validation from synced DoctorAggregate in Scheduling.Domain #19

--- Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Domain/SyncedAggregates/DoctorAggregate/Doctor.cs
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Domain/SyncedAggregates/DoctorAggregate/Doctor.cs
@@ -11,17 +11,6 @@ public class Doctor : BaseEntity<int>, IAggregateRoot
         FullName = string.Empty;
     }
 
-    public Doctor(string fullName)
-    {
-        FullName = fullName;
-    }
-
-    public Doctor(int id, string fullName)
-    {
-        Id = id;
-        FullName = fullName;
-    }
-
     public override string ToString()
     {
         return FullName.ToString();
