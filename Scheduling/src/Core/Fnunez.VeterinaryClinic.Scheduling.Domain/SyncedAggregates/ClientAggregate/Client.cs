5fc7d1796a6a833514c4ff42ffedb98ccbb132f4
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-23 14:11:18 -0800

    Remove business validation from synced ClientAggregate in Scheduling.Domain #19

--- Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Domain/SyncedAggregates/ClientAggregate/Client.cs
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Domain/SyncedAggregates/ClientAggregate/Client.cs
@@ -21,61 +21,6 @@ public class Client : BaseEntity<int>, IAggregateRoot
         EmailAddress = string.Empty;
     }
 
-    public Client(
-        string fullName,
-        string preferredName,
-        string salutation,
-        string emailAddress,
-        int preferredDoctorId)
-    {
-        if (string.IsNullOrEmpty(fullName))
-            throw new ArgumentException(
-                $"Required input {nameof(fullName)} was empty.",
-                nameof(fullName));
-
-        if (string.IsNullOrEmpty(preferredName))
-            throw new ArgumentException(
-                $"Required input {nameof(preferredName)} was empty.",
-                nameof(preferredName));
-
-        if (string.IsNullOrEmpty(salutation))
-            throw new ArgumentException(
-                $"Required input {nameof(salutation)} was empty.",
-                nameof(salutation));
-
-        if (string.IsNullOrEmpty(emailAddress))
-            throw new ArgumentException(
-                $"Required input {nameof(emailAddress)} was empty.",
-                nameof(emailAddress));
-
-        if (preferredDoctorId <= 0)
-            throw new ArgumentException(
-                $"Required input {nameof(preferredDoctorId)} cannot be zero or negative.",
-                nameof(preferredDoctorId));
-
-        FullName = fullName;
-        PreferredName = preferredName;
-        Salutation = salutation;
-        EmailAddress = emailAddress;
-        PreferredDoctorId = preferredDoctorId;
-    }
-
-    public void AddPatient(Patient patient)
-    {
-        if (patient is null)
-            throw new ArgumentNullException(nameof(patient));
-
-        _patients.Add(patient);
-    }
-
-    public void RemovePatient(Patient patient)
-    {
-        if (patient is null)
-            throw new ArgumentNullException(nameof(patient));
-
-        _patients.Remove(patient);
-    }
-
     public override string ToString()
     {
         return FullName.ToString();
