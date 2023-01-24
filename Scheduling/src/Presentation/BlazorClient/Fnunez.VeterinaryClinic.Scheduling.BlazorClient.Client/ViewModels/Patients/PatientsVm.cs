8c7902c94ab13e50e025083a4f131cc649b703f4
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-23 20:49:34 -0800

    PatientsVm in Scheduling.BlazorClient.Client #19

---
+++ Scheduling/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client/ViewModels/Patients/PatientsVm.cs
@@ -0,0 +1,14 @@
+using Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client.Models.Patients;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client.ViewModels.Patients;
+
+public class PatientsVm
+{
+    public int ClientId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int PatientId { get; set; }
+    public string PhotoBase64Encoded { get; set; } = string.Empty;
+    public string PhotoName { get; set; } = string.Empty;
+    public AnimalSex Sex { get; set; }
+    public string Species { get; set; } = string.Empty;
+}
