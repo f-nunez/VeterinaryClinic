ddc3a913c63f31e0faf2d286b13eda9303b8b6f2
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-23 22:27:21 -0800

    DoctorDetail component page in Scheduling.BlazorClient.Client #19

---
+++ Scheduling/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client/Pages/Doctors/DoctorDetail.razor.designer.cs
@@ -0,0 +1,23 @@
+using Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client.ViewModels.Doctors;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using Radzen;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client.Pages.Doctors;
+
+public partial class DoctorDetailComponent : ComponentBase
+{
+    [Inject]
+    private DialogService _dialogService { get; set; }
+
+    [Inject]
+    protected IStringLocalizer<DoctorDetailComponent> StringLocalizer { get; set; }
+
+    [Parameter]
+    public DoctorVm Model { get; set; }
+
+    protected void OnClickAccept()
+    {
+        _dialogService.Close();
+    }
+}
