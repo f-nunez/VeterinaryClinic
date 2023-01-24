beddf8398f68601b6d0b0529cacb8fc308f73e6b
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-24 12:07:23 -0800

    Add GetAppointmentAddAsync method into AppointmentService in Scheduling.BlazorClient.Client #19

--- Scheduling/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client/Services/AppointmentService/IAppointmentService.cs
+++ Scheduling/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client/Services/AppointmentService/IAppointmentService.cs
@@ -1,6 +1,7 @@
 using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Appointment;
 using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Appointment.CreateAppointment;
 using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Appointment.DeleteAppointment;
+using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Appointment.GetAppointmentAdd;
 using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Appointment.GetAppointmentDetail;
 using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Appointment.GetAppointmentEdit;
 using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Appointment.GetAppointments;
@@ -26,6 +27,7 @@ public interface IAppointmentService
     public Task<List<PatientFilterValueDto>> DataGridFilterPatientAsync(GetAppointmentsFilterPatientRequest request);
     public Task<DataGridResponse<RoomFilterValueDto>> DataGridFilterRoomAsync(GetAppointmentsFilterRoomRequest request);
     public Task<DeleteAppointmentResponse> DeleteAppointmentAsync(DeleteAppointmentRequest request);
+    public Task<GetAppointmentAddResponse> GetAppointmentAddAsync(GetAppointmentAddRequest request);
     public Task<GetAppointmentDetailResponse> GetAppointmentDetailAsync(GetAppointmentDetailRequest request);
     public Task<GetAppointmentEditResponse> GetAppointmentEditAsync(GetAppointmentEditRequest request);
     public Task<UpdateAppointmentResponse> UpdateAppointmentAsync(UpdateAppointmentRequest request);
