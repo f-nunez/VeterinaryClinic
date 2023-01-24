0fef17c0231c1109c64cf3a26b936b63588dcce9
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-23 22:26:12 -0800

    DoctorVm in Scheduling.BlazorClient.Client #19

---
+++ Scheduling/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client/ViewModels/Doctors/DoctorVm.cs
@@ -0,0 +1,7 @@
+namespace Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Client.ViewModels.Doctors;
+
+public class DoctorVm
+{
+    public string FullName { get; set; } = string.Empty;
+    public int Id { get; set; }
+}
