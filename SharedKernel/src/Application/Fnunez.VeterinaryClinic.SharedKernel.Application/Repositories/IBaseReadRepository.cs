5e05c3bb46b105eb51730b60b25c7ae042c5957e
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-26 19:17:27 -0800

    CodeSmells at IBaseReadRepository in SharedKernel.Application #19

--- SharedKernel/src/Application/Fnunez.VeterinaryClinic.SharedKernel.Application/Repositories/IBaseReadRepository.cs
+++ SharedKernel/src/Application/Fnunez.VeterinaryClinic.SharedKernel.Application/Repositories/IBaseReadRepository.cs
@@ -5,17 +5,48 @@ namespace Fnunez.VeterinaryClinic.SharedKernel.Application.Repositories;
 public interface IBaseReadRepository<T> where T : class
 {
     /// <summary>
-    /// Finds an entity with the given primary key value.
+    /// Returns a boolean that represents whether any entity satisfy the encapsulated query logic
+    /// of the <paramref name="specification"/> or not.
     /// </summary>
-    /// <typeparam name="TId">The type of primary key.</typeparam>
-    /// <param name="id">The value of the primary key for the entity to be found.</param>
+    /// <param name="specification">The encapsulated query logic.</param>
     /// <returns>
-    /// A task that represents the asynchronous operation.
-    /// The task result contains the <typeparamref name="T" />, or <see langword="null"/>.
+    /// A task that represents the asynchronous operation. The task result contains true if the 
+    /// source sequence contains any elements; otherwise, false.
     /// </returns>
-    Task<T?> GetByIdAsync<TId>(
-        TId id,
-        CancellationToken cancellationToken = default) where TId : notnull;
+    Task<bool> AnyAsync(
+        ISpecification<T> specification,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Returns a boolean whether any entity exists or not.
+    /// </summary>
+    /// <returns>
+    /// A task that represents the asynchronous operation. The task result contains true if the 
+    /// source sequence contains any elements; otherwise, false.
+    /// </returns>
+    Task<bool> AnyAsync(CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Returns a number that represents how many entities satisfy the encapsulated query logic
+    /// of the <paramref name="specification"/>.
+    /// </summary>
+    /// <param name="specification">The encapsulated query logic.</param>
+    /// <returns>
+    /// A task that represents the asynchronous operation. The task result contains the
+    /// number of elements in the input sequence.
+    /// </returns>
+    Task<int> CountAsync(
+        ISpecification<T> specification,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Returns the total number of records.
+    /// </summary>
+    /// <returns>
+    /// A task that represents the asynchronous operation. The task result contains the
+    /// number of elements in the input sequence.
+    /// </returns>
+    Task<int> CountAsync(CancellationToken cancellationToken = default);
 
     /// <summary>
     /// Returns the first element of a sequence, or a default value if the sequence contains no elements.
@@ -42,6 +73,19 @@ public interface IBaseReadRepository<T> where T : class
     Task<TResult?> FirstOrDefaultAsync<TResult>(
         ISpecification<T, TResult> specification,
         CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Finds an entity with the given primary key value.
+    /// </summary>
+    /// <typeparam name="TId">The type of primary key.</typeparam>
+    /// <param name="id">The value of the primary key for the entity to be found.</param>
+    /// <returns>
+    /// A task that represents the asynchronous operation.
+    /// The task result contains the <typeparamref name="T" />, or <see langword="null"/>.
+    /// </returns>
+    Task<T?> GetByIdAsync<TId>(
+        TId id,
+        CancellationToken cancellationToken = default) where TId : notnull;
 
     /// <summary>
     /// Finds all entities of <typeparamref name="T" /> from the database.
@@ -81,48 +125,4 @@ public interface IBaseReadRepository<T> where T : class
     Task<List<TResult>> ListAsync<TResult>(
         ISpecification<T, TResult> specification,
         CancellationToken cancellationToken = default);
-
-    /// <summary>
-    /// Returns a number that represents how many entities satisfy the encapsulated query logic
-    /// of the <paramref name="specification"/>.
-    /// </summary>
-    /// <param name="specification">The encapsulated query logic.</param>
-    /// <returns>
-    /// A task that represents the asynchronous operation. The task result contains the
-    /// number of elements in the input sequence.
-    /// </returns>
-    Task<int> CountAsync(
-        ISpecification<T> specification,
-        CancellationToken cancellationToken = default);
-
-    /// <summary>
-    /// Returns the total number of records.
-    /// </summary>
-    /// <returns>
-    /// A task that represents the asynchronous operation. The task result contains the
-    /// number of elements in the input sequence.
-    /// </returns>
-    Task<int> CountAsync(CancellationToken cancellationToken = default);
-
-    /// <summary>
-    /// Returns a boolean that represents whether any entity satisfy the encapsulated query logic
-    /// of the <paramref name="specification"/> or not.
-    /// </summary>
-    /// <param name="specification">The encapsulated query logic.</param>
-    /// <returns>
-    /// A task that represents the asynchronous operation. The task result contains true if the 
-    /// source sequence contains any elements; otherwise, false.
-    /// </returns>
-    Task<bool> AnyAsync(
-        ISpecification<T> specification,
-        CancellationToken cancellationToken = default);
-
-    /// <summary>
-    /// Returns a boolean whether any entity exists or not.
-    /// </summary>
-    /// <returns>
-    /// A task that represents the asynchronous operation. The task result contains true if the 
-    /// source sequence contains any elements; otherwise, false.
-    /// </returns>
-    Task<bool> AnyAsync(CancellationToken cancellationToken = default);
 }
