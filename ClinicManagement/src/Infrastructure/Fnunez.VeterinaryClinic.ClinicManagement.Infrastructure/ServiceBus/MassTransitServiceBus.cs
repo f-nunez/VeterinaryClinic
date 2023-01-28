21ee7463754c3c7f1e492a83d9d763b31a09a3f6
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-27 23:23:44 -0800

    MassTransitServicebus in ClinicManagement.Infrastructure #19

---
+++ ClinicManagement/src/Infrastructure/Fnunez.VeterinaryClinic.ClinicManagement.Infrastructure/ServiceBus/MassTransitServiceBus.cs
@@ -0,0 +1,26 @@
+using Fnunez.VeterinaryClinic.ClinicManagement.Application.Interfaces.ServiceBus;
+using MassTransit;
+
+namespace Fnunez.VeterinaryClinic.ClinicManagement.Infrastructure.ServiceBus;
+
+public class MassTransitServiceBus : IServiceBus
+{
+    public IPublishEndpoint _publishEndpoint { get; set; }
+
+    public MassTransitServiceBus(IPublishEndpoint publishEndpoint)
+    {
+        _publishEndpoint = publishEndpoint;
+    }
+
+    public Task PublishAsync<TMessage>(
+        TMessage message,
+        CancellationToken cancellationToken)
+    {
+        if (message is null)
+            throw new ArgumentNullException(nameof(message));
+
+        return Task.WhenAll(
+            _publishEndpoint.Publish(message, cancellationToken)
+        );
+    }
+}
