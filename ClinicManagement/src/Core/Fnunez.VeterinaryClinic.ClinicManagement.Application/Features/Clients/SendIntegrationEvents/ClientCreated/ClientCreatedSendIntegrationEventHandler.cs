95045656437348a2efe2bb64ea070d9041dd825b
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-29 13:20:21 -0800

    ClientCreatedSendIntegrationEvent in ClinicManagement.Application #19

---
+++ ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Features/Clients/SendIntegrationEvents/ClientCreated/ClientCreatedSendIntegrationEventHandler.cs
@@ -0,0 +1,24 @@
+using Fnunez.VeterinaryClinic.ClinicManagement.Application.Interfaces.ServiceBus;
+using MediatR;
+
+namespace Fnunez.VeterinaryClinic.ClinicManagement.Application.Features.Clients.SendIntegrationEvents.ClientCreated;
+
+public class ClientCreatedSendIntegrationEventHandler
+    : INotificationHandler<ClientCreatedSendIntegrationEvent>
+{
+    private readonly IServiceBus _serviceBus;
+
+    public ClientCreatedSendIntegrationEventHandler(IServiceBus serviceBus)
+    {
+        _serviceBus = serviceBus;
+    }
+
+    public Task Handle(
+        ClientCreatedSendIntegrationEvent integrationEvent,
+        CancellationToken cancellationToken)
+    {
+        var message = integrationEvent.ClientCreatedIntegrationEventContract;
+
+        return _serviceBus.PublishAsync(message, cancellationToken);
+    }
+}
