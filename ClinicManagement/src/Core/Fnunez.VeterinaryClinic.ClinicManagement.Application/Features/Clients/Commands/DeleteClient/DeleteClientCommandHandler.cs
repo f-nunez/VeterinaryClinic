f1befecb7dea8e9ac75e94ab0eb51a0c27d023e6
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-29 13:26:32 -0800

    Send integration event from DeleteClientCommand in ClinicManagement.Application #19

--- ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Features/Clients/Commands/DeleteClient/DeleteClientCommandHandler.cs
+++ ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Features/Clients/Commands/DeleteClient/DeleteClientCommandHandler.cs
@@ -1,4 +1,6 @@
 using AutoMapper;
+using Contracts;
+using Fnunez.VeterinaryClinic.ClinicManagement.Application.Features.Clients.SendIntegrationEvents.ClientDeleted;
 using Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel.Client.DeleteClient;
 using Fnunez.VeterinaryClinic.ClinicManagement.Domain.ClientAggregate;
 using Fnunez.VeterinaryClinic.SharedKernel.Application.Repositories;
@@ -9,11 +11,16 @@ namespace Fnunez.VeterinaryClinic.ClinicManagement.Application.Features.Clients.
 public class DeleteClientCommandHandler : IRequestHandler<DeleteClientCommand, DeleteClientResponse>
 {
     private readonly IMapper _mapper;
+    private readonly IMediator _mediator;
     private readonly IUnitOfWork _unitOfWork;
 
-    public DeleteClientCommandHandler(IMapper mapper, IUnitOfWork unitOfWork)
+    public DeleteClientCommandHandler(
+        IMapper mapper,
+        IMediator mediator,
+        IUnitOfWork unitOfWork)
     {
         _mapper = mapper;
+        _mediator = mediator;
         _unitOfWork = unitOfWork;
     }
 
@@ -30,6 +37,32 @@ public class DeleteClientCommandHandler : IRequestHandler<DeleteClientCommand, D
 
         await _unitOfWork.CommitAsync(cancellationToken);
 
+        await SendIntegrationEventAsync(
+            request.Id,
+            request.CorrelationId,
+            cancellationToken
+        );
+
         return response;
+    }
+
+    private async Task SendIntegrationEventAsync(
+        int clientId,
+        Guid correlationId,
+        CancellationToken cancellationToken)
+    {
+        var message = new ClientDeletedIntegrationEventContract
+        {
+            CausationId = correlationId,
+            CorrelationId = correlationId,
+            Id = Guid.NewGuid(),
+            OccurredOn = DateTimeOffset.UtcNow,
+            ClientId = clientId
+        };
+
+        await _mediator.Publish(
+            new ClientDeletedSendIntegrationEvent(message),
+            cancellationToken
+        );
     }
 }
