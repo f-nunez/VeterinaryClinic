f9d34ad7bdcb90d7af05f153e351a7c16f02ce29
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-29 23:18:28 -0800

    RoomDeletedReceiveIntegrationEvent in Scheduling.Application #19

---
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application/Features/Rooms/ReceiveIntegrationEvents/RoomDeleted/RoomDeletedReceiveIntegrationEventHandler.cs
@@ -0,0 +1,31 @@
+using Fnunez.VeterinaryClinic.SharedKernel.Application.Repositories;
+using MediatR;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.Application.Features.Rooms.ReceiveIntegrationEvents.RoomDeleted;
+
+public class RoomDeletedReceiveIntegrationEventHandler
+    : INotificationHandler<RoomDeletedReceiveIntegrationEvent>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public RoomDeletedReceiveIntegrationEventHandler(
+        IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task Handle(
+        RoomDeletedReceiveIntegrationEvent integrationEvent,
+        CancellationToken cancellationToken)
+    {
+        var contract = integrationEvent
+            .RoomDeletedIntegrationEventContract;
+
+        string sql = @$"
+        DELETE FROM [dbo].[Rooms]
+        WHERE Id = {contract.RoomId}";
+
+        var result = await _unitOfWork
+            .ExecuteSqlCommandAsync(sql, cancellationToken);
+    }
+}
