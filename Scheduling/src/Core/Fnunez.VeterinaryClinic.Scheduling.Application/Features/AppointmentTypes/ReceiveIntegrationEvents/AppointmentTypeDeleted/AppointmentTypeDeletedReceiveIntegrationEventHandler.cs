f64fb94877f55d6e67161f5f876792b9fcd0d38b
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-29 23:03:23 -0800

    AppointmentTypeDeletedReceiveIntegrationEvent in Scheduling.Application #19

---
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application/Features/AppointmentTypes/ReceiveIntegrationEvents/AppointmentTypeDeleted/AppointmentTypeDeletedReceiveIntegrationEventHandler.cs
@@ -0,0 +1,31 @@
+using Fnunez.VeterinaryClinic.SharedKernel.Application.Repositories;
+using MediatR;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.Application.Features.AppointmentTypes.ReceiveIntegrationEvents.AppointmentTypeDeleted;
+
+public class AppointmentTypeDeletedReceiveIntegrationEventHandler
+    : INotificationHandler<AppointmentTypeDeletedReceiveIntegrationEvent>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public AppointmentTypeDeletedReceiveIntegrationEventHandler(
+        IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task Handle(
+        AppointmentTypeDeletedReceiveIntegrationEvent integrationEvent,
+        CancellationToken cancellationToken)
+    {
+        var contract = integrationEvent
+            .AppointmentTypeDeletedIntegrationEventContract;
+
+        string sql = @$"
+        DELETE FROM [dbo].[AppointmentTypes]
+        WHERE Id = {contract.AppointmentTypeId}";
+
+        var result = await _unitOfWork
+            .ExecuteSqlCommandAsync(sql, cancellationToken);
+    }
+}
