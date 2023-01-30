22b5a0ab4f546554edd3fcec1a6b0c44a2fee815
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-29 23:17:14 -0800

    PatientCreatedReceiveIntegrationEvent in Scheduling.Application #19

---
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application/Features/Patients/ReceiveIntegrationEvents/PatientCreated/PatientCreatedReceiveIntegrationEventHandler.cs
@@ -0,0 +1,58 @@
+using Fnunez.VeterinaryClinic.SharedKernel.Application.Repositories;
+using MediatR;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.Application.Features.Patients.ReceiveIntegrationEvents.PatientCreated;
+
+public class PatientCreatedReceiveIntegrationEventHandler
+    : INotificationHandler<PatientCreatedReceiveIntegrationEvent>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public PatientCreatedReceiveIntegrationEventHandler(
+        IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task Handle(
+        PatientCreatedReceiveIntegrationEvent integrationEvent,
+        CancellationToken cancellationToken)
+    {
+        var contract = integrationEvent
+            .PatientCreatedIntegrationEventContract;
+
+        var preferredDoctorId = contract.PatientPreferredDoctorId.HasValue
+            ? $"{contract.PatientPreferredDoctorId}"
+            : "NULL";
+
+        string sql = @$"
+        SET IDENTITY_INSERT [dbo].[Patients] ON;
+        
+        INSERT [dbo].[Patients] (
+            [Id],
+            [ClientId],
+            [Name],
+            [AnimalSex],
+            [AnimalType_Breed],
+            [AnimalType_Species],
+            [Photo_Name],
+            [Photo_StoredName],
+            [PreferredDoctorId]
+        ) VALUES (
+            {contract.PatientId},
+            {contract.PatientClientId},
+            N'{contract.PatientName}',
+            {contract.PatientSex},
+            N'{contract.PatientBreed}',
+            N'{contract.PatientSpecies}',
+            N'{contract.PatientPhotoName}',
+            N'{contract.PatientPhotoStoredName}',
+            {preferredDoctorId}
+        );
+        
+        SET IDENTITY_INSERT [dbo].[Patients] OFF;";
+
+        var result = await _unitOfWork
+            .ExecuteSqlCommandAsync(sql, cancellationToken);
+    }
+}
