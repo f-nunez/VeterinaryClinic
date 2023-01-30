638fe9e2f020dfb99d664fad3a1d7de01fcef264
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-29 23:04:53 -0800

    ClinicCreatedReceiveIntegrationEvent in Scheduling.Application #19

---
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application/Features/Clinics/ReceiveIntegrationEvents/ClinicCreated/ClinicCreatedReceiveIntegrationEventHandler.cs
@@ -0,0 +1,44 @@
+using Fnunez.VeterinaryClinic.SharedKernel.Application.Repositories;
+using MediatR;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.Application.Features.Clinics.ReceiveIntegrationEvents.ClinicCreated;
+
+public class ClinicCreatedReceiveIntegrationEventHandler
+    : INotificationHandler<ClinicCreatedReceiveIntegrationEvent>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public ClinicCreatedReceiveIntegrationEventHandler(
+        IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task Handle(
+        ClinicCreatedReceiveIntegrationEvent integrationEvent,
+        CancellationToken cancellationToken)
+    {
+        var contract = integrationEvent
+            .ClinicCreatedIntegrationEventContract;
+
+        string sql = @$"
+        SET IDENTITY_INSERT [dbo].[Clinics] ON;
+
+        INSERT [dbo].[Clinics] (
+            [Id],
+            [Address],
+            [EmailAddress],
+            [Name])
+        VALUES (
+            {contract.ClinicId},
+            N'{contract.ClinicAddress}',
+            N'{contract.ClinicEmailAddress}',
+            N'{contract.ClinicName}'
+        );
+        
+        SET IDENTITY_INSERT [dbo].[Clinics] OFF;";
+
+        var result = await _unitOfWork
+            .ExecuteSqlCommandAsync(sql, cancellationToken);
+    }
+}
