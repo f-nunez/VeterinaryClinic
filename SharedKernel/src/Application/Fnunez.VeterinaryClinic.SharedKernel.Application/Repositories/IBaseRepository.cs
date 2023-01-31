da4646390d3b5b1dc3623ec54cf41e06586ce4b0
Francisco Nuñez <fonfonferi@gmail.com>
2023-01-30 19:13:51 -0800

    CodeSmells into IBaseRepository in SharedKernel.Application #21

--- SharedKernel/src/Application/Fnunez.VeterinaryClinic.SharedKernel.Application/Repositories/IBaseRepository.cs
+++ SharedKernel/src/Application/Fnunez.VeterinaryClinic.SharedKernel.Application/Repositories/IBaseRepository.cs
@@ -6,6 +6,8 @@ public interface IBaseRepository<T> where T : class
     /// Adds an entity in the database.
     /// </summary>
     /// <param name="entity">The entity to add.</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken" /> to observe
+    /// while waiting for the task to complete.</param>
     /// <returns>
     /// A task that represents the asynchronous operation.
     /// The task result contains the <typeparamref name="T" />.
@@ -13,10 +15,11 @@ public interface IBaseRepository<T> where T : class
     Task<T> AddAsync(T entity, CancellationToken cancellationToken = default);
 
     /// <summary>
-    /// Adds the given entities in the database
+    /// Adds the given entities in the database.
     /// </summary>
-    /// <param name="entities"></param>
-    /// <param name="cancellationToken"></param>
+    /// <param name="entities">The entities to add.</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken" /> to observe
+    /// while waiting for the task to complete.</param>
     /// <returns>
     /// A task that represents the asynchronous operation.
     /// The task result contains the <typeparamref name="IEnumerable<T>" />.
@@ -26,33 +29,40 @@ public interface IBaseRepository<T> where T : class
         CancellationToken cancellationToken = default);
 
     /// <summary>
-    /// Removes an entity in the database
+    /// Removes an entity with soft delete in the database.
     /// </summary>
     /// <param name="entity">The entity to delete.</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken" /> to observe
+    /// while waiting for the task to complete.</param>
     /// <returns>A task that represents the asynchronous operation.</returns>
     Task DeleteAsync(T entity, CancellationToken cancellationToken = default);
 
     /// <summary>
-    /// Removes the given entities in the database
+    /// Removes the given entities with soft delete in the database.
     /// </summary>
     /// <param name="entities">The entities to remove.</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken" /> to observe
+    /// while waiting for the task to complete.</param>
     /// <returns>A task that represents the asynchronous operation.</returns>
     Task DeleteRangeAsync(
         IEnumerable<T> entities,
         CancellationToken cancellationToken = default);
 
     /// <summary>
-    /// Updates an entity in the database
+    /// Updates an entity in the database.
     /// </summary>
     /// <param name="entity">The entity to update.</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken" /> to observe
+    /// while waiting for the task to complete.</param>
     /// <returns>A task that represents the asynchronous operation.</returns>
     Task UpdateAsync(T entity, CancellationToken cancellationToken = default);
 
     /// <summary>
-    /// Updates the given entities in the database
+    /// Updates the given entities in the database.
     /// </summary>
     /// <param name="entities">The entities to update.</param>
-    /// <param name="cancellationToken"></param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken" /> to observe
+    /// while waiting for the task to complete.</param>
     /// <returns>A task that represents the asynchronous operation.</returns>
     Task UpdateRangeAsync(
         IEnumerable<T> entities,
