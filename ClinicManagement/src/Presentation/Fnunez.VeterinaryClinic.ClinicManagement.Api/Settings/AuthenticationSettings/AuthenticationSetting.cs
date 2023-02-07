5841d04c81080de5f6ee7d33546b903272e86813
Francisco Nuñez <fonfonferi@gmail.com>
2023-02-06 18:17:50 -0800

    AuthenticationSetting into ClinicManagement.Api #23

---
+++ ClinicManagement/src/Presentation/Fnunez.VeterinaryClinic.ClinicManagement.Api/Settings/AuthenticationSettings/AuthenticationSetting.cs
@@ -0,0 +1,9 @@
+namespace Fnunez.VeterinaryClinic.ClinicManagement.Api.Settings;
+
+public class AuthenticationSetting : IAuthenticationSetting
+{
+    public string Audience { get; set; } = null!;
+    public string Authority { get; set; } = null!;
+    public string DefaultScheme { get; set; } = null!;
+    public bool ValidateAudience { get; set; }
+}
