e588d47f980513fa466c0344f91b9ede56e720a6
Francisco Nuñez <fonfonferi@gmail.com>
2023-02-11 19:37:34 -0800

    Add profile menu into MainLayout in ClinicManagement.BlazorClient.Client #23

--- ClinicManagement/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client/Shared/MainLayout.razor.designer.cs
+++ ClinicManagement/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client/Shared/MainLayout.razor.designer.cs
@@ -1,3 +1,4 @@
+using Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client.Services;
 using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Localization;
 using Radzen;
@@ -17,6 +18,9 @@ public partial class MainLayoutComponent : LayoutComponentBase
     protected NotificationService NotificationService { get; set; }
 
     [Inject]
+    protected ISecurityService SecurityService { get; set; }
+
+    [Inject]
     protected IStringLocalizer<MainLayout> StringLocalizer { get; set; }
 
     protected RadzenBody RadzenBody;
@@ -27,11 +31,24 @@ public partial class MainLayoutComponent : LayoutComponentBase
 
     protected bool IsSidebarExpanded = true;
 
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+
+        await SecurityService.SetUserInfoAsync();
+    }
+
     protected async Task SidebarToggleClick(dynamic args)
     {
         await InvokeAsync(() => { RadzenSidebar.Toggle(); });
         await InvokeAsync(() => { RadzenBody.Toggle(); });
         IsSidebarExpanded = !IsSidebarExpanded;
         IsBodyExpanded = !IsBodyExpanded;
+    }
+
+    protected async System.Threading.Tasks.Task ProfileMenuClick(dynamic args)
+    {
+        if (args.Value == "Logout")
+            await SecurityService.Logout();
     }
 }
