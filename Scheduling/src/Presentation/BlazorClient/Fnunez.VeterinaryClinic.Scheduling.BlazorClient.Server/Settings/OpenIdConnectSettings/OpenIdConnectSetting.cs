fb121a4f2699c2eeb4584cfca8c362989f335d70
Francisco Nuñez <fonfonferi@gmail.com>
2023-02-16 13:46:40 -0800

    OpenIdConnectSetting in Scheduling.BlazorClient.Server #23

---
+++ Scheduling/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Server/Settings/OpenIdConnectSettings/OpenIdConnectSetting.cs
@@ -0,0 +1,18 @@
+namespace Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Server.Settings;
+
+public class OpenIdConnectSetting : IOpenIdConnectSetting
+{
+    public string AccessDeniedPath { get; set; } = null!;
+    public string AuthenticationScheme { get; set; } = null!;
+    public string Authority { get; set; } = null!;
+    public string[] ClaimActionsToMap { get; set; } = null!;
+    public string ClientId { get; set; } = null!;
+    public string ClientSecret { get; set; } = null!;
+    public bool EnabledGetClaimsFromUserInfoEndpoint { get; set; }
+    public bool EnabledMapInboundClaims { get; set; }
+    public bool EnabledSaveTokens { get; set; }
+    public bool EnabledUsePkce { get; set; }
+    public string ResponseMode { get; set; } = null!;
+    public string ResponseType { get; set; } = null!;
+    public string[] Scopes { get; set; } = null!;
+}
