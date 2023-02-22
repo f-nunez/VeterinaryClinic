8e2e7d294fe768c6e2f624c1b8ac39062efdd9f4
Francisco Nuñez <fonfonferi@gmail.com>
2023-02-21 18:15:47 -0800

    Add auditable properties into DoctorConfiguration in ClinicManagement.Infrastructure #25

--- ClinicManagement/src/Infrastructure/Fnunez.VeterinaryClinic.ClinicManagement.Infrastructure/Persistence/Configurations/DoctorConfiguration.cs
+++ ClinicManagement/src/Infrastructure/Fnunez.VeterinaryClinic.ClinicManagement.Infrastructure/Persistence/Configurations/DoctorConfiguration.cs
@@ -8,8 +8,14 @@ public class DoctorConfiguration : IEntityTypeConfiguration<Doctor>
 {
     public void Configure(EntityTypeBuilder<Doctor> builder)
     {
+        builder.Property(d => d.CreatedBy)
+            .HasMaxLength(450);
+
         builder.Property(d => d.FullName)
             .HasMaxLength(200)
             .IsRequired();
+
+        builder.Property(d => d.UpdatedBy)
+            .HasMaxLength(450);
     }
 }
