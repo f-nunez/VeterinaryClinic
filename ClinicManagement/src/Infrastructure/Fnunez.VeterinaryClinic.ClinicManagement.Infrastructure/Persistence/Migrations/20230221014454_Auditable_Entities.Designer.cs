288049d6ca6c196e6b60b615e68c47e01727461a
Francisco Nuñez <fonfonferi@gmail.com>
2023-02-21 18:18:01 -0800

    Add Auditable_Entities migration in ClinicManagement.Infrastructure #25

--- ClinicManagement/src/Infrastructure/Fnunez.VeterinaryClinic.ClinicManagement.Infrastructure/Persistence/Migrations/20230221014454_Auditable_Entities.Designer.cs
+++ ClinicManagement/src/Infrastructure/Fnunez.VeterinaryClinic.ClinicManagement.Infrastructure/Persistence/Migrations/20230221014454_Auditable_Entities.Designer.cs
@@ -4,6 +4,7 @@ using Fnunez.VeterinaryClinic.ClinicManagement.Infrastructure.Persistence.Contex
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Fnunez.VeterinaryClinic.ClinicManagement.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230221014454_Auditable_Entities")]
+    partial class Auditable_Entities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ClinicManagement/src/Infrastructure/Fnunez.VeterinaryClinic.ClinicManagement.Infrastructure/Persistence/Migrations/20230221014454_Auditable_Entities.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fnunez.VeterinaryClinic.ClinicManagement.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Auditable_Entities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Rooms",
+                type: "nvarchar(450)",
+                maxLength: 450,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreatedOn",
+                table: "Rooms",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdatedBy",
+                table: "Rooms",
+                type: "nvarchar(450)",
+                maxLength: 450,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "UpdatedOn",
+                table: "Rooms",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Patients",
+                type: "nvarchar(450)",
+                maxLength: 450,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreatedOn",
+                table: "Patients",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdatedBy",
+                table: "Patients",
+                type: "nvarchar(450)",
+                maxLength: 450,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "UpdatedOn",
+                table: "Patients",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Doctors",
+                type: "nvarchar(450)",
+                maxLength: 450,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreatedOn",
+                table: "Doctors",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdatedBy",
+                table: "Doctors",
+                type: "nvarchar(450)",
+                maxLength: 450,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "UpdatedOn",
+                table: "Doctors",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Clinics",
+                type: "nvarchar(450)",
+                maxLength: 450,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreatedOn",
+                table: "Clinics",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdatedBy",
+                table: "Clinics",
+                type: "nvarchar(450)",
+                maxLength: 450,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "UpdatedOn",
+                table: "Clinics",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Clients",
+                type: "nvarchar(450)",
+                maxLength: 450,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreatedOn",
+                table: "Clients",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdatedBy",
+                table: "Clients",
+                type: "nvarchar(450)",
+                maxLength: 450,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "UpdatedOn",
+                table: "Clients",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "AppointmentTypes",
+                type: "nvarchar(450)",
+                maxLength: 450,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreatedOn",
+                table: "AppointmentTypes",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdatedBy",
+                table: "AppointmentTypes",
+                type: "nvarchar(450)",
+                maxLength: 450,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "UpdatedOn",
+                table: "AppointmentTypes",
+                type: "datetimeoffset",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedOn",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Patients");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "Patients");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "Patients");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedOn",
+                table: "Patients");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Doctors");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "Doctors");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "Doctors");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedOn",
+                table: "Doctors");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Clinics");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "Clinics");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "Clinics");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedOn",
+                table: "Clinics");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedOn",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "AppointmentTypes");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "AppointmentTypes");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "AppointmentTypes");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedOn",
+                table: "AppointmentTypes");
+        }
+    }
+}
