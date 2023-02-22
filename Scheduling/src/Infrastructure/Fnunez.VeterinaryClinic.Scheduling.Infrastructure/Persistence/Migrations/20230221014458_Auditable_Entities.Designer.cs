51fd052224d4d285e7e8ff8041b3ae62e8bab844
Francisco Nuñez <fonfonferi@gmail.com>
2023-02-21 18:22:05 -0800

    Add Auditable_Entities migration Add in Scheduling.Infrastructure #25

--- Scheduling/src/Infrastructure/Fnunez.VeterinaryClinic.Scheduling.Infrastructure/Persistence/Migrations/20230221014458_Auditable_Entities.Designer.cs
+++ Scheduling/src/Infrastructure/Fnunez.VeterinaryClinic.Scheduling.Infrastructure/Persistence/Migrations/20230221014458_Auditable_Entities.Designer.cs
@@ -4,6 +4,7 @@ using Fnunez.VeterinaryClinic.Scheduling.Infrastructure.Persistence.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Fnunez.VeterinaryClinic.Scheduling.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230221014458_Auditable_Entities")]
+    partial class Auditable_Entities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Scheduling/src/Infrastructure/Fnunez.VeterinaryClinic.Scheduling.Infrastructure/Persistence/Migrations/20230221014458_Auditable_Entities.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fnunez.VeterinaryClinic.Scheduling.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Auditable_Entities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Appointments",
+                type: "nvarchar(450)",
+                maxLength: 450,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreatedOn",
+                table: "Appointments",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdatedBy",
+                table: "Appointments",
+                type: "nvarchar(450)",
+                maxLength: 450,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "UpdatedOn",
+                table: "Appointments",
+                type: "datetimeoffset",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedOn",
+                table: "Appointments");
+        }
+    }
+}
