00378f9daef151c2b3b79baa85fe0efb0e1657eb
Francisco Nuñez <fonfonferi@gmail.com>
2023-02-24 14:25:27 -0800

    RabbitMqSetting into Settings in ClinicManagement.Application #25

--- ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Interfaces/Settings/IRabbitMqSetting.cs
+++
@@ -1,6 +0,0 @@
-namespace Fnunez.VeterinaryClinic.ClinicManagement.Application.Interfaces.Settings;
-
-public interface IRabbitMqSetting
-{
-    public Uri HostAddress { get; }
-}
---
+++ ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Settings/IRabbitMqSetting.cs
@@ -0,0 +1,6 @@
+namespace Fnunez.VeterinaryClinic.ClinicManagement.Application.Settings;
+
+public interface IRabbitMqSetting
+{
+    public Uri HostAddress { get; }
+}
