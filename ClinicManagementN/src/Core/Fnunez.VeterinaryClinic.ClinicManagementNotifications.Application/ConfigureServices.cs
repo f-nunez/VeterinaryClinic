ea1ab4c87dd1b1868b01ee9f7aef8b3be263aba7
Francisco Nuñez <fonfonferi@gmail.com>
2023-02-27 23:17:41 -0800

    ConfigureServices in ClinicManagementNotifications.Application #25

---
+++ ClinicManagementN/src/Core/Fnunez.VeterinaryClinic.ClinicManagementNotifications.Application/ConfigureServices.cs
@@ -0,0 +1,27 @@
+using System.Reflection;
+using FluentValidation;
+using Fnunez.VeterinaryClinic.ClinicManagementNotifications.Application.Common.Behaviors;
+using MediatR;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddApplicationServices(
+        this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+        services.AddMediatR(Assembly.GetExecutingAssembly());
+
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
+
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehavior<,>));
+
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+        return services;
+    }
+}
