c581d72e67a52252312d975046ffffc8f4bbbe48
Francisco Nuñez <fonfonferi@gmail.com>
2023-02-28 21:03:28 -0800

    NotificationEvent in ClinicManagementNotifications.Domain #25

---
+++ ClinicManagementN/src/Core/Fnunez.VeterinaryClinic.ClinicManagementNotifications.Domain/NotificationAggregate/Enums/NotificationEvent.cs
@@ -0,0 +1,23 @@
+namespace Fnunez.VeterinaryClinic.ClinicManagementNotifications.Domain.NotificationAggregate.Enums;
+
+public enum NotificationEvent
+{
+    AppointmentTypeCreated,
+    AppointmentTypeDeleted,
+    AppointmentTypeUpdated,
+    ClientCreated,
+    ClientDeleted,
+    ClientUpdated,
+    ClinicCreated,
+    ClinicDeleted,
+    ClinicUpdated,
+    DoctorCreated,
+    DoctorDeleted,
+    DoctorUpdated,
+    PatientCreated,
+    PatientDeleted,
+    PatientUpdated,
+    RoomCreated,
+    RoomDeleted,
+    RoomUpdated
+}
