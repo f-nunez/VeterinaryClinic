dbcb85c55f3dfe2e7313e6576c2aacfcb6a91f65
Francisco Nuñez <fonfonferi@gmail.com>
2023-03-02 12:10:00 -0800

    ClientCreatedNotificationRequest in ClinicManagement.Application #25

---
+++ ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Services/NotificationRequest/Requests/Client/ClientCreatedNotificationRequest.cs
@@ -0,0 +1,7 @@
+namespace Fnunez.VeterinaryClinic.ClinicManagement.Application.Services.NotificationRequest.Requests;
+
+public class ClientCreatedNotificationRequest : BaseNotificationRequest
+{
+    public int Id { get; set; }
+    public string? FullName { get; set; }
+}
