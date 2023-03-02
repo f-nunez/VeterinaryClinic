596534a1fa01810460b1711c7666d8adad744b54
Francisco Nuñez <fonfonferi@gmail.com>
2023-03-01 17:07:30 -0800

    NotificationRequestConsumer in ClinicManagementNotifications.Infrastructure #25

---
+++ ClinicManagementN/src/Infrastructure/Fnunez.VeterinaryClinic.ClinicManagementNotifications.Infrastructure/ServiceBus/Consumers/NotificationRequestConsumer.cs
@@ -0,0 +1,26 @@
+using ClinicManagementContracts;
+using Fnunez.VeterinaryClinic.ClinicManagementNotifications.Application.Services;
+using MassTransit;
+
+namespace Fnunez.VeterinaryClinic.ClinicManagement.Infrastructure.ServiceBus.Consumers;
+
+public class NotificationRequestConsumer
+    : IConsumer<NotificationRequestContract>
+{
+    private readonly INotificationEngineService _notificationEngineService;
+
+    public NotificationRequestConsumer(
+        INotificationEngineService notificationEngineService)
+    {
+        _notificationEngineService = notificationEngineService;
+    }
+
+    public async Task Consume(
+        ConsumeContext<NotificationRequestContract> context)
+    {
+        await _notificationEngineService.CreateAndNotifyAsync(
+            context.Message.NotificationEvent,
+            context.Message.SerializedNotificationRequest
+        );
+    }
+}
