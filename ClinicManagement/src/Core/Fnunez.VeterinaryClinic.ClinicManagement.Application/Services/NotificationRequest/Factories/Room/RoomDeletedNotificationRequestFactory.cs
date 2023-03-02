a275b408c5679880a2fdd1a841b0990fa223388d
Francisco Nuñez <fonfonferi@gmail.com>
2023-03-02 12:24:08 -0800

    RoomDeletedNotificationRequestFactory in ClinicManagement.Application #25

---
+++ ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Services/NotificationRequest/Factories/Room/RoomDeletedNotificationRequestFactory.cs
@@ -0,0 +1,38 @@
+using Fnunez.VeterinaryClinic.ClinicManagement.Application.Services.NotificationRequest.Requests;
+using Fnunez.VeterinaryClinic.ClinicManagement.Domain.RoomAggregate;
+
+namespace Fnunez.VeterinaryClinic.ClinicManagement.Application.Services.NotificationRequest.Factories;
+
+public class RoomDeletedNotificationRequestFactory
+    : INotificationRequestFactory
+{
+    private readonly Room _room;
+    private readonly Guid _correlationId;
+    private readonly string? _userId;
+
+    public RoomDeletedNotificationRequestFactory(
+        Room room,
+        Guid correlationId,
+        string? userId)
+    {
+        _room = room;
+        _correlationId = correlationId;
+        _userId = userId;
+    }
+
+    public BaseNotificationRequest CreateNotificationRequest()
+    {
+        return new RoomDeletedNotificationRequest
+        {
+            CorrelationId = _correlationId,
+            Id = _room.Id,
+            Name = _room.Name,
+            TriggeredByUserId = _userId
+        };
+    }
+
+    public string GetNotificationEvent()
+    {
+        return NotificationEvent.RoomDeleted.ToString();
+    }
+}
