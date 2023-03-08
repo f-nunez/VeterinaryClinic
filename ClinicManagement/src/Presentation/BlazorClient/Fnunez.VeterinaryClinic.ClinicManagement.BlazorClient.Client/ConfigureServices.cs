81cfcb2a4fd79269b84bab628c9655fb1fad7577
Francisco Nuñez <fonfonferi@gmail.com>
2023-03-07 21:47:28 -0800

    Register AppNotificationBuilder into dependency container in ClinicManagement.BlazorClient.Client #25

--- ClinicManagement/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client/ConfigureServices.cs
+++ ClinicManagement/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client/ConfigureServices.cs
@@ -3,6 +3,7 @@ using Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client.BackendForFro
 using Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client.Handlers;
 using Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client.Services;
 using Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client.Settings;
+using Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client.Shared.Components.AppNotification.Factories;
 using Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client.Shared.Components.Language;
 using Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client.Shared.Components.Spinner;
 using Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client.Shared.Components.TimeZone;
@@ -70,6 +71,8 @@ public static class ConfigureServices
 
         services.AddScoped<IUserSettingsService, UserSettingsService>();
 
+        // register AppNotification component
+        services.AddScoped<IAppNotificationBuilder, AppNotificationBuilder>();
         // register Language component
         services.AddSingleton<ILanguageComponentData>(
             configuration.GetSection(typeof(LanguageComponentData).Name)
