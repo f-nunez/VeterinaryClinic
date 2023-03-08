83c7027e689a7646767dabc7b0187b94b62c5610
Francisco Nuñez <fonfonferi@gmail.com>
2023-03-07 21:38:20 -0800

    Patient notification payloads in ClinicManagement.BlazorClient.Client #25

---
+++ ClinicManagement/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client/Shared/Components/AppNotification/Payloads/Patient/PatientDeletedPayload.cs
@@ -0,0 +1,7 @@
+namespace Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client.Shared.Components.AppNotification.Payloads;
+
+public class PatientDeletedPayload
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+}
