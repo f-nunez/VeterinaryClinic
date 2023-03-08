e1927870fc379b312dc8e9b1b82d6135c20ec4a8
Francisco Nuñez <fonfonferi@gmail.com>
2023-03-07 21:37:33 -0800

    AppointmentType notification payloads in ClinicManagement.BlazorClient.Client #25

---
+++ ClinicManagement/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client/Shared/Components/AppNotification/Payloads/AppointmentType/AppointmentTypeCreatedPayload.cs
@@ -0,0 +1,7 @@
+namespace Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client.Shared.Components.AppNotification.Payloads;
+
+public class AppointmentTypeCreatedPayload
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+}
