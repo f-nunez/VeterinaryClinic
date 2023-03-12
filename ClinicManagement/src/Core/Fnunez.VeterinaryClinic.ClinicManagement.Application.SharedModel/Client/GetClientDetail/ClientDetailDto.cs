54a1bb4eabc8d5efe5638f7ddfcc336cfb6acc8e
Francisco Nuñez <fonfonferi@gmail.com>
2023-03-11 22:25:44 -0800

    Add IsActive property into ClientDetailDto in ClinicManagement.Application.SharedModel #25

--- ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel/Client/GetClientDetail/ClientDetailDto.cs
+++ ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel/Client/GetClientDetail/ClientDetailDto.cs
@@ -5,6 +5,7 @@ public class ClientDetailDto
     public int ClientId { get; set; }
     public string EmailAddress { get; set; } = string.Empty;
     public string FullName { get; set; } = string.Empty;
+    public bool IsActive { get; set; }
     public string PreferredDoctorFullName { get; set; } = string.Empty;
     public string PreferredName { get; set; } = string.Empty;
     public string Salutation { get; set; } = string.Empty;
