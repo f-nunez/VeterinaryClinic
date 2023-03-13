e0d305d5dfc95d92a19bb151d9b3bcd0ed12eaf0
Francisco Nuñez <fonfonferi@gmail.com>
2023-03-13 15:55:37 -0700

    DoctorDetailNotification page in ClinicManagement.BlazorClient.Client #25

---
+++ ClinicManagement/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client/Pages/Doctors/DoctorDetailNotification.razor.designer.cs
@@ -0,0 +1,61 @@
+using Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel.Doctor.GetDoctorById;
+using Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client.Helpers;
+using Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client.Services;
+using Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client.ViewModels.Doctors;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+
+namespace Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client.Pages.Doctors;
+
+public partial class DoctorDetailNotificationComponent : ComponentBase
+{
+    [Inject]
+    private IDoctorService _doctorService { get; set; }
+
+    [Inject]
+    private ILogger<DoctorDetailNotificationComponent> _logger { get; set; }
+
+    [Inject]
+    private NavigationManager _navigationManager { get; set; }
+
+    protected bool IsNotActive { get; set; }
+
+    protected bool IsNotFound { get; set; }
+
+    protected DoctorVm Model { get; set; }
+
+    [Inject]
+    protected IStringLocalizer<DoctorDetailNotificationComponent> StringLocalizer { get; set; }
+
+    [Parameter]
+    public int DoctorId { get; set; }
+
+    protected async override Task OnInitializedAsync()
+    {
+        var request = new GetDoctorByIdRequest
+        {
+            Id = DoctorId
+        };
+
+        try
+        {
+            var doctor = await _doctorService
+                .GetByIdAsync(request);
+
+            Model = DoctorHelper
+                .MapDoctorViewModel(doctor);
+
+            IsNotActive = !Model.IsActive;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, ex.Message);
+            IsNotFound = true;
+        }
+    }
+
+    protected void OnClickBack()
+    {
+        _navigationManager.NavigateTo("doctors");
+    }
+}
