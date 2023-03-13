44551adf3925d083d296057546e860e384f4dbe1
Francisco Nuñez <fonfonferi@gmail.com>
2023-03-13 15:43:59 -0700

    Add IsActive property into ClinicVm in ClinicManagement.BlazorClient.Client #25

--- ClinicManagement/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client/ViewModels/Clinics/ClinicVm.cs
+++ ClinicManagement/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client/ViewModels/Clinics/ClinicVm.cs
@@ -5,5 +5,6 @@ public class ClinicVm
     public string Address { get; set; } = string.Empty;
     public string EmailAddress { get; set; } = string.Empty;
     public int Id { get; set; }
+    public bool IsActive { get; set; }
     public string Name { get; set; } = string.Empty;
 }
