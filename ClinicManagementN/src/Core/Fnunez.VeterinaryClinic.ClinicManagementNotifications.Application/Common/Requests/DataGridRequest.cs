e2ea58073127ccad8d7ae31174c96a3417845afb
Francisco Nuñez <fonfonferi@gmail.com>
2023-03-17 21:58:56 -0700

    DataGridRequest in ClinicManagementNotifications.Application #25

---
+++ ClinicManagementN/src/Core/Fnunez.VeterinaryClinic.ClinicManagementNotifications.Application/Common/Requests/DataGridRequest.cs
@@ -0,0 +1,18 @@
+namespace Fnunez.VeterinaryClinic.ClinicManagementNotifications.Application.Common.Requests;
+
+public class DataGridRequest
+{
+    public string Search { get; set; } = string.Empty;
+    public int? Skip { get; set; }
+    public List<DataGridRequestSort>? Sorts { get; set; }
+    public int? Take { get; set; }
+
+    public override string ToString()
+    {
+        string sortProperties = string.Empty;
+        if (Sorts != null)
+            sortProperties = string.Join(',', Sorts.Select(s => $"{s.PropertyName} isAsc:{s.IsAscending.ToString()}"));
+
+        return $"Sorts: {sortProperties}, Search: {Search}, Skip: {Skip}, Take: {Take}";
+    }
+}
