f7bf45004870c0696ebc6c1a5d1f12d553c0ace6
Francisco Nuñez <fonfonferi@gmail.com>
2023-03-19 23:08:49 -0700

    NotificationRequestContract in SchedulingNotifications.Application #25

---
+++ SchedulingN/src/Core/Fnunez.VeterinaryClinic.SchedulingNotifications.Application/SchedulingContracts/NotificationRequests/NotificationRequestContract.cs
@@ -0,0 +1,11 @@
+namespace SchedulingContracts;
+
+public class NotificationRequestContract
+{
+    public Guid CausationId { get; set; } = Guid.NewGuid();
+    public Guid CorrelationId { get; set; } = Guid.NewGuid();
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public DateTimeOffset OccurredOn = DateTimeOffset.UtcNow;
+    public string NotificationEvent { get; set; } = string.Empty;
+    public string SerializedNotificationRequest { get; set; } = string.Empty;
+}
