8cc5c7be95b26b31c04a06aae9dea335160e5650
Francisco Nuñez <fonfonferi@gmail.com>
2023-03-20 14:09:48 -0700

    BaseRequest in SchedulingNotifications.Application #25

---
+++ SchedulingN/src/Core/Fnunez.VeterinaryClinic.SchedulingNotifications.Application/Common/Requests/BaseRequest.cs
@@ -0,0 +1,8 @@
+namespace Fnunez.VeterinaryClinic.SchedulingNotifications.Application.Common.Requests;
+
+/// <summary>
+/// Base class used by API requests
+/// </summary>
+public abstract class BaseRequest : BaseMessage
+{
+}
