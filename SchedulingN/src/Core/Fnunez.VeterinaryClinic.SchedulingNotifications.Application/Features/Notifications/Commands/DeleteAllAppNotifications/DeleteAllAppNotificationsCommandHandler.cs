d34ac47971041aad8fca18533a1df869dddf8e3a
Francisco Nuñez <fonfonferi@gmail.com>
2023-03-20 15:10:24 -0700

    DeleteAllAppNotificationsCommandHandler in SchedulingNotifications.Application #25

---
+++ SchedulingN/src/Core/Fnunez.VeterinaryClinic.SchedulingNotifications.Application/Features/Notifications/Commands/DeleteAllAppNotifications/DeleteAllAppNotificationsCommandHandler.cs
@@ -0,0 +1,54 @@
+using Fnunez.VeterinaryClinic.SchedulingNotifications.Application.Common.Interfaces;
+using Fnunez.VeterinaryClinic.SchedulingNotifications.Domain.AppNotificationAggregate;
+using Fnunez.VeterinaryClinic.SharedKernel.Application.Repositories;
+using MediatR;
+
+namespace Fnunez.VeterinaryClinic.SchedulingNotifications.Application.Features.Notifications.Commands.DeleteAllAppNotifications;
+
+public class DeleteAllAppNotificationsCommandHandler
+    : IRequestHandler<DeleteAllAppNotificationsCommand, DeleteAllAppNotificationsResponse>
+{
+    private readonly ICurrentUserService _currentUserService;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public DeleteAllAppNotificationsCommandHandler(
+        ICurrentUserService currentUserService,
+        IUnitOfWork unitOfWork)
+    {
+        _currentUserService = currentUserService;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<DeleteAllAppNotificationsResponse> Handle(
+        DeleteAllAppNotificationsCommand command,
+        CancellationToken cancellationToken)
+    {
+        var request = command.DeleteAllAppNotificationsRequest;
+
+        var response = new DeleteAllAppNotificationsResponse(
+            request.CorrelationId);
+
+        var specification = new AppNotificationSpecification(
+            _currentUserService.UserId);
+
+        var appNotifications = await _unitOfWork
+            .Repository<AppNotification>()
+            .ListAsync(specification, cancellationToken);
+
+        if (!appNotifications.Any())
+            return response;
+
+        var deletedOn = DateTimeOffset.UtcNow;
+
+        foreach (var appNotification in appNotifications)
+            appNotification.UpdateDeletedOn(deletedOn);
+
+        await _unitOfWork
+            .Repository<AppNotification>()
+            .DeleteRangeAsync(appNotifications, cancellationToken);
+
+        await _unitOfWork.CommitAsync(cancellationToken);
+
+        return response;
+    }
+}
