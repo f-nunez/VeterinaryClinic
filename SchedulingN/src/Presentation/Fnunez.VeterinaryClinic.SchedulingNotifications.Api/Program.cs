7675d3df8582c8d1acdec1553c423976ceb373b6
Francisco Nuñez <fonfonferi@gmail.com>
2023-03-19 23:32:12 -0700

    Add ConfigureService methods into Program startup class in SchedulingNotifications.Api #25

--- SchedulingN/src/Presentation/Fnunez.VeterinaryClinic.SchedulingNotifications.Api/Program.cs
+++ SchedulingN/src/Presentation/Fnunez.VeterinaryClinic.SchedulingNotifications.Api/Program.cs
@@ -1,25 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddApplicationServices();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
+builder.Services.AddWebServices(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+app.AddWebApplicationBuilder();
 
 app.Run();
