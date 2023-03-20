0ce2e66771e7c62b98b443afa67692a52985a667
Francisco Nuñez <fonfonferi@gmail.com>
2023-03-19 23:23:09 -0700

    ConfigureServices in SchedulingNotifications.Infrastructure #25

---
+++ SchedulingN/src/Infrastructure/Fnunez.VeterinaryClinic.SchedulingNotifications.Infrastructure/ConfigureServices.cs
@@ -0,0 +1,12 @@
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddInfrastructureServices(
+        this IServiceCollection services, IConfiguration configuration)
+    {
+        return services;
+    }
+}
