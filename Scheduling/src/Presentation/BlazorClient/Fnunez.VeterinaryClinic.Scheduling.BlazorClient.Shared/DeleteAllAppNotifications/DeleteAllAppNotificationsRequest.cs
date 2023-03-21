961b7d80f1c5727574a55206fdee77e784a25329
Francisco Nuñez <fonfonferi@gmail.com>
2023-03-20 19:44:33 -0700

    DeleteAllAppNotificationsRequest in Scheduling.BlazorClient.Shared #25

---
+++ Scheduling/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Shared/DeleteAllAppNotifications/DeleteAllAppNotificationsRequest.cs
@@ -0,0 +1,10 @@
+using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Common;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Shared.DeleteAllAppNotifications;
+
+public class DeleteAllAppNotificationsRequest : BaseRequest
+{
+    public DeleteAllAppNotificationsRequest()
+    {
+    }
+}
