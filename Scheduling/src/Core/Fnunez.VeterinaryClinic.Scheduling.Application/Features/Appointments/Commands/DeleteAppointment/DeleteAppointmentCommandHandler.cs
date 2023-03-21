72f59e06c80bf63072207a561a6b90d9ca1f5d80
Francisco Nuñez <fonfonferi@gmail.com>
2023-03-20 19:40:38 -0700

    Add SendNotificationRequest method into DeleteAppointmentCommandHandler in Scheduling.Application #25

--- Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application/Features/Appointments/Commands/DeleteAppointment/DeleteAppointmentCommandHandler.cs
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application/Features/Appointments/Commands/DeleteAppointment/DeleteAppointmentCommandHandler.cs
@@ -1,6 +1,8 @@
 using AutoMapper;
 using Fnunez.VeterinaryClinic.Scheduling.Application.Common.Exceptions;
 using Fnunez.VeterinaryClinic.Scheduling.Application.Common.Interfaces;
+using Fnunez.VeterinaryClinic.Scheduling.Application.Services.NotificationRequest;
+using Fnunez.VeterinaryClinic.Scheduling.Application.Services.NotificationRequest.Factories;
 using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Appointment.DeleteAppointment;
 using Fnunez.VeterinaryClinic.Scheduling.Domain.AppointmentAggregate;
 using Fnunez.VeterinaryClinic.SharedKernel.Application.Repositories;
@@ -13,15 +15,18 @@ public class DeleteAppointmentCommandHandler
 {
     private readonly ICurrentUserService _currentUserService;
     private readonly IMapper _mapper;
+    private readonly INotificationRequestService _notificationRequestService;
     private readonly IUnitOfWork _unitOfWork;
 
     public DeleteAppointmentCommandHandler(
         ICurrentUserService currentUserService,
         IMapper mapper,
+        INotificationRequestService notificationRequestService,
         IUnitOfWork unitOfWork)
     {
         _currentUserService = currentUserService;
         _mapper = mapper;
+        _notificationRequestService = notificationRequestService;
         _unitOfWork = unitOfWork;
     }
 
@@ -48,6 +53,27 @@ public class DeleteAppointmentCommandHandler
 
         await _unitOfWork.CommitAsync();
 
+        await SendNotificationRequestAsync(
+            appointmentToDelete,
+            request.CorrelationId,
+            cancellationToken
+        );
+
         return response;
+    }
+
+    private async Task SendNotificationRequestAsync(
+        Appointment appointment,
+        Guid correlationId,
+        CancellationToken cancellationToken)
+    {
+        var factory = new AppointmentDeletedNotificationRequestFactory(
+            appointment,
+            correlationId,
+            _currentUserService.UserId
+        );
+
+        await _notificationRequestService.CreateAndSendAsync(
+            factory, cancellationToken);
     }
 }
