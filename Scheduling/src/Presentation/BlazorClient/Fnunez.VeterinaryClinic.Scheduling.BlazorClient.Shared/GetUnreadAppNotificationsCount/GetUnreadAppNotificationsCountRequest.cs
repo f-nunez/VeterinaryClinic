c983f41f384afdea8a03a169cd3cb3e1434a6eff
Francisco Nuñez <fonfonferi@gmail.com>
2023-03-20 19:48:13 -0700

    GetUnreadAppNotificationsCountRequest in Scheduling.BlazorClient.Shared #25

---
+++ Scheduling/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Shared/GetUnreadAppNotificationsCount/GetUnreadAppNotificationsCountRequest.cs
@@ -0,0 +1,7 @@
+using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Common;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.BlazorClient.Shared.GetUnreadAppNotificationsCount;
+
+public class GetUnreadAppNotificationsCountRequest : BaseRequest
+{
+}
