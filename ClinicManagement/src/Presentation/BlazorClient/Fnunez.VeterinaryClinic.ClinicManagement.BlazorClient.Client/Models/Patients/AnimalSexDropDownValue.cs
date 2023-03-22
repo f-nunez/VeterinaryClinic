f06e281b137d3504f10c2b9f4baf7d7ffbd8f6a3
Francisco Nuñez <fonfonferi@gmail.com>
2023-03-22 00:31:17 -0700

    Remove replaced class by DropDownValue in ClinicManagement.BlazorClient.Client #27

--- ClinicManagement/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client/Models/Patients/AnimalSexDropDownValue.cs
+++
@@ -1,7 +0,0 @@
-namespace Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client.Models.Patients;
-
-public class AnimalSexDropDownValue
-{
-    public string Text { get; set; } = string.Empty;
-    public int Value { get; set; }
-}
