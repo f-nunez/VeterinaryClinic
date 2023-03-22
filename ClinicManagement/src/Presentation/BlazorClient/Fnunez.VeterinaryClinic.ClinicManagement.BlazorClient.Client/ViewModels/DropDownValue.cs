0bdd59f44da1164e2ed0ffdcc4cb66a5ba5c112a
Francisco Nuñez <fonfonferi@gmail.com>
2023-03-22 00:20:42 -0700

    DropDownValue in ClinicManagement.BlazorClient.Client #27

---
+++ ClinicManagement/src/Presentation/BlazorClient/Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client/ViewModels/DropDownValue.cs
@@ -0,0 +1,7 @@
+namespace Fnunez.VeterinaryClinic.ClinicManagement.BlazorClient.Client.ViewModels;
+
+public class DropDownValue
+{
+    public string Text { get; set; } = string.Empty;
+    public int Value { get; set; }
+}
