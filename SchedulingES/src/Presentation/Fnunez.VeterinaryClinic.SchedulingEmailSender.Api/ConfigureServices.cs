1db893a4c307374b72454c8d1601ca6cd7fbd61a
Francisco Nuñez <fonfonferi@gmail.com>
2023-03-22 15:35:40 -0700

    SchedulingEmailSender skeleton #27

---
+++ SchedulingES/src/Presentation/Fnunez.VeterinaryClinic.SchedulingEmailSender.Api/ConfigureServices.cs
@@ -0,0 +1,39 @@
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddWebServices(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddControllers();
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddEndpointsApiExplorer();
+
+        services.AddSwaggerGen();
+
+        return services;
+    }
+
+    public static WebApplication AddWebApplicationBuilder(this WebApplication app)
+    {
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        return app;
+    }
+}
