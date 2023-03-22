e30912f3041eccac54b969a6a4a3a14c5c84bdde
Francisco Nuñez <fonfonferi@gmail.com>
2023-03-22 10:23:58 -0700

    Migration to add PreferredLanguage into Client aggregate in Scheduling.Infrastructure #27

--- Scheduling/src/Infrastructure/Fnunez.VeterinaryClinic.Scheduling.Infrastructure/Persistence/Migrations/20230322063230_Add_PreferredLanguage_Client.Designer.cs
+++ Scheduling/src/Infrastructure/Fnunez.VeterinaryClinic.Scheduling.Infrastructure/Persistence/Migrations/20230322063230_Add_PreferredLanguage_Client.Designer.cs
@@ -4,6 +4,7 @@ using Fnunez.VeterinaryClinic.Scheduling.Infrastructure.Persistence.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Fnunez.VeterinaryClinic.Scheduling.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230322063230_Add_PreferredLanguage_Client")]
+    partial class Add_PreferredLanguage_Client
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
