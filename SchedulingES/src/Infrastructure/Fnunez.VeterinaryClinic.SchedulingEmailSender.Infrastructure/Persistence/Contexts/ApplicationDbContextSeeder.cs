c9a8f487aac6195f69905b7ed814a96ffb7c5bc8
Francisco Nuñez <fonfonferi@gmail.com>
2023-03-22 16:25:51 -0700

    ApplicationDbContextSeeder in SchedulingEmailSender.Infrastructure #27

---
+++ SchedulingES/src/Infrastructure/Fnunez.VeterinaryClinic.SchedulingEmailSender.Infrastructure/Persistence/Contexts/ApplicationDbContextSeeder.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Fnunez.VeterinaryClinic.SchedulingEmailSender.Infrastructure.Persistence.Contexts;
+
+public class ApplicationDbContextSeeder
+{
+    private readonly ILogger<ApplicationDbContextSeeder> _logger;
+    private readonly ApplicationDbContext _context;
+
+    public ApplicationDbContextSeeder(
+        ILogger<ApplicationDbContextSeeder> logger,
+        ApplicationDbContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    public async Task MigrateAsync()
+    {
+        try
+        {
+            if (_context.Database.IsSqlServer())
+                await _context.Database.MigrateAsync();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred while initialising the database.");
+            throw;
+        }
+    }
+}
