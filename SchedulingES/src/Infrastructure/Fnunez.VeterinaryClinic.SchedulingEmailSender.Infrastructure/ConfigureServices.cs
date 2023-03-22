a7bc8145f54bcf36fd1f00c54b0e53782172e8a6
Francisco Nuñez <fonfonferi@gmail.com>
2023-03-22 16:28:30 -0700

    Add ApplicationDbContext to dependency container in SchedulingEmailSender.Infrastructure #27

--- SchedulingES/src/Infrastructure/Fnunez.VeterinaryClinic.SchedulingEmailSender.Infrastructure/ConfigureServices.cs
+++ SchedulingES/src/Infrastructure/Fnunez.VeterinaryClinic.SchedulingEmailSender.Infrastructure/ConfigureServices.cs
@@ -1,3 +1,5 @@
+using Fnunez.VeterinaryClinic.SchedulingEmailSender.Infrastructure.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
 namespace Microsoft.Extensions.DependencyInjection;
@@ -7,6 +9,20 @@ public static class ConfigureServices
     public static IServiceCollection AddInfrastructureServices(
         this IServiceCollection services, IConfiguration configuration)
     {
+        if (configuration.GetValue<bool>("UseInMemoryDatabase"))
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseInMemoryDatabase("SchedulingEmailSender")
+            );
+        else
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection"),
+                    builder => builder.MigrationsAssembly(
+                        typeof(ApplicationDbContext).Assembly.FullName
+                    )
+                )
+            );
+
         return services;
     }
 }
