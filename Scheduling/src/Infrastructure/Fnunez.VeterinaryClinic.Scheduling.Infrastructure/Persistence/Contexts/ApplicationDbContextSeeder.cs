822a456e9edd8804a9ada60516e70ced299e9cd9
Francisco Nuñez <fonfonferi@gmail.com>
2023-03-22 10:22:53 -0700

    Add missing data into ApplicationDbContextSeeder in Scheduling.Infrastructure #27

--- Scheduling/src/Infrastructure/Fnunez.VeterinaryClinic.Scheduling.Infrastructure/Persistence/Contexts/ApplicationDbContextSeeder.cs
+++ Scheduling/src/Infrastructure/Fnunez.VeterinaryClinic.Scheduling.Infrastructure/Persistence/Contexts/ApplicationDbContextSeeder.cs
@@ -176,6 +176,7 @@ public class ApplicationDbContextSeeder
                 "Chris",
                 "Mister",
                 "christian.demo@hotmail.com",
+                PreferredLanguage.English,
                 null
             )
         };
