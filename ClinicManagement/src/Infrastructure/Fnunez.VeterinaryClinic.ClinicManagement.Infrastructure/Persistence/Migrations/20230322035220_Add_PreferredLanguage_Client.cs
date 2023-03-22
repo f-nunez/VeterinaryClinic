bd538ed50869902ff80357f9a16455d8c8647d50
Francisco Nuñez <fonfonferi@gmail.com>
2023-03-22 00:14:25 -0700

    Migration to add PreferredLanguage into Client model in ClinicManagement.Infrastructure #27

---
+++ ClinicManagement/src/Infrastructure/Fnunez.VeterinaryClinic.ClinicManagement.Infrastructure/Persistence/Migrations/20230322035220_Add_PreferredLanguage_Client.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fnunez.VeterinaryClinic.ClinicManagement.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_PreferredLanguage_Client : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PreferredLanguage",
+                table: "Clients",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PreferredLanguage",
+                table: "Clients");
+        }
+    }
+}
