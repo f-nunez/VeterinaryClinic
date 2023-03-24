7d5c004bb3365f9156bbeaecc3f8ef02d7025bd7
Francisco Nuñez <fonfonferi@gmail.com>
2023-03-23 22:14:33 -0700

    Refactor into UpdateClinicCommandHandler in ClinicManagement.Application #27

--- ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Features/Clinics/Commands/UpdateClinic/UpdateClinicCommandHandler.cs
+++ ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Features/Clinics/Commands/UpdateClinic/UpdateClinicCommandHandler.cs
@@ -50,32 +50,52 @@ public class UpdateClinicCommandHandler
         if (clinicToUpdate is null)
             throw new NotFoundException(nameof(clinicToUpdate), request.Id);
 
-        clinicToUpdate.UpdateAddress(request.Address);
-        clinicToUpdate.UpdateEmailAddress(request.EmailAddress);
-        clinicToUpdate.UpdateName(request.Name);
-        clinicToUpdate.SetUpdatedBy(_currentUserService.UserId);
-
-        await _unitOfWork
-            .Repository<Clinic>()
-            .UpdateAsync(clinicToUpdate, cancellationToken);
-
-        await _unitOfWork.CommitAsync(cancellationToken);
+        await UpdateClinicAsync(request, clinicToUpdate, cancellationToken);
 
         response.Clinic = _mapper.Map<ClinicDto>(clinicToUpdate);
 
-        await SendIntegrationEventAsync(
-            clinicToUpdate,
-            request.CorrelationId,
-            cancellationToken
-        );
-
-        await SendNotificationRequestAsync(
+        await SendContractsToServiceBusAsync(
             clinicToUpdate,
             request.CorrelationId,
             cancellationToken
         );
 
         return response;
+    }
+
+    private async Task UpdateClinicAsync(
+        UpdateClinicRequest request,
+        Clinic clinic,
+        CancellationToken cancellationToken)
+    {
+        clinic.UpdateAddress(request.Address);
+        clinic.UpdateEmailAddress(request.EmailAddress);
+        clinic.UpdateName(request.Name);
+        clinic.SetUpdatedBy(_currentUserService.UserId);
+
+        await _unitOfWork
+            .Repository<Clinic>()
+            .UpdateAsync(clinic, cancellationToken);
+
+        await _unitOfWork.CommitAsync(cancellationToken);
+    }
+
+    private async Task SendContractsToServiceBusAsync(
+        Clinic clinic,
+        Guid correlationId,
+        CancellationToken cancellationToken)
+    {
+        await SendIntegrationEventAsync(
+            clinic,
+            correlationId,
+            cancellationToken
+        );
+
+        await SendNotificationRequestAsync(
+            clinic,
+            correlationId,
+            cancellationToken
+        );
     }
 
     private async Task SendIntegrationEventAsync(
