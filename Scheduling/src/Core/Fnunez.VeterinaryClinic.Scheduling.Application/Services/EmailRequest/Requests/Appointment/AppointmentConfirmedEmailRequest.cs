4a28dd1a1e6c58a1b35728c90de5f885f2e62d97
Francisco Nuñez <fonfonferi@gmail.com>
2023-03-24 20:14:27 -0700

    AppointmentConfirmedEmailRequest in Scheduling.Application #27

---
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application/Services/EmailRequest/Requests/Appointment/AppointmentConfirmedEmailRequest.cs
@@ -0,0 +1,13 @@
+namespace Fnunez.VeterinaryClinic.Scheduling.Application.Services.EmailRequest.Requests;
+
+public class AppointmentConfirmedEmailRequest : BaseEmailRequest
+{
+    public DateTimeOffset AppointmentEndOn { get; set; }
+    public Guid AppointmentId { get; set; }
+    public DateTimeOffset AppointmentStartOn { get; set; }
+    public string? ClientFullName { get; set; }
+    public string? ClinicAddress { get; set; }
+    public string? ClinicName { get; set; }
+    public string? DoctorFullName { get; set; }
+    public string? PatientName { get; set; }
+}
