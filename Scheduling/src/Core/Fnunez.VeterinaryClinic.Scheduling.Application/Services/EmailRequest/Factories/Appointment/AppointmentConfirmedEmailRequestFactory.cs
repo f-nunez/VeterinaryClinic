d89c58251cf78c4e284f8caeab7106a27c3b618e
Francisco Nuñez <fonfonferi@gmail.com>
2023-03-24 20:16:12 -0700

    AppointmentConfirmedEmailRequestFactory in Scheduling.Application #27

---
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application/Services/EmailRequest/Factories/Appointment/AppointmentConfirmedEmailRequestFactory.cs
@@ -0,0 +1,45 @@
+using Fnunez.VeterinaryClinic.Scheduling.Application.Services.EmailRequest.Requests;
+using Fnunez.VeterinaryClinic.Scheduling.Domain.AppointmentAggregate;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.Application.Services.EmailRequest.Factories;
+
+public class AppointmentConfirmedEmailRequestFactory : IEmailRequestFactory
+{
+    private readonly Appointment _appointment;
+    private readonly Guid _correlationId;
+    private readonly string? _userId;
+
+    public AppointmentConfirmedEmailRequestFactory(
+        Appointment appointment,
+        Guid correlationId,
+        string? userId)
+    {
+        _appointment = appointment;
+        _correlationId = correlationId;
+        _userId = userId;
+    }
+
+    public BaseEmailRequest CreateEmailRequest()
+    {
+        return new AppointmentConfirmedEmailRequest
+        {
+            AppointmentEndOn = _appointment.DateRange.EndOn,
+            AppointmentId = _appointment.Id,
+            AppointmentStartOn = _appointment.DateRange.StartOn,
+            ClientFullName = _appointment.Client.FullName,
+            ClinicAddress = _appointment.Clinic.Address,
+            ClinicName = _appointment.Clinic.Name,
+            CorrelationId = _correlationId,
+            DoctorFullName = _appointment.Doctor.FullName,
+            PatientName = _appointment.Patient.Name,
+            Language = _appointment.Client.PreferredLanguage.ToString(),
+            SendTo = _appointment.Client.EmailAddress,
+            TriggeredByUserId = _userId
+        };
+    }
+
+    public string GetEmailEvent()
+    {
+        return EmailEvent.AppointmentConfirmed.ToString();
+    }
+}
