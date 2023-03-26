a6fea26d3265f0eede80883faf14c8dec6f9351e
Francisco Nuñez <fonfonferi@gmail.com>
2023-03-25 17:42:25 -0700

    MassTransitServiceBus in SchedulingEmailSender.Infrastructure #27

---
+++ SchedulingES/src/Infrastructure/Fnunez.VeterinaryClinic.SchedulingEmailSender.Infrastructure/ServiceBus/MassTransitServiceBus.cs
@@ -0,0 +1,24 @@
+using Fnunez.VeterinaryClinic.SchedulingEmailSender.Application.Common.Interfaces;
+using MassTransit;
+
+namespace Fnunez.VeterinaryClinic.SchedulingEmailSender.Infrastructure.ServiceBus;
+
+public class MassTransitServiceBus : IServiceBus
+{
+    public IPublishEndpoint _publishEndpoint { get; set; }
+
+    public MassTransitServiceBus(IPublishEndpoint publishEndpoint)
+    {
+        _publishEndpoint = publishEndpoint;
+    }
+
+    public async Task PublishAsync<TMessage>(
+        TMessage message,
+        CancellationToken cancellationToken)
+    {
+        if (message is null)
+            throw new ArgumentNullException(nameof(message));
+
+        await _publishEndpoint.Publish(message, cancellationToken);
+    }
+}
