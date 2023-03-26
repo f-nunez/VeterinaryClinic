dd7f41b890de3ee98d4800e87950d5b049e85267
Francisco Nuñez <fonfonferi@gmail.com>
2023-03-25 17:36:36 -0700

    Migration to add RetryCount into Email entity in SchedulingEmailSender.Infrastructure #27

---
+++ SchedulingES/src/Infrastructure/Fnunez.VeterinaryClinic.SchedulingEmailSender.Infrastructure/Persistence/Migrations/20230323190602_Add_RetryCount_Email.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fnunez.VeterinaryClinic.SchedulingEmailSender.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_RetryCount_Email : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RetryCount",
+                table: "Emails",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RetryCount",
+                table: "Emails");
+        }
+    }
+}
