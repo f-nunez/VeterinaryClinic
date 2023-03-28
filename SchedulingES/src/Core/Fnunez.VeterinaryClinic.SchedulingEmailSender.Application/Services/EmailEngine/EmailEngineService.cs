c57ab1acf33299d642fe6af87cc25791b9e07124
Francisco Nuñez <fonfonferi@gmail.com>
2023-03-27 23:12:02 -0700

    EmailEngineService in SchedulingEmailSender.Application #27

---
+++ SchedulingES/src/Core/Fnunez.VeterinaryClinic.SchedulingEmailSender.Application/Services/EmailEngine/EmailEngineService.cs
@@ -0,0 +1,103 @@
+using System.Text.Json;
+using Fnunez.VeterinaryClinic.SchedulingEmailSender.Application.Services.EmailEngine.EmailCompositions;
+using Fnunez.VeterinaryClinic.SchedulingEmailSender.Application.Services.EmailEngine.Payloads;
+using Fnunez.VeterinaryClinic.SchedulingEmailSender.Application.Services.EmailEngine.Requests;
+using Fnunez.VeterinaryClinic.SchedulingEmailSender.Domain.EmailAggregate;
+using Fnunez.VeterinaryClinic.SchedulingEmailSender.Domain.EmailAggregate.Enums;
+using Fnunez.VeterinaryClinic.SharedKernel.Application.Repositories;
+using Microsoft.Extensions.Logging;
+
+namespace Fnunez.VeterinaryClinic.SchedulingEmailSender.Application.Services.EmailEngine;
+
+public class EmailEngineService : IEmailEngineService
+{
+    private readonly IEmailCompositionFactory _emailCompositionFactory;
+    private readonly IEmailRequestFactory _emailRequestFactory;
+    private readonly ILogger<EmailEngineService> _logger;
+    private readonly IPayloadFactory _payloadFactory;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public EmailEngineService(
+        IEmailCompositionFactory emailCompositionFactory,
+        IEmailRequestFactory emailRequestFactory,
+        ILogger<EmailEngineService> logger,
+        IPayloadFactory payloadFactory,
+        IUnitOfWork unitOfWork)
+    {
+        _emailCompositionFactory = emailCompositionFactory;
+        _emailRequestFactory = emailRequestFactory;
+        _logger = logger;
+        _payloadFactory = payloadFactory;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task CreateAndSendAsync(
+        string emailEventString,
+        string serializedEmailRequest,
+        CancellationToken cancellationToken)
+    {
+        if (string.IsNullOrEmpty(emailEventString))
+            throw new ArgumentNullException(nameof(emailEventString));
+
+        if (string.IsNullOrEmpty(serializedEmailRequest))
+            throw new ArgumentNullException(
+                nameof(serializedEmailRequest));
+
+        EmailEvent emailEvent = GetEmailEvent(emailEventString);
+
+        BaseEmailRequest emailRequest = _emailRequestFactory
+            .GetEmailRequest(emailEvent, serializedEmailRequest);
+
+        BasePayload payload = _payloadFactory
+            .GetPayload(emailEvent, emailRequest);
+
+        await CreateEmailAsync(
+           emailEvent, emailRequest, payload, cancellationToken);
+
+        EmailComposition emailComposition = await _emailCompositionFactory
+            .GetEmailCompositionAsync(emailEvent, payload);
+
+        try
+        {
+            //TODO: send email composition to EmailService queue
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex.Message, ex);
+        }
+    }
+
+    private async Task CreateEmailAsync(
+        EmailEvent emailEvent,
+        BaseEmailRequest emailRequest,
+        BasePayload payload,
+        CancellationToken cancellationToken)
+    {
+        var email = new Email(
+            emailRequest.CorrelationId,
+            emailRequest.SendTo,
+            DateTimeOffset.UtcNow,
+            emailEvent,
+            JsonSerializer.Serialize((object)payload),
+            emailRequest.TriggeredByUserId
+        );
+
+        await _unitOfWork
+            .Repository<Email>()
+            .AddAsync(email, cancellationToken);
+
+        await _unitOfWork.CommitAsync(cancellationToken);
+    }
+
+    private EmailEvent GetEmailEvent(string emailEventString)
+    {
+        bool isParsedEmailEvent = Enum.TryParse(
+            emailEventString, out EmailEvent emailEvent);
+
+        if (isParsedEmailEvent)
+            return emailEvent;
+
+        throw new ArgumentException(
+            $"{nameof(emailEventString)} not found with value: {emailEventString}");
+    }
+}
