4bef714240075582e982a7779dc9dec0a60b6d47
Francisco Nuñez <fonfonferi@gmail.com>
2023-03-31 16:55:05 -0700

    Enable Personally Identifiable Information logging in ClinicManagement.Api #29

--- ClinicManagement/src/Presentation/Fnunez.VeterinaryClinic.ClinicManagement.Api/ConfigureServices.cs
+++ ClinicManagement/src/Presentation/Fnunez.VeterinaryClinic.ClinicManagement.Api/ConfigureServices.cs
@@ -4,6 +4,7 @@ using Fnunez.VeterinaryClinic.ClinicManagement.Api.Settings;
 using Fnunez.VeterinaryClinic.ClinicManagement.Application.Common.Interfaces;
 using Fnunez.VeterinaryClinic.ClinicManagement.Infrastructure.Persistence.Contexts;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Logging;
 using Microsoft.IdentityModel.Tokens;
 
 namespace Microsoft.Extensions.DependencyInjection;
@@ -25,6 +26,9 @@ public static class ConfigureServices
         var corsPolicySetting = configuration
             .GetSection(typeof(CorsPolicySetting).Name)
             .Get<CorsPolicySetting>()!;
+
+        // ShowPII only for development stages
+        IdentityModelEventSource.ShowPII = true;
 
         services.AddHttpContextAccessor();
 
