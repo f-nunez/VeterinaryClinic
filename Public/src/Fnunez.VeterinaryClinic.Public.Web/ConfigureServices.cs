92f5ef34186c45844e1df52f482364d76dcfd07a
Francisco Nuñez <fonfonferi@gmail.com>
2023-04-02 18:31:49 -0700

    Add middleware according to Environment stack in Public.Web #29

--- Public/src/Fnunez.VeterinaryClinic.Public.Web/ConfigureServices.cs
+++ Public/src/Fnunez.VeterinaryClinic.Public.Web/ConfigureServices.cs
@@ -70,14 +70,23 @@ public static class ConfigureServices
     public static WebApplication AddWebApplicationBuilder(this WebApplication app)
     {
         // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
+        switch (app.Environment.EnvironmentName)
         {
-            app.UseExceptionHandler("/Home/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
+            case "DockerNginx":
+                app.UseDeveloperExceptionPage();
+                app.UseForwardedHeaders();
+                break;
+            case "DockerDevelopment":
+            case "Development":
+                app.UseDeveloperExceptionPage();
+                app.UseHsts();
+                app.UseHttpsRedirection();
+                break;
+            default:
+                app.UseHsts();
+                app.UseHttpsRedirection();
+                break;
         }
-
-        app.UseHttpsRedirection();
 
         app.UseStaticFiles();
 
