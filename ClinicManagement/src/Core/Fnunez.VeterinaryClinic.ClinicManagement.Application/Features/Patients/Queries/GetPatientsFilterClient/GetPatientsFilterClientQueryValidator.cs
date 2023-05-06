4f61d042bfc4fca22ffa452f99d9d18f353ae1ab
Francisco Nuñez <fonfonferi@gmail.com>
2023-05-05 21:23:26 -0700

    Update GetPatientsFilterClientQueryValidator in ClinicManagement.Application #36

--- ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Features/Patients/Queries/GetPatientsFilterClient/GetPatientsFilterClientQueryValidator.cs
+++ ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Features/Patients/Queries/GetPatientsFilterClient/GetPatientsFilterClientQueryValidator.cs
@@ -8,6 +8,13 @@ public class GetPatientsFilterClientQueryValidator
     public GetPatientsFilterClientQueryValidator()
     {
         RuleFor(v => v.GetPatientsFilterClientRequest.DataGridRequest.Search)
-            .MaximumLength(200).WithMessage("Search must be less or equals than 200 characters.");
+            .MaximumLength(200).WithMessage("Search must not exceed 200 characters.");
+
+        RuleFor(v => v.GetPatientsFilterClientRequest.DataGridRequest.Skip)
+            .GreaterThanOrEqualTo(0).WithMessage("Skip must be greater than or equal to 0.");
+
+        RuleFor(v => v.GetPatientsFilterClientRequest.DataGridRequest.Take)
+            .GreaterThanOrEqualTo(0).WithMessage("Take must be greater than or equal to 0.")
+            .LessThanOrEqualTo(100).WithMessage("Take must be less than or equal to 100.");
     }
 }
