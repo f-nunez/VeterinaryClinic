7a622e5ba5708e26763fe4125a2268f8efa4d3b0
Francisco Nuñez <fonfonferi@gmail.com>
2023-05-05 21:16:33 -0700

    Update GetAppointmentTypesFilterCodeQueryValidator in ClinicManagement.Application #36

--- ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Features/AppointmentTypes/Queries/GetAppointmentTypesFilterCode/GetAppointmentTypesFilterCodeQueryValidator.cs
+++ ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Features/AppointmentTypes/Queries/GetAppointmentTypesFilterCode/GetAppointmentTypesFilterCodeQueryValidator.cs
@@ -9,6 +9,7 @@ public class GetAppointmentTypesFilterCodeQueryValidator
     {
         RuleFor(v => v.GetAppointmentTypesFilterCodeRequest.CodeFilterValue)
             .NotNull().WithMessage("CodeFilterValue is required.")
-            .NotEmpty().WithMessage("CodeFilterValue is required.");
+            .NotEmpty().WithMessage("CodeFilterValue is required.")
+            .MaximumLength(200).WithMessage("CodeFilterValue must not exceed 200 characters.");
     }
 }
