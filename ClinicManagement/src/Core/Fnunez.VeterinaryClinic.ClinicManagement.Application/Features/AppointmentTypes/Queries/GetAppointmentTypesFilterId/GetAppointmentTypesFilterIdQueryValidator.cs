8889e4b1a7732c554684d011ff77dadd0eabbad5
Francisco Nuñez <fonfonferi@gmail.com>
2023-05-05 21:17:25 -0700

    Update GetAppointmentTypesFilterIdQueryValidator in ClinicManagement.Application #36

--- ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Features/AppointmentTypes/Queries/GetAppointmentTypesFilterId/GetAppointmentTypesFilterIdQueryValidator.cs
+++ ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Features/AppointmentTypes/Queries/GetAppointmentTypesFilterId/GetAppointmentTypesFilterIdQueryValidator.cs
@@ -9,6 +9,7 @@ public class GetAppointmentTypesFilterIdQueryValidator
     {
         RuleFor(v => v.GetAppointmentTypesFilterIdRequest.IdFilterValue)
             .NotNull().WithMessage("IdFilterValue is required.")
-            .NotEmpty().WithMessage("IdFilterValue is required.");
+            .NotEmpty().WithMessage("IdFilterValue is required.")
+            .MaximumLength(200).WithMessage("IdFilterValue must not exceed 200 characters.");
     }
 }
