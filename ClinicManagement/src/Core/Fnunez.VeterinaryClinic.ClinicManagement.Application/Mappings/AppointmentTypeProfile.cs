b0815a968c0bcd3cc38548d7a9060cc959bd99cc
Francisco Nuñez <fonfonferi@gmail.com>
2023-05-06 19:56:07 -0700

    Strict mapping rules into AppointmentTypeProfile in ClinicManagement.Application #36

--- ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Mappings/AppointmentTypeProfile.cs
+++ ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Mappings/AppointmentTypeProfile.cs
@@ -9,10 +9,88 @@ public class AppointmentTypeProfile : Profile
 {
     public AppointmentTypeProfile()
     {
-        CreateMap<AppointmentType, AppointmentTypeDto>();
+        CreateMap<AppointmentType, AppointmentTypeDto>()
+            .ForMember(
+                d => d.Code,
+                m => m.MapFrom(s => s.Code)
+            ).ForMember(
+                d => d.Duration,
+                m => m.MapFrom(s => s.Duration)
+            ).ForMember(
+                d => d.Id,
+                m => m.MapFrom(s => s.Id)
+            ).ForMember(
+                d => d.IsActive,
+                m => m.MapFrom(s => s.IsActive)
+            ).ForMember(
+                d => d.Name,
+                m => m.MapFrom(s => s.Name)
+            );
 
-        CreateMap<AppointmentTypeDto, AppointmentType>();
+        CreateMap<AppointmentTypeDto, AppointmentType>()
+            .ForMember(
+                d => d.Code,
+                m => m.MapFrom(s => s.Code)
+            ).ForMember(
+                d => d.CreatedBy,
+                m => m.Ignore()
+            ).ForMember(
+                d => d.CreatedOn,
+                m => m.Ignore()
+            ).ForMember(
+                d => d.DomainEvents,
+                m => m.Ignore()
+            ).ForMember(
+                d => d.Duration,
+                m => m.MapFrom(s => s.Duration)
+            ).ForMember(
+                d => d.Id,
+                m => m.MapFrom(s => s.Id)
+            ).ForMember(
+                d => d.IsActive,
+                m => m.MapFrom(s => s.IsActive)
+            ).ForMember(
+                d => d.Name,
+                m => m.MapFrom(s => s.Name)
+            ).ForMember(
+                d => d.UpdatedBy,
+                m => m.Ignore()
+            ).ForMember(
+                d => d.UpdatedOn,
+                m => m.Ignore()
+            );
 
-        CreateMap<CreateAppointmentTypeRequest, AppointmentType>();
+        CreateMap<CreateAppointmentTypeRequest, AppointmentType>()
+            .ForMember(
+                d => d.Code,
+                m => m.MapFrom(s => s.Code)
+            ).ForMember(
+                d => d.CreatedBy,
+                m => m.Ignore()
+            ).ForMember(
+                d => d.CreatedOn,
+                m => m.Ignore()
+            ).ForMember(
+                d => d.DomainEvents,
+                m => m.Ignore()
+            ).ForMember(
+                d => d.Duration,
+                m => m.MapFrom(s => s.Duration)
+            ).ForMember(
+                d => d.Id,
+                m => m.Ignore()
+            ).ForMember(
+                d => d.IsActive,
+                m => m.Ignore()
+            ).ForMember(
+                d => d.Name,
+                m => m.MapFrom(s => s.Name)
+            ).ForMember(
+                d => d.UpdatedBy,
+                m => m.Ignore()
+            ).ForMember(
+                d => d.UpdatedOn,
+                m => m.Ignore()
+            );
     }
 }
