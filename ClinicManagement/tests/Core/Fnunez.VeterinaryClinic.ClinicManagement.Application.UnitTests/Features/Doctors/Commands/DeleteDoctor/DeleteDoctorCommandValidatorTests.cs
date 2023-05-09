33d6577067443bb6d7b3816445819d524219a36a
Francisco Nuñez <fonfonferi@gmail.com>
2023-05-08 22:32:37 -0700

    DeleteDoctorCommandValidatorTests in ClinicManagement.Application.UnitTests #36

---
+++ ClinicManagement/tests/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application.UnitTests/Features/Doctors/Commands/DeleteDoctor/DeleteDoctorCommandValidatorTests.cs
@@ -0,0 +1,49 @@
+using FluentValidation.TestHelper;
+using Fnunez.VeterinaryClinic.ClinicManagement.Application.Features.Doctors.Commands.DeleteDoctor;
+using Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel.Doctor.DeleteDoctor;
+
+public class DeleteDoctorCommandValidatorTests
+{
+    private readonly DeleteDoctorCommandValidator _validator;
+
+    public DeleteDoctorCommandValidatorTests()
+    {
+        _validator = new();
+    }
+
+    [Fact]
+    public void Validation_IdIsGreaterThanZero_IsValid()
+    {
+        // Arrange
+        int id = 1;
+
+        var request = new DeleteDoctorRequest { Id = id };
+
+        var command = new DeleteDoctorCommand(request);
+
+        //Act
+        var validationResult = _validator.TestValidate(command);
+
+        //Assert
+        validationResult.ShouldNotHaveValidationErrorFor(x =>
+            x.DeleteDoctorRequest.Id);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void Validation_IdIsLessOrEqualsThanZero_Fails(int id)
+    {
+        // Arrange
+        var request = new DeleteDoctorRequest { Id = id };
+
+        var command = new DeleteDoctorCommand(request);
+
+        //Act
+        var validationResult = _validator.TestValidate(command);
+
+        //Assert
+        validationResult.ShouldHaveValidationErrorFor(x =>
+            x.DeleteDoctorRequest.Id);
+    }
+}
