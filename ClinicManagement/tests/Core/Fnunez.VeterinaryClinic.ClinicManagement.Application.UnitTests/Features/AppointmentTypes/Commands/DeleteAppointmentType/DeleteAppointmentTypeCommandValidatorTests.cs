0ac3bbe0d11123009aa579ae7f24137db0a89331
Francisco Nuñez <fonfonferi@gmail.com>
2023-05-08 22:22:14 -0700

    DeleteAppointmentTypeCommandValidatorTests in ClinicManagement.Application.UnitTests #36

---
+++ ClinicManagement/tests/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application.UnitTests/Features/AppointmentTypes/Commands/DeleteAppointmentType/DeleteAppointmentTypeCommandValidatorTests.cs
@@ -0,0 +1,51 @@
+using FluentValidation.TestHelper;
+using Fnunez.VeterinaryClinic.ClinicManagement.Application.Features.AppointmentTypes.Commands.DeleteAppointmentType;
+using Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel.AppointmentType.DeleteAppointmentType;
+
+namespace Fnunez.VeterinaryClinic.ClinicManagement.Application.UnitTests.Features.AppointmentTypes.Commands.DeleteAppointmentType;
+
+public class DeleteAppointmentTypeCommandValidatorTests
+{
+    private readonly DeleteAppointmentTypeCommandValidator _validator;
+
+    public DeleteAppointmentTypeCommandValidatorTests()
+    {
+        _validator = new();
+    }
+
+    [Fact]
+    public void Validation_IdIsGreaterThanZero_IsValid()
+    {
+        // Arrange
+        int id = 1;
+
+        var request = new DeleteAppointmentTypeRequest { Id = id };
+
+        var command = new DeleteAppointmentTypeCommand(request);
+
+        // Act
+        var validationResult = _validator.TestValidate(command);
+
+        //Assert
+        validationResult.ShouldNotHaveValidationErrorFor(x =>
+            x.DeleteAppointmentTypeRequest.Id);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void Validation_IdIsLessOrEqualsThanZero_Fails(int id)
+    {
+        // Arrange
+        var request = new DeleteAppointmentTypeRequest { Id = id };
+
+        var command = new DeleteAppointmentTypeCommand(request);
+
+        // Act
+        var validationResult = _validator.TestValidate(command);
+
+        //Assert
+        validationResult.ShouldHaveValidationErrorFor(x =>
+            x.DeleteAppointmentTypeRequest.Id);
+    }
+}
