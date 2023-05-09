42bf32b8ad10a56fa528042fca1a37d125fa6727
Francisco Nuñez <fonfonferi@gmail.com>
2023-05-08 22:33:06 -0700

    GetDoctorByIdQueryValidatorTests in ClinicManagement.Application.UnitTests #36

---
+++ ClinicManagement/tests/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application.UnitTests/Features/Doctors/Queries/GetDoctorById/GetDoctorByIdQueryValidatorTests.cs
@@ -0,0 +1,51 @@
+using FluentValidation.TestHelper;
+using Fnunez.VeterinaryClinic.ClinicManagement.Application.Features.Doctors.Queries.GetDoctorById;
+using Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel.Doctor.GetDoctorById;
+
+namespace Fnunez.VeterinaryClinic.ClinicManagement.Application.UnitTests.Features.Doctors.Queries.GetDoctorById;
+
+public class GetDoctorByIdQueryValidatorTests
+{
+    private readonly GetDoctorByIdQueryValidator _validator;
+
+    public GetDoctorByIdQueryValidatorTests()
+    {
+        _validator = new();
+    }
+
+    [Fact]
+    public void Valdiation_IdIsGreaterThanZero_IsValid()
+    {
+        // Arrange
+        int id = 1;
+
+        var request = new GetDoctorByIdRequest { Id = id };
+
+        var query = new GetDoctorByIdQuery(request);
+
+        // Act
+        var validationResult = _validator.TestValidate(query);
+
+        // Assert
+        validationResult.ShouldNotHaveValidationErrorFor(x =>
+            x.GetDoctorByIdRequest.Id);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void Valdiation_IdIsLessOrEqualsThanZero_Fails(int id)
+    {
+        // Arrange
+        var request = new GetDoctorByIdRequest { Id = id };
+
+        var query = new GetDoctorByIdQuery(request);
+
+        // Act
+        var validationResult = _validator.TestValidate(query);
+
+        // Assert
+        validationResult.ShouldHaveValidationErrorFor(x =>
+            x.GetDoctorByIdRequest.Id);
+    }
+}
