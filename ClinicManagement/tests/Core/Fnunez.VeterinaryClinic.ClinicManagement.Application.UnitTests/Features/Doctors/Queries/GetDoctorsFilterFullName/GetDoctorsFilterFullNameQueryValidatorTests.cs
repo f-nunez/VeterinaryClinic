1899a199a6a457217493797b7611a3b2c59c3f18
Francisco Nuñez <fonfonferi@gmail.com>
2023-05-08 22:33:45 -0700

    GetDoctorsFilterFullNameQueryValidatorTests in ClinicManagement.Application.UnitTests #36

---
+++ ClinicManagement/tests/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application.UnitTests/Features/Doctors/Queries/GetDoctorsFilterFullName/GetDoctorsFilterFullNameQueryValidatorTests.cs
@@ -0,0 +1,61 @@
+using FluentValidation.TestHelper;
+using Fnunez.VeterinaryClinic.ClinicManagement.Application.Features.Doctors.Queries.GetDoctorsFilterFullName;
+using Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel.Doctor.GetDoctorsFilterFullName;
+
+namespace Fnunez.VeterinaryClinic.ClinicManagement.Application.UnitTests.Features.Doctors.Queries.GetDoctorsFilterFullName;
+
+public class GetDoctorsFilterFullNameQueryValidatorTests
+{
+    private readonly GetDoctorsFilterFullNameQueryValidator _validator;
+
+    public GetDoctorsFilterFullNameQueryValidatorTests()
+    {
+        _validator = new();
+    }
+
+    [Fact]
+    public void Validation_FullNameFilterValueHasMoreThanTwoHundredCharacters_Fails()
+    {
+        // Arrange
+        var fullNameFilterValue = string.Empty;
+
+        for (int i = 0; i < 201; i++)
+            fullNameFilterValue += "a";
+
+        var request = new GetDoctorsFilterFullNameRequest
+        {
+            FullNameFilterValue = fullNameFilterValue
+        };
+
+        var query = new GetDoctorsFilterFullNameQuery(request);
+
+        // Act
+        var validationResult = _validator.TestValidate(query);
+
+        // Assert
+        validationResult.ShouldHaveValidationErrorFor(x =>
+            x.GetDoctorsFilterFullNameRequest.FullNameFilterValue);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    public void Validation_FullFullNameFilterValueIsEmpty_Fails(
+        string fullFullNameFilterValue)
+    {
+        // Arrange
+        var request = new GetDoctorsFilterFullNameRequest
+        {
+            FullNameFilterValue = fullFullNameFilterValue
+        };
+
+        var query = new GetDoctorsFilterFullNameQuery(request);
+
+        // Act
+        var validationResult = _validator.TestValidate(query);
+
+        // Assert
+        validationResult.ShouldHaveValidationErrorFor(x =>
+            x.GetDoctorsFilterFullNameRequest.FullNameFilterValue);
+    }
+}
