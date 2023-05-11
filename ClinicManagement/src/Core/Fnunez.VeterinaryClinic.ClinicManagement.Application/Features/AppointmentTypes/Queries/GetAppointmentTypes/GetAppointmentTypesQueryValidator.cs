bfb60fdbd4e156c68874e2afb4eb4a64a77313ce
Francisco Nuñez <fonfonferi@gmail.com>
2023-05-11 13:43:18 -0700

    Update GetAppointmentTypesQueryValidator in ClinicManagement.Application #36

--- ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Features/AppointmentTypes/Queries/GetAppointmentTypes/GetAppointmentTypesQueryValidator.cs
+++ ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Features/AppointmentTypes/Queries/GetAppointmentTypes/GetAppointmentTypesQueryValidator.cs
@@ -17,7 +17,7 @@ public class GetAppointmentTypesQueryValidator
             .GreaterThanOrEqualTo(0).WithMessage("Skip must be greater than or equal to 0.");
 
         RuleFor(v => v.GetAppointmentTypesRequest.DataGridRequest.Take)
-            .GreaterThanOrEqualTo(0).WithMessage("Take must be greater than or equal to 0.")
+            .GreaterThan(0).WithMessage("Take must be greater than 0.")
             .LessThanOrEqualTo(100).WithMessage("Take must be less than or equal to 100.");
 
         RuleFor(v => v.GetAppointmentTypesRequest.DurationFilterValue)
