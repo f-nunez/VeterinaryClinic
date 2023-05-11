a531bf7e483f972401e241e595fa00ff40b1b9bc
Francisco Nuñez <fonfonferi@gmail.com>
2023-05-11 13:47:47 -0700

    Update GetRoomsQueryValidator in ClinicManagement.Application #36

--- ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Features/Rooms/Queries/GetRooms/GetRoomsQueryValidator.cs
+++ ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Features/Rooms/Queries/GetRooms/GetRoomsQueryValidator.cs
@@ -13,7 +13,7 @@ public class GetRoomsQueryValidator : AbstractValidator<GetRoomsQuery>
             .GreaterThanOrEqualTo(0).WithMessage("Skip must be greater than or equal to 0.");
 
         RuleFor(v => v.GetRoomsRequest.DataGridRequest.Take)
-            .GreaterThanOrEqualTo(0).WithMessage("Take must be greater than or equal to 0.")
+            .GreaterThan(0).WithMessage("Take must be greater than 0.")
             .LessThanOrEqualTo(100).WithMessage("Take must be less than or equal to 100.");
 
         RuleFor(v => v.GetRoomsRequest.IdFilterValue)
