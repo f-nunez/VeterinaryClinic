039f8035c78f6197e049bbc585a015274f0ead6f
Francisco Nuñez <fonfonferi@gmail.com>
2023-05-11 13:46:38 -0700

    Update GetDoctorsQueryValidator in ClinicManagement.Application #36

--- ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Features/Doctors/Queries/GetDoctors/GetDoctorsQueryValidator.cs
+++ ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Features/Doctors/Queries/GetDoctors/GetDoctorsQueryValidator.cs
@@ -13,7 +13,7 @@ public class GetDoctorsQueryValidator : AbstractValidator<GetDoctorsQuery>
             .GreaterThanOrEqualTo(0).WithMessage("Skip must be greater than or equal to 0.");
 
         RuleFor(v => v.GetDoctorsRequest.DataGridRequest.Take)
-            .GreaterThanOrEqualTo(0).WithMessage("Take must be greater than or equal to 0.")
+            .GreaterThan(0).WithMessage("Take must be greater than 0.")
             .LessThanOrEqualTo(100).WithMessage("Take must be less than or equal to 100.");
 
         RuleFor(v => v.GetDoctorsRequest.FullNameFilterValue)
