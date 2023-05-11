783a9d8173441aee9000d7b83403bcbe2285b0ab
Francisco Nuñez <fonfonferi@gmail.com>
2023-05-11 13:45:55 -0700

    Update GetClientsFilterPreferredDoctorQueryValidator in ClinicManagement.Application #36

--- ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Features/Clients/Queries/GetClientsFilterPreferredDoctor/GetClientsFilterPreferredDoctorQueryValidator.cs
+++ ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Features/Clients/Queries/GetClientsFilterPreferredDoctor/GetClientsFilterPreferredDoctorQueryValidator.cs
@@ -9,5 +9,12 @@ public class GetClientsFilterPreferredDoctorQueryValidator
     {
         RuleFor(v => v.GetClientsFilterPreferredDoctorRequest.DataGridRequest.Search)
             .MaximumLength(200).WithMessage("Search must not exceed 200 characters.");
+
+        RuleFor(v => v.GetClientsFilterPreferredDoctorRequest.DataGridRequest.Skip)
+            .GreaterThanOrEqualTo(0).WithMessage("Skip must be greater than or equal to 0.");
+
+        RuleFor(v => v.GetClientsFilterPreferredDoctorRequest.DataGridRequest.Take)
+            .GreaterThan(0).WithMessage("Take must be greater than 0.")
+            .LessThanOrEqualTo(100).WithMessage("Take must be less than or equal to 100.");
     }
 }
