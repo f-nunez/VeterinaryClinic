03939bcd4d170a1de595f75d1f2b3b5e1c78af80
Francisco Nuñez <fonfonferi@gmail.com>
2023-05-14 13:25:09 -0700

    GetClinicsFilterEmailAddressQueryHandlerTests in ClinicManagement.Application.IntegrationTests #36

---
+++ ClinicManagement/tests/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application.IntegrationTests/Features/Clinics/Queries/GetClinicsFilterEmailAddress/GetClinicsFilterEmailAddressQueryHandlerTests.cs
@@ -0,0 +1,73 @@
+using Fnunez.VeterinaryClinic.ClinicManagement.Application.Common.Exceptions;
+using Fnunez.VeterinaryClinic.ClinicManagement.Application.Features.Clinics.Queries.GetClinicsFilterEmailAddress;
+using Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel.Clinic.GetClinicsFilterEmailAddress;
+using Fnunez.VeterinaryClinic.ClinicManagement.Domain.ClinicAggregate;
+
+namespace Fnunez.VeterinaryClinic.ClinicManagement.Application.IntegrationTests.Features.Clinics.Queries.GetClinicsFilterEmailAddress;
+
+[Collection(nameof(TestContextFixture))]
+public class GetClinicsFilterEmailAddressQueryHandlerTests
+    : IClassFixture<TestContextFixture>
+{
+    private readonly TestContextFixture _fixture;
+
+    public GetClinicsFilterEmailAddressQueryHandlerTests(
+        TestContextFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact]
+    public async Task Handle_RequiredEntriesAreEmpty_ThrowsValidationException()
+    {
+        // Arrange
+        var request = new GetClinicsFilterEmailAddressRequest();
+
+        var query = new GetClinicsFilterEmailAddressQuery(request);
+
+        // Act
+        var actual = await Assert.ThrowsAsync<ValidationException>(() =>
+            _fixture.SendAsync(query));
+
+        // Assert
+        Assert.NotNull(actual);
+
+        Assert.IsType<ValidationException>(actual);
+    }
+
+    [Fact]
+    public async Task Handle_ReturnsGetClinicsFilterEmailAddressResponse()
+    {
+        // Arrange
+        var address = "a";
+
+        var emailAddress = "test@nunez.ninja";
+
+        var name = "b";
+
+        var clinic = new Clinic(address, emailAddress, name);
+
+        await _fixture.AddAsync<Clinic>(clinic);
+
+        var request = new GetClinicsFilterEmailAddressRequest
+        {
+            EmailAddressFilterValue = emailAddress
+        };
+
+        var query = new GetClinicsFilterEmailAddressQuery(request);
+
+        // Act
+        var actual = await _fixture.SendAsync(query);
+
+        // Assert
+        Assert.NotNull(actual);
+
+        Assert.IsType<GetClinicsFilterEmailAddressResponse>(actual);
+
+        Assert.NotNull(actual.ClinicEmailAddresses);
+
+        Assert.NotEmpty(actual.ClinicEmailAddresses);
+
+        Assert.StartsWith(emailAddress, actual.ClinicEmailAddresses.FirstOrDefault());
+    }
+}
