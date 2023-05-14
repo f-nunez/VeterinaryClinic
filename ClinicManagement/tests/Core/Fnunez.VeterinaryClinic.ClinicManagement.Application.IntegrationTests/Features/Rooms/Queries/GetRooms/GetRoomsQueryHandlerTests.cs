abd4efb09b7c7aef5b3ab915d56f0c04544db690
Francisco Nuñez <fonfonferi@gmail.com>
2023-05-14 13:30:32 -0700

    GetRoomsQueryHandlerTests in ClinicManagement.Application.IntegrationTests #36

---
+++ ClinicManagement/tests/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application.IntegrationTests/Features/Rooms/Queries/GetRooms/GetRoomsQueryHandlerTests.cs
@@ -0,0 +1,70 @@
+using Fnunez.VeterinaryClinic.ClinicManagement.Application.Common.Exceptions;
+using Fnunez.VeterinaryClinic.ClinicManagement.Application.Features.Rooms.Queries.GetRooms;
+using Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel.Common;
+using Fnunez.VeterinaryClinic.ClinicManagement.Application.SharedModel.Room.GetRooms;
+using Fnunez.VeterinaryClinic.ClinicManagement.Domain.RoomAggregate;
+
+namespace Fnunez.VeterinaryClinic.ClinicManagement.Application.IntegrationTests.Features.Rooms.Queries.GetRooms;
+
+[Collection(nameof(TestContextFixture))]
+public class GetRoomsQueryHandlerTests : IClassFixture<TestContextFixture>
+{
+    private readonly TestContextFixture _fixture;
+
+    public GetRoomsQueryHandlerTests(TestContextFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact]
+    public async Task Handle_RequiredEntriesAreEmpty_ThrowsValidationException()
+    {
+        // Arrange
+        var request = new GetRoomsRequest();
+
+        var query = new GetRoomsQuery(request);
+
+        // Act
+        var actual = await Assert.ThrowsAsync<ValidationException>(() =>
+            _fixture.SendAsync(query));
+
+        // Assert
+        Assert.NotNull(actual);
+
+        Assert.IsType<ValidationException>(actual);
+    }
+
+    [Fact]
+    public async Task Handle_ReturnsGetRoomsResponse()
+    {
+        // Arrange
+        var name = "a";
+
+        var room = new Room(name);
+
+        await _fixture.AddAsync<Room>(room);
+
+        var take = 1;
+
+        var request = new GetRoomsRequest
+        {
+            DataGridRequest = new DataGridRequest { Take = take }
+        };
+
+        var query = new GetRoomsQuery(request);
+
+        // Act
+        var actual = await _fixture.SendAsync(query);
+
+        // Assert
+        Assert.NotNull(actual);
+
+        Assert.IsType<GetRoomsResponse>(actual);
+
+        Assert.NotNull(actual.DataGridResponse);
+
+        Assert.NotEmpty(actual.DataGridResponse.Items);
+
+        Assert.Equal(take, actual.DataGridResponse.Items.Count);
+    }
+}
