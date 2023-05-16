60b2a3914fd92f9acacb25b7362d0ddfda0c271d
Francisco Nuñez <fonfonferi@gmail.com>
2023-05-16 12:01:05 -0700

    GetRoomsFilterNameQueryValidatorTests in Scheduling.Application.UnitTests #36
    Test cases

---
+++ Scheduling/tests/Core/Fnunez.VeterinaryClinic.Scheduling.Application.UnitTests/Features/Rooms/Queries/GetRoomsFilterName/GetRoomsFilterNameQueryValidatorTests.cs
@@ -0,0 +1,60 @@
+using FluentValidation.TestHelper;
+using Fnunez.VeterinaryClinic.Scheduling.Application.Features.Rooms.Queries.GetRoomsFilterName;
+using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Room.GetRoomsFilterName;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.Application.UnitTests.Features.Rooms.Queries.GetRoomsFilterName;
+
+public class GetRoomsFilterNameQueryValidatorTests
+{
+    private readonly GetRoomsFilterNameQueryValidator _validator;
+
+    public GetRoomsFilterNameQueryValidatorTests()
+    {
+        _validator = new();
+    }
+
+    [Fact]
+    public void Validation_NameFilterValueHasMoreThanTwoHundredCharacters_Fails()
+    {
+        // Arrange
+        var nameFilterValue = string.Empty;
+
+        for (int i = 0; i < 201; i++)
+            nameFilterValue += "a";
+
+        var request = new GetRoomsFilterNameRequest
+        {
+            NameFilterValue = nameFilterValue
+        };
+
+        var query = new GetRoomsFilterNameQuery(request);
+
+        // Act
+        var validationResult = _validator.TestValidate(query);
+
+        // Assert
+        validationResult.ShouldHaveValidationErrorFor(x =>
+            x.GetRoomsFilterNameRequest.NameFilterValue);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    public void Validation_NameFilterValueIsEmpty_Fails(string nameFilterValue)
+    {
+        // Arrange
+        var request = new GetRoomsFilterNameRequest
+        {
+            NameFilterValue = nameFilterValue
+        };
+
+        var query = new GetRoomsFilterNameQuery(request);
+
+        // Act
+        var validationResult = _validator.TestValidate(query);
+
+        // Assert
+        validationResult.ShouldHaveValidationErrorFor(x =>
+            x.GetRoomsFilterNameRequest.NameFilterValue);
+    }
+}
