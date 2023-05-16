adc3fdbaeeba5ed19d957385458137de077b1466
Francisco Nuñez <fonfonferi@gmail.com>
2023-05-16 11:53:50 -0700

    GetAppointmentsFilterDoctorQueryValidatorTests in Scheduling.Application.UnitTests #36
    Test cases

---
+++ Scheduling/tests/Core/Fnunez.VeterinaryClinic.Scheduling.Application.UnitTests/Features/Appointments/Queries/GetAppointmentsFilterDoctor/GetAppointmentsFilterDoctorQueryHandlerValidatorTests.cs
@@ -0,0 +1,111 @@
+using FluentValidation.TestHelper;
+using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Appointment.GetAppointmentsFilterDoctor;
+using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Common;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.Application.Features.Appointments.Queries.GetAppointmentsFilterDoctor;
+
+public class GetAppointmentsFilterDoctorQueryHandlerValidatorTests
+{
+    private readonly GetAppointmentsFilterDoctorQueryHandlerValidator _validator;
+
+    public GetAppointmentsFilterDoctorQueryHandlerValidatorTests()
+    {
+        _validator = new();
+    }
+
+    [Fact]
+    public void Validation_DataGridRequestSearchHasMoreThanTwoHundredCharacters_Fails()
+    {
+        // Arrange
+        var search = string.Empty;
+
+        for (int i = 0; i < 201; i++)
+            search += "a";
+
+        var dataGridRequest = new DataGridRequest { Search = search };
+
+        var request = new GetAppointmentsFilterDoctorRequest
+        {
+            DataGridRequest = dataGridRequest
+        };
+
+        var query = new GetAppointmentsFilterDoctorQuery(request);
+
+        // Act
+        var validationResult = _validator.TestValidate(query);
+
+        // Assert
+        validationResult.ShouldHaveValidationErrorFor(x =>
+            x.GetAppointmentsFilterDoctorRequest.DataGridRequest.Search);
+    }
+
+    [Fact]
+    public void Validation_DataGridRequestSkipIsLessThanZero_Fails()
+    {
+        // Arrange
+        int skip = -1;
+
+        var dataGridRequest = new DataGridRequest { Skip = skip };
+
+        var request = new GetAppointmentsFilterDoctorRequest
+        {
+            DataGridRequest = dataGridRequest
+        };
+
+        var query = new GetAppointmentsFilterDoctorQuery(request);
+
+        // Act
+        var validationResult = _validator.TestValidate(query);
+
+        // Assert
+        validationResult.ShouldHaveValidationErrorFor(x =>
+            x.GetAppointmentsFilterDoctorRequest.DataGridRequest.Skip);
+    }
+
+    [Fact]
+    public void Validation_DataGridRequestTakeIsGreaterThanOnehundred_Fails()
+    {
+        // Arrange
+        int take = 101;
+
+        var dataGridRequest = new DataGridRequest { Take = take };
+
+        var request = new GetAppointmentsFilterDoctorRequest
+        {
+            DataGridRequest = dataGridRequest
+        };
+
+        var query = new GetAppointmentsFilterDoctorQuery(request);
+
+        // Act
+        var validationResult = _validator.TestValidate(query);
+
+        // Assert
+        validationResult.ShouldHaveValidationErrorFor(x =>
+            x.GetAppointmentsFilterDoctorRequest.DataGridRequest.Take);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void Validation_DataGridRequestTakeIsLessThanOrEqualToZero_Fails(
+        int take)
+    {
+        // Arrange
+        var dataGridRequest = new DataGridRequest { Take = take };
+
+        var request = new GetAppointmentsFilterDoctorRequest
+        {
+            DataGridRequest = dataGridRequest
+        };
+
+        var query = new GetAppointmentsFilterDoctorQuery(request);
+
+        // Act
+        var validationResult = _validator.TestValidate(query);
+
+        // Assert
+        validationResult.ShouldHaveValidationErrorFor(x =>
+            x.GetAppointmentsFilterDoctorRequest.DataGridRequest.Take);
+    }
+}
