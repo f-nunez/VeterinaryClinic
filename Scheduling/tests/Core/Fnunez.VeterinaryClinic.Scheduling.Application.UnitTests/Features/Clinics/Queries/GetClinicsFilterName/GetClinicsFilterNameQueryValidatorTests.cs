fab9c4001e9706b091aca1d53d66b53a0b6baa9e
Francisco Nuñez <fonfonferi@gmail.com>
2023-05-16 11:58:37 -0700

    GetClinicsFilterNameQueryValidatorTests in Scheduling.Application.UnitTests #36
    Test cases

---
+++ Scheduling/tests/Core/Fnunez.VeterinaryClinic.Scheduling.Application.UnitTests/Features/Clinics/Queries/GetClinicsFilterName/GetClinicsFilterNameQueryValidatorTests.cs
@@ -0,0 +1,60 @@
+using FluentValidation.TestHelper;
+using Fnunez.VeterinaryClinic.Scheduling.Application.Features.Clinics.Queries.GetClinicsFilterName;
+using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Clinic.GetClinicsFilterName;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.Application.UnitTests.Features.Clinics.Queries.GetClinicsFilterName;
+
+public class GetClinicsFilterNameQueryValidatorTests
+{
+    private readonly GetClinicsFilterNameQueryValidator _validator;
+
+    public GetClinicsFilterNameQueryValidatorTests()
+    {
+        _validator = new();
+    }
+
+    [Fact]
+    public void Validation_NameFilterValueHasMoreThanTwoHundredCharacters_Fails()
+    {
+        // Arrange
+        var nameFilterValue = string.Empty;
+
+        for (int i = 0; i < 201; i++)
+            nameFilterValue += "a";
+
+        var request = new GetClinicsFilterNameRequest
+        {
+            NameFilterValue = nameFilterValue
+        };
+
+        var query = new GetClinicsFilterNameQuery(request);
+
+        // Act
+        var validationResult = _validator.TestValidate(query);
+
+        // Assert
+        validationResult.ShouldHaveValidationErrorFor(x =>
+            x.GetClinicsFilterNameRequest.NameFilterValue);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    public void Validation_NameFilterValueIsEmpty_Fails(string nameFilterValue)
+    {
+        // Arrange
+        var request = new GetClinicsFilterNameRequest
+        {
+            NameFilterValue = nameFilterValue
+        };
+
+        var query = new GetClinicsFilterNameQuery(request);
+
+        // Act
+        var validationResult = _validator.TestValidate(query);
+
+        // Assert
+        validationResult.ShouldHaveValidationErrorFor(x =>
+            x.GetClinicsFilterNameRequest.NameFilterValue);
+    }
+}
