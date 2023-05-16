eee9065b90473c8a75ac79064abc11aeefdfdcf0
Francisco Nuñez <fonfonferi@gmail.com>
2023-05-16 11:55:23 -0700

    GetAppointmentTypesFilterDurationQueryValidatorTests in Scheduling.Application.UnitTests #36
    Test cases

---
+++ Scheduling/tests/Core/Fnunez.VeterinaryClinic.Scheduling.Application.UnitTests/Features/AppointmentTypes/Queries/GetAppointmentTypesFilterDuration/GetAppointmentTypesFilterDurationQueryValidatorTests.cs
@@ -0,0 +1,61 @@
+using FluentValidation.TestHelper;
+using Fnunez.VeterinaryClinic.Scheduling.Application.Features.AppointmentTypes.Queries.GetAppointmentTypesFilterDuration;
+using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.AppointmentType.GetAppointmentTypesFilterDuration;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.Application.UnitTests.Features.AppointmentTypes.Queries.GetAppointmentTypesFilterDuration;
+
+public class GetAppointmentTypesFilterDurationQueryValidatorTests
+{
+    private readonly GetAppointmentTypesFilterDurationQueryValidator _validator;
+
+    public GetAppointmentTypesFilterDurationQueryValidatorTests()
+    {
+        _validator = new();
+    }
+
+    [Fact]
+    public void Validation_DurationFilterValueHasMoreThanTwoHundredCharacters_Fails()
+    {
+        // Arrange
+        var durationFilterValue = string.Empty;
+
+        for (int i = 0; i < 201; i++)
+            durationFilterValue += "a";
+
+        var request = new GetAppointmentTypesFilterDurationRequest
+        {
+            DurationFilterValue = durationFilterValue
+        };
+
+        var query = new GetAppointmentTypesFilterDurationQuery(request);
+
+        // Act
+        var validationResult = _validator.TestValidate(query);
+
+        // Assert
+        validationResult.ShouldHaveValidationErrorFor(x =>
+            x.GetAppointmentTypesFilterDurationRequest.DurationFilterValue);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    public void Validation_DurationFilterValueIsEmpty_Fails(
+        string durationFilterValue)
+    {
+        // Arrange
+        var request = new GetAppointmentTypesFilterDurationRequest
+        {
+            DurationFilterValue = durationFilterValue
+        };
+
+        var query = new GetAppointmentTypesFilterDurationQuery(request);
+
+        // Act
+        var validationResult = _validator.TestValidate(query);
+
+        // Assert
+        validationResult.ShouldHaveValidationErrorFor(x =>
+            x.GetAppointmentTypesFilterDurationRequest.DurationFilterValue);
+    }
+}
