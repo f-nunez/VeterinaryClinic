33fc4aa0d21b03d26ae6100204677ac25f16d9d7
Francisco Nuñez <fonfonferi@gmail.com>
2023-05-15 22:12:54 -0700

    UpdateppointmentCommandValidator in Scheduling.Application #36
    Update validation rules

--- Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application/Features/Appointments/Commands/UpdateAppointment/UpdateAppointmentCommandValidator.cs
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application/Features/Appointments/Commands/UpdateAppointment/UpdateAppointmentCommandValidator.cs
@@ -16,23 +16,26 @@ public class UpdateAppointmentCommandValidator
         RuleFor(v => v.UpdateAppointmentRequest.DoctorId)
             .GreaterThan(0).WithMessage("DoctorId is required.");
 
-        RuleFor(v => v.UpdateAppointmentRequest.RoomId)
-            .GreaterThan(0).WithMessage("RoomId is required.");
-
         RuleFor(v => v.UpdateAppointmentRequest.Description)
             .NotNull().WithMessage("Description is required.")
             .NotEmpty().WithMessage("Description is required.")
             .MaximumLength(2000).WithMessage("Description must be less or equals than 2000 characters.");
-        
+
+        RuleFor(v => v.UpdateAppointmentRequest.EndOn)
+            .NotNull().WithMessage("EndOn is required.")
+            .NotEmpty().WithMessage("EndOn is required.");
+
+        RuleFor(v => v.UpdateAppointmentRequest.RoomId)
+            .GreaterThan(0).WithMessage("RoomId is required.");
+
+        RuleFor(v => v.UpdateAppointmentRequest.StartOn)
+            .NotNull().WithMessage("StartOn is required.")
+            .NotEmpty().WithMessage("StartOn is required.")
+            .LessThan(v => v.UpdateAppointmentRequest.EndOn).WithMessage("StartOn must be less than EndOn.");
+
         RuleFor(v => v.UpdateAppointmentRequest.Title)
             .NotNull().WithMessage("Title is required.")
             .NotEmpty().WithMessage("Title is required.")
             .MaximumLength(200).WithMessage("Title must be less or equals than 2000 characters.");
-
-        RuleFor(v => v.UpdateAppointmentRequest.StartOn)
-            .NotNull().WithMessage("StartOn is required.");
-
-        RuleFor(v => v.UpdateAppointmentRequest.EndOn)
-            .NotNull().WithMessage("StartOn is required.");
     }
 }
