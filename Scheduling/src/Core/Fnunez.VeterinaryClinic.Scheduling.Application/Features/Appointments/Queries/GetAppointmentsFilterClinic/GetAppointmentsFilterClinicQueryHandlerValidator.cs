24b91cd46b9e28bba7d774236e06d70ee5ed6e98
Francisco Nuñez <fonfonferi@gmail.com>
2023-05-15 22:24:08 -0700

    GetAppointmentsFilterClinicQueryValidator in Scheduling.Application #36
    Update validation rules

--- Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application/Features/Appointments/Queries/GetAppointmentsFilterClinic/GetAppointmentsFilterClinicQueryHandlerValidator.cs
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application/Features/Appointments/Queries/GetAppointmentsFilterClinic/GetAppointmentsFilterClinicQueryHandlerValidator.cs
@@ -8,6 +8,13 @@ public class GetAppointmentsFilterClinicQueryHandlerValidator
     public GetAppointmentsFilterClinicQueryHandlerValidator()
     {
         RuleFor(v => v.GetAppointmentsFilterClinicRequest.DataGridRequest.Search)
-            .MaximumLength(200).WithMessage("Search must be less or equals than 200 characters.");
+            .MaximumLength(200).WithMessage("Search must not exceed 200 characters.");
+
+        RuleFor(v => v.GetAppointmentsFilterClinicRequest.DataGridRequest.Skip)
+            .GreaterThanOrEqualTo(0).WithMessage("Skip must be greater than or equal to 0.");
+
+        RuleFor(v => v.GetAppointmentsFilterClinicRequest.DataGridRequest.Take)
+            .GreaterThan(0).WithMessage("Take must be greater than 0.")
+            .LessThanOrEqualTo(100).WithMessage("Take must be less than or equal to 100.");
     }
 }
