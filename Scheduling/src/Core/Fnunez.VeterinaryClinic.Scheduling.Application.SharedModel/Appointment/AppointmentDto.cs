0c5c2dc5a3c58165d0be4489f5198bea9bee1e35
Francisco Nuñez <fonfonferi@gmail.com>
2023-05-16 17:57:06 -0700

    AppointmentDto in Scheduling.Application.SharedModel #36
    Remove unused properties

--- Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel/Appointment/AppointmentDto.cs
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel/Appointment/AppointmentDto.cs
@@ -5,17 +5,12 @@ public class AppointmentDto
     public Guid AppointmentId { get; set; }
     public int AppointmentTypeId { get; set; }
     public int ClientId { get; set; }
-    public string ClientFullName { get; set; } = string.Empty;
     public int ClinicId { get; set; }
-    public string ClinicName { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public int DoctorId { get; set; }
-    public string DoctorFullName { get; set; } = string.Empty;
     public bool IsConfirmed { get; set; }
     public int PatientId { get; set; }
-    public string PatientName { get; set; } = string.Empty;
     public int RoomId { get; set; }
-    public string RoomName { get; set; } = string.Empty;
     public string Title { get; set; } = string.Empty;
     public DateTimeOffset StartOn { get; set; }
     public DateTimeOffset EndOn { get; set; }
