ab37c1535de74e62d7ad9fc40d43dca20a44ab61
Francisco Nuñez <fonfonferi@gmail.com>
2023-05-16 18:26:13 -0700

    GetDoctorsFilterFullNameQueryHandlerTests in Scheduling.Application.IntegrationTests #36

---
+++ Scheduling/tests/Core/Fnunez.VeterinaryClinic.Scheduling.Application.IntegrationTests/Features/Doctors/Queries/GetDoctorsFilterFullName/GetDoctorsFilterFullNameQueryHandlerTests.cs
@@ -0,0 +1,69 @@
+using Fnunez.VeterinaryClinic.Scheduling.Application.Common.Exceptions;
+using Fnunez.VeterinaryClinic.Scheduling.Application.Features.Doctors.Queries.GetDoctorsFilterFullName;
+using Fnunez.VeterinaryClinic.Scheduling.Application.SharedModel.Doctor.GetDoctorsFilterFullName;
+using Fnunez.VeterinaryClinic.Scheduling.Domain.SyncedAggregates.DoctorAggregate;
+
+namespace Fnunez.VeterinaryClinic.Scheduling.Application.IntegrationTests.Features.Doctors.Queries.GetDoctorsFilterFullName;
+
+[Collection(nameof(TestContextFixture))]
+public class GetDoctorsFilterFullNameQueryHandlerTests
+    : IClassFixture<TestContextFixture>
+{
+    private readonly TestContextFixture _fixture;
+
+    public GetDoctorsFilterFullNameQueryHandlerTests(
+        TestContextFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact]
+    public async Task Handle_RequiredEntriesAreEmpty_ThrowsValidationException()
+    {
+        // Arrange
+        var request = new GetDoctorsFilterFullNameRequest();
+
+        var query = new GetDoctorsFilterFullNameQuery(request);
+
+        // Act
+        var actual = await Assert.ThrowsAsync<ValidationException>(() =>
+            _fixture.SendAsync(query));
+
+        // Assert
+        Assert.NotNull(actual);
+
+        Assert.IsType<ValidationException>(actual);
+    }
+
+    [Fact]
+    public async Task Handle_ReturnsGetDoctorsFilterFullNameResponse()
+    {
+        // Arrange
+        var fullName = "a";
+
+        var doctor = new Doctor(fullName);
+
+        await _fixture.AddAsync<Doctor>(doctor);
+
+        var request = new GetDoctorsFilterFullNameRequest
+        {
+            FullNameFilterValue = fullName
+        };
+
+        var query = new GetDoctorsFilterFullNameQuery(request);
+
+        // Act
+        var actual = await _fixture.SendAsync(query);
+
+        // Assert
+        Assert.NotNull(actual);
+
+        Assert.IsType<GetDoctorsFilterFullNameResponse>(actual);
+
+        Assert.NotNull(actual.DoctorFullNames);
+
+        Assert.NotEmpty(actual.DoctorFullNames);
+
+        Assert.StartsWith(fullName, actual.DoctorFullNames.FirstOrDefault());
+    }
+}
