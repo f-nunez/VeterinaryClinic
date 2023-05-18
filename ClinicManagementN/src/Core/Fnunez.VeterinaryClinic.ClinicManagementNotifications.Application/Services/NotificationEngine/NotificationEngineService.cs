eb650e8e7bfd154fd5eeb17c416d171515b62e38
Francisco Nuñez <fonfonferi@gmail.com>
2023-05-17 21:27:52 -0700

    NotificationEngineService in ClinicManagementNotifications.Application #36
    Throw Argumentexception when notificationEventString is empty

--- ClinicManagementN/src/Core/Fnunez.VeterinaryClinic.ClinicManagementNotifications.Application/Services/NotificationEngine/NotificationEngineService.cs
+++ ClinicManagementN/src/Core/Fnunez.VeterinaryClinic.ClinicManagementNotifications.Application/Services/NotificationEngine/NotificationEngineService.cs
@@ -36,7 +36,8 @@ public class NotificationEngineService : INotificationEngineService
         CancellationToken cancellationToken)
     {
         if (string.IsNullOrEmpty(notificationEventString))
-            throw new ArgumentNullException(nameof(notificationEventString));
+            throw new ArgumentException(
+                $"{nameof(notificationEventString)} is empty.");
 
         if (string.IsNullOrEmpty(serializedNotificationRequest))
             throw new ArgumentNullException(
