1b389c7445b4ed8e1ad9c9bc57bca92363adc182
Francisco Nuñez <fonfonferi@gmail.com>
2023-05-17 21:39:08 -0700

    NotificationTests in ClinicManagementNotifications.Domain.UnitTests #36

---
+++ ClinicManagementN/tests/Core/Fnunez.VeterinaryClinic.ClinicManagementNotifications.Domain.UnitTests/NotificationAggregate/NotificationTests.cs
@@ -0,0 +1,228 @@
+using Fnunez.VeterinaryClinic.ClinicManagementNotifications.Domain.NotificationAggregate;
+using Fnunez.VeterinaryClinic.ClinicManagementNotifications.Domain.NotificationAggregate.Enums;
+
+namespace Fnunez.VeterinaryClinic.ClinicManagementNotifications.Domain.UnitTests.NotificationAggregate;
+
+public class NotificationTests
+{
+    [Fact]
+    public void Constructor_CorrelationId_SetsCorrelationIdProperty()
+    {
+        // Arrange
+        var correlationId = Guid.NewGuid();
+
+        var createdOn = DateTimeOffset.UtcNow;
+
+        var notificationEvent = NotificationEvent.AppointmentTypeCreated;
+
+        var payload = "payload";
+
+        var triggeredByUserId = Guid.NewGuid().ToString();
+
+        var notification = new Notification
+        (
+            correlationId,
+            createdOn,
+            notificationEvent,
+            payload,
+            triggeredByUserId
+        );
+
+        // Assert
+        Assert.Equal(correlationId, notification.CorrelationId);
+    }
+
+    [Fact]
+    public void Constructor_CorrelationIdIsEmpty_ThrowsArgumentException()
+    {
+        // Arrange
+        var correlationId = Guid.Empty;
+
+        var createdOn = DateTimeOffset.UtcNow;
+
+        var notificationEvent = NotificationEvent.AppointmentTypeCreated;
+
+        var payload = "payload";
+
+        var triggeredByUserId = Guid.NewGuid().ToString();
+
+        // Act
+        Action actual = () => new Notification
+        (
+            correlationId,
+            createdOn,
+            notificationEvent,
+            payload,
+            triggeredByUserId
+        );
+
+        // Assert
+        Assert.Throws<ArgumentException>(actual);
+    }
+
+    [Fact]
+    public void Constructor_CreatedOn_SetsCreatedOnProperty()
+    {
+        // Arrange
+        var correlationId = Guid.NewGuid();
+
+        var createdOn = DateTimeOffset.UtcNow;
+
+        var notificationEvent = NotificationEvent.AppointmentTypeCreated;
+
+        var payload = "payload";
+
+        var triggeredByUserId = Guid.NewGuid().ToString();
+
+        var notification = new Notification
+        (
+            correlationId,
+            createdOn,
+            notificationEvent,
+            payload,
+            triggeredByUserId
+        );
+
+        // Assert
+        Assert.Equal(createdOn, notification.CreatedOn);
+    }
+
+    [Fact]
+    public void Constructor_NotificationEvent_SetsNotificationEventProperty()
+    {
+        // Arrange
+        var correlationId = Guid.NewGuid();
+
+        var createdOn = DateTimeOffset.UtcNow;
+
+        var notificationEvent = NotificationEvent.AppointmentTypeCreated;
+
+        var payload = "payload";
+
+        var triggeredByUserId = Guid.NewGuid().ToString();
+
+        var notification = new Notification
+        (
+            correlationId,
+            createdOn,
+            notificationEvent,
+            payload,
+            triggeredByUserId
+        );
+
+        // Assert
+        Assert.Equal(notificationEvent, notification.NotificationEvent);
+    }
+
+    [Fact]
+    public void Constructor_Payload_SetsPayloadProperty()
+    {
+        // Arrange
+        var correlationId = Guid.NewGuid();
+
+        var createdOn = DateTimeOffset.UtcNow;
+
+        var notificationEvent = NotificationEvent.AppointmentTypeCreated;
+
+        var payload = "payload";
+
+        var triggeredByUserId = Guid.NewGuid().ToString();
+
+        var notification = new Notification
+        (
+            correlationId,
+            createdOn,
+            notificationEvent,
+            payload,
+            triggeredByUserId
+        );
+
+        // Assert
+        Assert.Equal(payload, notification.Payload);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    public void Constructor_PayloadIsEmpty_ThrowsArgumentException(
+        string payload)
+    {
+        // Arrange
+        var correlationId = Guid.NewGuid();
+
+        var createdOn = DateTimeOffset.UtcNow;
+
+        var notificationEvent = NotificationEvent.AppointmentTypeCreated;
+
+        var triggeredByUserId = Guid.NewGuid().ToString();
+
+        // Act
+        Action actual = () => new Notification
+        (
+            correlationId,
+            createdOn,
+            notificationEvent,
+            payload,
+            triggeredByUserId
+        );
+
+        // Assert
+        Assert.Throws<ArgumentException>(actual);
+    }
+
+    [Fact]
+    public void Constructor_TriggeredByUserId_SetsTriggeredByUserIdProperty()
+    {
+        // Arrange
+        var correlationId = Guid.NewGuid();
+
+        var createdOn = DateTimeOffset.UtcNow;
+
+        var notificationEvent = NotificationEvent.AppointmentTypeCreated;
+
+        var payload = "payload";
+
+        var triggeredByUserId = Guid.NewGuid().ToString();
+
+        var notification = new Notification
+        (
+            correlationId,
+            createdOn,
+            notificationEvent,
+            payload,
+            triggeredByUserId
+        );
+
+        // Assert
+        Assert.Equal(triggeredByUserId, notification.TriggeredByUserId);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    public void Constructor_TriggeredByUserIdIsEmpty_ThrowsArgumentException(
+        string triggeredByUserId)
+    {
+        // Arrange
+        var correlationId = Guid.NewGuid();
+
+        var createdOn = DateTimeOffset.UtcNow;
+
+        var notificationEvent = NotificationEvent.AppointmentTypeCreated;
+
+        var payload = "payload";
+
+        // Act
+        Action actual = () => new Notification
+        (
+            correlationId,
+            createdOn,
+            notificationEvent,
+            payload,
+            triggeredByUserId
+        );
+
+        // Assert
+        Assert.Throws<ArgumentException>(actual);
+    }
+}
