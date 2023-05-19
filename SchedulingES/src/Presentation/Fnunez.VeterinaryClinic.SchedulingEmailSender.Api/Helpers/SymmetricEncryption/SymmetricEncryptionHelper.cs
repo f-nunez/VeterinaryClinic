275d11399f293ea285457aea0d0b17f03f06ad9e
Francisco Nuñez <fonfonferi@gmail.com>
2023-05-19 15:26:41 -0700

    SymmetricEncryptionHelper in SchedulingEmailSender.Api #36
    Throws ArgumentException when stringToEncrypt is empty

--- SchedulingES/src/Presentation/Fnunez.VeterinaryClinic.SchedulingEmailSender.Api/Helpers/SymmetricEncryption/SymmetricEncryptionHelper.cs
+++ SchedulingES/src/Presentation/Fnunez.VeterinaryClinic.SchedulingEmailSender.Api/Helpers/SymmetricEncryption/SymmetricEncryptionHelper.cs
@@ -39,6 +39,9 @@ public class SymmetricEncryptionHelper : ISymmetricEncryptionHelper
 
     public async Task<string> EncryptToBase64Async(string stringToEncrypt)
     {
+        if (string.IsNullOrEmpty(stringToEncrypt))
+            throw new ArgumentException($"{nameof(stringToEncrypt)} is empty.");
+
         using var aes = Aes.Create();
 
         aes.IV = GetIV();
