bb8dde59ad10a9a08b6355cb7a317be5e801fdef
Francisco Nuñez <fonfonferi@gmail.com>
2023-05-21 20:44:28 -0700

    RoomUpdatedSendIntegrationEvent in ClinicManagement.Application #39
    Delete class

--- ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Features/Rooms/SendIntegrationEvents/RoomUpdated/RoomUpdatedSendIntegrationEvent.cs
+++
@@ -1,7 +0,0 @@
-using Contracts;
-using MediatR;
-
-namespace Fnunez.VeterinaryClinic.ClinicManagement.Application.Features.Rooms.SendIntegrationEvents.RoomUpdated;
-
-public record RoomUpdatedSendIntegrationEvent(RoomUpdatedIntegrationEventContract RoomUpdatedIntegrationEventContract)
-    : INotification;
