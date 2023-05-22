5c7f19ce98cb998e53f3e06e38423309290ac243
Francisco Nuñez <fonfonferi@gmail.com>
2023-05-21 20:49:54 -0700

    DoctorUpdatedIntegrationEventContract in Scheduling.Application #39
    Delete class

--- Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application/Contracts/IntegrationEvents/Doctor/DoctorUpdatedIntegrationEventContract.cs
+++
@@ -1,8 +0,0 @@
-namespace Contracts;
-
-public class DoctorUpdatedIntegrationEventContract
-    : BaseIntegrationEventContract
-{
-    public string DoctorFullName { get; set; } = string.Empty;
-    public int DoctorId { get; set; }
-}
