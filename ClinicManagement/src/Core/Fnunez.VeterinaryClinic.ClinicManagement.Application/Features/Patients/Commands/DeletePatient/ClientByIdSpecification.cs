a722666a1263352d157f12a5e15c25f596552460
Francisco Nuñez <fonfonferi@gmail.com>
2023-05-21 20:28:41 -0700

    ClientByIdSpecification in ClinicManagement.Application #39

---
+++ ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Features/Patients/Commands/DeletePatient/ClientByIdSpecification.cs
@@ -0,0 +1,14 @@
+using Fnunez.VeterinaryClinic.ClinicManagement.Domain.ClientAggregate;
+using Fnunez.VeterinaryClinic.SharedKernel.Application.Specifications;
+
+namespace Fnunez.VeterinaryClinic.ClinicManagement.Application.Features.Patients.Commands.DeletePatient;
+
+public class ClientByIdSpecification : BaseSpecification<Client>
+{
+    public ClientByIdSpecification(int clientId)
+    {
+        Query
+            .Include(c => c.Patients)
+            .Where(c => c.Id == clientId);
+    }
+}
