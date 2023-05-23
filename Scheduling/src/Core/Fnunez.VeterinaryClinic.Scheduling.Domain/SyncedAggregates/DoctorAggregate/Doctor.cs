5e72298a787ca976e45f5d100db828500013f2d5
Francisco Nuñez <fonfonferi@gmail.com>
2023-05-22 23:17:33 -0700

    Doctor in Scheduling.Domain #39
    Add domain behaviors

--- Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Domain/SyncedAggregates/DoctorAggregate/Doctor.cs
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Domain/SyncedAggregates/DoctorAggregate/Doctor.cs
@@ -13,6 +13,37 @@ public class Doctor : BaseEntity<int>, IAggregateRoot
 
     public Doctor(string fullName)
     {
+        if (string.IsNullOrEmpty(fullName))
+            throw new ArgumentException(
+                $"Required input {nameof(fullName)} was empty.",
+                nameof(fullName));
+
+        FullName = fullName;
+    }
+
+    public Doctor(int id, string fullName)
+    {
+        if (id <= 0)
+            throw new ArgumentException(
+                $"Required input {nameof(id)} cannot be zero or negative.",
+                nameof(id));
+
+        if (string.IsNullOrEmpty(fullName))
+            throw new ArgumentException(
+                $"Required input {nameof(fullName)} was empty.",
+                nameof(fullName));
+
+        Id = id;
+        FullName = fullName;
+    }
+
+    public void UpdateFullName(string fullName)
+    {
+        if (string.IsNullOrEmpty(fullName))
+            throw new ArgumentException(
+                $"Required input {nameof(fullName)} was empty.",
+                nameof(fullName));
+
         FullName = fullName;
     }
 
