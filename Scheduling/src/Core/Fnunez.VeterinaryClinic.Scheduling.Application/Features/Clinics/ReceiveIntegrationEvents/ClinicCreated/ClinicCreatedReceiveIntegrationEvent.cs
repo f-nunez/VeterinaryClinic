3b7ce70ee8d075c355a68accecbcb9a8b50be0ed
Francisco Nuñez <fonfonferi@gmail.com>
2023-05-22 23:27:54 -0700

    ClinicCreatedReceiveIntegrationEvent in Scheduling.Application #39
    Implement integration event

--- Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application/Features/Clinics/ReceiveIntegrationEvents/ClinicCreated/ClinicCreatedReceiveIntegrationEvent.cs
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application/Features/Clinics/ReceiveIntegrationEvents/ClinicCreated/ClinicCreatedReceiveIntegrationEvent.cs
@@ -1,7 +1,7 @@
-using Contracts;
+using Fnunez.VeterinaryClinic.Scheduling.Application.Services.IntegrationEventReceiver.IntegrationEvents;
 using MediatR;
 
 namespace Fnunez.VeterinaryClinic.Scheduling.Application.Features.Clinics.ReceiveIntegrationEvents.ClinicCreated;
 
-public record ClinicCreatedReceiveIntegrationEvent(ClinicCreatedIntegrationEventContract ClinicCreatedIntegrationEventContract)
+public record ClinicCreatedReceiveIntegrationEvent(ClinicCreatedIntegrationEvent ClinicCreatedIntegrationEvent)
     : INotification;
