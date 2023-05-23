545eb0a9418b0fa6d584ad8c2d6156ef1b30b55a
Francisco Nuñez <fonfonferi@gmail.com>
2023-05-22 23:52:13 -0700

    DoctorTests in Scheduling.Domain.UnitTests #39
    Test cases on domain behaviors

--- Scheduling/tests/Core/Fnunez.VeterinaryClinic.Scheduling.Domain.UnitTests/SyncedAggregates/DoctorAggregate/DoctorTests.cs
+++ Scheduling/tests/Core/Fnunez.VeterinaryClinic.Scheduling.Domain.UnitTests/SyncedAggregates/DoctorAggregate/DoctorTests.cs
@@ -15,4 +15,46 @@ public class DoctorTests
         // Assert
         Assert.Equal(_fullName, doctor.FullName);
     }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    public void Constructor_FullNameIsEmpty_ThrowsArgumentException(
+        string fullName)
+    {
+        // Act
+        Action actual = () => new Doctor(fullName);
+
+        // Assert
+        Assert.Throws<ArgumentException>(actual);
+    }
+
+    [Fact]
+    public void UpdateFullName_FullName_UpdatesFullNameProperty()
+    {
+        // Arrange
+        var doctor = new Doctor();
+
+        // Act
+        doctor.UpdateFullName(_fullName);
+
+        // Assert
+        Assert.Equal(_fullName, doctor.FullName);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    public void UpdateFullName_FullNameIsEmpty_ThrowsArgumentException(
+        string fullName)
+    {
+        // Arrange
+        var doctor = new Doctor();
+
+        // Act
+        Action actual = () => doctor.UpdateFullName(fullName);
+
+        // Assert
+        Assert.Throws<ArgumentException>(actual);
+    }
 }
