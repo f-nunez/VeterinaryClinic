7a6a687cf88d75a1bd4740ae7d81877dd431ad5d
Francisco Nuñez <fonfonferi@gmail.com>
2023-05-22 23:33:37 -0700

    RoomDeletedReceiveIntegrationEvent in Scheduling.Application #39
    Implement integration event

--- Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application/Features/Rooms/ReceiveIntegrationEvents/RoomDeleted/RoomDeletedReceiveIntegrationEvent.cs
+++ Scheduling/src/Core/Fnunez.VeterinaryClinic.Scheduling.Application/Features/Rooms/ReceiveIntegrationEvents/RoomDeleted/RoomDeletedReceiveIntegrationEvent.cs
@@ -1,7 +1,7 @@
-using Contracts;
+using Fnunez.VeterinaryClinic.Scheduling.Application.Services.IntegrationEventReceiver.IntegrationEvents;
 using MediatR;
 
 namespace Fnunez.VeterinaryClinic.Scheduling.Application.Features.Rooms.ReceiveIntegrationEvents.RoomDeleted;
 
-public record RoomDeletedReceiveIntegrationEvent(RoomDeletedIntegrationEventContract RoomDeletedIntegrationEventContract)
+public record RoomDeletedReceiveIntegrationEvent(RoomDeletedIntegrationEvent RoomDeletedIntegrationEvent)
     : INotification;
