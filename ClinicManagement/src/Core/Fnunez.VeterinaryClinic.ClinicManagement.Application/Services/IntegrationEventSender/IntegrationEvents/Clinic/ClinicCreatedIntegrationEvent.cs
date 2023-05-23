24ad6a650e7f0d0c50363792a49b1dc045c8a8ea
Francisco Nuñez <fonfonferi@gmail.com>
2023-05-22 22:49:46 -0700

    ClinicCreatedIntegrationEvent in ClinicManagement.Application #39

---
+++ ClinicManagement/src/Core/Fnunez.VeterinaryClinic.ClinicManagement.Application/Services/IntegrationEventSender/IntegrationEvents/Clinic/ClinicCreatedIntegrationEvent.cs
@@ -0,0 +1,9 @@
+namespace Fnunez.VeterinaryClinic.ClinicManagement.Application.Services.IntegrationEventSender.IntegrationEvents;
+
+public class ClinicCreatedIntegrationEvent : BaseIntegrationEvent
+{
+    public string ClinicAddress { get; set; } = string.Empty;
+    public string ClinicEmailAddress { get; set; } = string.Empty;
+    public int ClinicId { get; set; }
+    public string ClinicName { get; set; } = string.Empty;
+}
