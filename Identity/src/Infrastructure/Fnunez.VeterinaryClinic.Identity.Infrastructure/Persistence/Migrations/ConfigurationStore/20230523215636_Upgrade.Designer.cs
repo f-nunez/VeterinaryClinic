49bce305e2d80d597e14ae682c3b4f12556ecd16
Francisco Nuñez <fonfonferi@gmail.com>
2023-05-23 20:53:03 -0700

    Identity.Infrastructure #41
    Add migrations to upgrade database features for IdentityServer

--- Identity/src/Infrastructure/Fnunez.VeterinaryClinic.Identity.Infrastructure/Persistence/Migrations/ConfigurationStore/20230523215636_Upgrade.Designer.cs
+++ Identity/src/Infrastructure/Fnunez.VeterinaryClinic.Identity.Infrastructure/Persistence/Migrations/ConfigurationStore/20230523215636_Upgrade.Designer.cs
@@ -4,6 +4,7 @@ using Fnunez.VeterinaryClinic.Identity.Infrastructure.Persistence.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Fnunez.VeterinaryClinic.Identity.Infrastructure.Persistence.Migrations.ConfigurationStore
 {
     [DbContext(typeof(ConfigurationStoreDbContext))]
-    partial class ConfigurationStoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230523215636_Upgrade")]
+    partial class Upgrade
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Identity/src/Infrastructure/Fnunez.VeterinaryClinic.Identity.Infrastructure/Persistence/Migrations/OperationalStore/20230523215650_Upgrade.Designer.cs
+++ Identity/src/Infrastructure/Fnunez.VeterinaryClinic.Identity.Infrastructure/Persistence/Migrations/OperationalStore/20230523215650_Upgrade.Designer.cs
@@ -4,6 +4,7 @@ using Fnunez.VeterinaryClinic.Identity.Infrastructure.Persistence.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Fnunez.VeterinaryClinic.Identity.Infrastructure.Persistence.Migrations.OperationalStore
 {
     [DbContext(typeof(OperationalStoreDbContext))]
-    partial class OperationalStoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230523215650_Upgrade")]
+    partial class Upgrade
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
